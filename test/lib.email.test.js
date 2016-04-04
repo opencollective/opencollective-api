b1d36b86ee0fff7f04b39884b6146348ebca3a2b
Xavier Damman <xdamman@gmail.com>
2016-04-04 00:39:58 -0400

    added test for wwcode template

--- test/lib.email.test.js
+++ test/lib.email.test.js
@@ -9,7 +9,7 @@ describe('lib/email', () => {
   
   it('sends the thankyou.fr email template', function(done) {
     this.timeout(2000);
-    
+
     const paymentData = {
       amountFloat: 50.00,
       currency: 'EUR'
@@ -29,15 +29,42 @@ describe('lib/email', () => {
     const previousSendMail = app.mailgun.sendMail;
     app.mailgun.sendMail = (options) => {
       expect(options.to).to.equal(data.user.email);
-      console.log("Called!", options.subject);
       expect(options.subject).to.contain('Merci pour votre donation de €50/mois à La Primaire');
-      console.log("html:",options.html);
       done();
       app.mailgun.sendMail = previousSendMail;
       return options;
     }
-
     emailLib.send('thankyou', data.user.email, data);
+  });
 
+  it('sends the thankyou.wwcode email template', function(done) {
+    this.timeout(2000);
+
+    const paymentData = {
+      amountFloat: 50.00,
+      currency: 'USD'
+     };
+
+    const data = {
+      donation: paymentData,
+      interval: 'month',
+      user: emailData.user,
+      group: {
+        name: "WWCode Austin",
+        slug: "wwcodeaustin"
+      },
+      config: config
+    };
+
+    const previousSendMail = app.mailgun.sendMail;
+    app.mailgun.sendMail = (options) => {
+      expect(options.to).to.equal(data.user.email);
+      expect(options.subject).to.contain('Thank you for your $50/month donation to WWCode Austin');
+      expect(options.html).to.contain('4218859');
+      done();
+      app.mailgun.sendMail = previousSendMail;
+      return options;
+    }
+    emailLib.send('thankyou', data.user.email, data);
   });
 });
