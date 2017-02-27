d0bc1b4190f4e0badb6ecc3e0b64ed7a1b9e10f6
Xavier Damman <xdamman@gmail.com>
2017-02-27 18:41:40 +0100

    fix for default backgroundImage in email templates

--- server/models/Group.js
+++ server/models/Group.js
@@ -15,7 +15,7 @@ import activities from '../constants/activities';
 import Promise from 'bluebird';
 
 const DEFAULT_LOGO = '/static/images/1px.png';
-const DEFAULT_BACKGROUND_IMG = '/static/images/collectives/default-header-bg.jpg';
+const DEFAULT_BACKGROUND_IMG = `${config.host.website}/static/images/collectives/default-header-bg.jpg`;
 
 const getDefaultSettings = (group) => {
   return {
