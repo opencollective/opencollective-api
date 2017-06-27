c8fc3337129acd3f570fbe54a130856ce9b2d3d1
Xavier Damman <xdamman@gmail.com>
2017-06-26 23:27:23 -0400

    updated npm run git:clean

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#if [[ `git checkout master` ]]; then 
-#  for BRANCH in `git branch | grep -v "*"`; do
-BRANCH=updateUsername
+if [[ `git checkout master` ]]; then 
+  for BRANCH in `git branch | grep -v "*"`; do
     git checkout $BRANCH > /dev/null 2>&1;
     if [[ `git merge master --no-edit 2> /dev/null` ]]; then
       echo "> $BRANCH merged with master";
@@ -16,6 +15,6 @@ BRANCH=updateUsername
       git reset --hard > /dev/null;
       echo "Unable to merge $BRANCH with master";
     fi; 
-#  done;
-#  git checkout master;
-#fi;
+  done;
+  git checkout master;
+fi;
