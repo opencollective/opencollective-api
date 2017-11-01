424960eecd5918885eae756de24097f073185fd1
Aseem Sood <asood123@yahoo.com>
2017-11-01 11:35:17 -0400

    fetch right data for invoicing

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,26 +1,45 @@
-import models from '../models';
+import models, { sequelize } from '../models';
 
 /**
  * Get a transaction
  */
-export const getOne = (req, res) => {
+export const getOne = (req, res, next) => {
   Promise.all([
-    models.User.findOne({
-      where: { CollectiveId: req.transaction.HostCollectiveId },
-      include: [ { model: models.Collective, as: 'collective' } ]
+    models.Collective.findOne({
+      where: { id: req.transaction.HostCollectiveId },
     }),
     req.transaction.getCollective(),
     req.transaction.getFromCollective(),
-    req.transaction.getCreatedByUser()
+    req.transaction.getCreatedByUser(),
+    sequelize.query(`
+    WITH admins AS 
+      (SELECT "MemberCollectiveId" FROM "Members" 
+        WHERE "CollectiveId" = :HostCollectiveId 
+        AND (role = 'ADMIN' OR role = 'HOST'))
+    
+    SELECT * FROM "Users" 
+    WHERE "billingAddress" IS NOT NULL 
+      AND "CollectiveId" IN (SELECT * FROM admins);
+
+      `, {
+        type: sequelize.QueryTypes.SELECT,
+        replacements: {
+          HostCollectiveId: req.transaction.HostCollectiveId
+        }}) // fetch all admins of the host and see if any of them have a billingAddress. One of them should.
   ])
     .then(results => {
-      const host = results[0].collective.info;
+      const host = results[0].info;
       const collective = results[1].card;
       const fromCollective = results[2].card;
       const createdByUser = results[3].public;
-      host.billingAddress = results[0].billingAddress;
+      const hostAdmins = results[4];
       createdByUser.billingAddress = results[3].billingAddress;
+
+      if (hostAdmins.length > 0) {
+        host.billingAddress = hostAdmins[0].billingAddress;
+      }
       return Object.assign({}, req.transaction.info, { host, fromCollective, collective, createdByUser });
     })
     .then(transaction => res.send(transaction))
+    .catch(next)
 };
