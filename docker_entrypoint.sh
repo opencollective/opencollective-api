43859446364dd9da4c75a0c383bd2414e08162e6
miguelpeixe <miguel@cardume.art.br>
2018-02-28 14:58:40 -0300

    add docker setup and allow postinstall to skip migrate

---
+++ docker_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Executing migrations..."
+# wait for Postgres
+sleep 10
+until npm run db:migrate; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+./scripts/postinstall.sh
+echo "Executing process..."
+exec "$@"
