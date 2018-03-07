74920d99a1f10c8b168be6ff3e4989467be69083
Lincoln de Sousa <lincoln@comum.org>
2018-03-06 23:42:54 -0500

    data rewrite: Recalculate fees in the migration script

--- scripts/properly_populate_transaction_fees.js
+++ scripts/properly_populate_transaction_fees.js
@@ -1,5 +1,23 @@
+/**
+ * These are some recommendations for using the script in
+ * *development* mode. We currently have ~70000 transactions in our
+ * database, running it with a large batch to process all the
+ * transactions at once is the fastest way to run it and get the
+ * output, however it also exhausts node's memory limits with batches
+ * larger than ~50000. Use --max-old-space=8192 to do it all in one
+ * batch and iterate faster.
+ *
+ * For production usage, It might be a good idea to break it down in
+ * multiple batches to leave some room for the database to process
+ * operations from the web clients. I've been running 7 batches of
+ * 10000 and didn't take more than ~20s in my machine (i7~2.2GHz),
+ * which is just a little bit slower than running it all in one batch.
+ */
 import { ArgumentParser } from 'argparse';
 import models, { sequelize } from '../server/models';
+import * as transactionsLib from '../server/lib/transactions';
+import * as paymentsLib from '../server/lib/payments';
+import { OC_FEE_PERCENT } from '../server/constants/transactions';
 
 class Migration {
   constructor(options) {
@@ -19,29 +37,15 @@ class Migration {
       where: { deletedAt: null },
       order: ['TransactionGroup'],
       limit: this.options.batchSize,
-      offset: this.offset
+      offset: this.offset,
+      include: [{ model: models.Collective, as: 'collective' }]
     });
     this.offset += transactions.length;
     return transactions;
   }
 
-  /** net value of a transaction */
-  trValue = (tr) => Math.round(
-      tr.amountInHostCurrency +
-      tr.hostFeeInHostCurrency +
-      tr.platformFeeInHostCurrency +
-      tr.paymentProcessorFeeInHostCurrency) * tr.hostCurrencyFxRate;
-
-  /** Verify net value of a transaction */
-  verify = (tr) => this.trValue(tr) === tr.netAmountInCollectiveCurrency;
-
-  /** Difference between transaction net value & netAmountInCollectiveCurrency */
-  difference = (tr) => this.trValue(tr) - tr.netAmountInCollectiveCurrency;
-
   /** Convert `value` to negative if it's possitive */
-  toNegative = (value) => {
-    return value > 0 ? -value : value;
-  }
+  toNegative = (value) => value > 0 ? -value : value;
 
   /** Ensure that `tr` has the `hostCurrencyFxRate` field filled in */
   ensureHostCurrencyFxRate = (tr) => {
@@ -49,46 +53,130 @@ class Migration {
       tr.hostCurrencyFxRate = 1;
   }
 
+  /** Return true if the transaction has any host fees */
+  hasHostFee = (tr) => tr.hostFeeInHostCurrency
+    && parseInt(tr.hostFeeInHostCurrency, 10) !== 0;
+  /** Return true if the transaction has any platform fees */
+  hasPlatformFee = (tr) => tr.paymentProcessorFeeInHostCurrency
+    && parseInt(tr.paymentProcessorFeeInHostCurrency, 10) !== 0;
   /** Return false if there are no fees on a transaction */
-  hasFees = (tr) => (tr.hostFeeInHostCurrency && parseInt(tr.hostFeeInHostCurrency, 10) !== 0)
-    || (tr.platformFeeInHostCurrency && parseInt(tr.platformFeeInHostCurrency, 10) !== 0)
-    || (tr.paymentProcessorFeeInHostCurrency && parseInt(tr.paymentProcessorFeeInHostCurrency, 10) !== 0);
+  hasFees = (tr) => this.hasHostFee(tr) || this.hasPlatformFee(tr)
+    || (tr.platformFeeInHostCurrency && parseInt(tr.platformFeeInHostCurrency, 10) !== 0);
 
-  /** Rewrite the values of the fees */
   rewriteFees = (credit, debit) => {
-    credit.hostFeeInHostCurrency = debit.hostFeeInHostCurrency = this.toNegative(credit.hostFeeInHostCurrency);
-    credit.platformFeeInHostCurrency = debit.platformFeeInHostCurrency = this.toNegative(credit.platformFeeInHostCurrency);
-    credit.paymentProcessorFeeInHostCurrency = debit.paymentProcessorFeeInHostCurrency = this.toNegative(credit.paymentProcessorFeeInHostCurrency);
+    credit.hostFeeInHostCurrency = debit.hostFeeInHostCurrency =
+      this.toNegative(credit.hostFeeInHostCurrency || credit.hostFeeInHostCurrency);
+    credit.platformFeeInHostCurrency = debit.platformFeeInHostCurrency =
+      this.toNegative(credit.platformFeeInHostCurrency || debit.platformFeeInHostCurrency);
+    credit.paymentProcessorFeeInHostCurrency = debit.paymentProcessorFeeInHostCurrency =
+      this.toNegative(credit.paymentProcessorFeeInHostCurrency || debit.paymentProcessorFeeInHostCurrency);
+  }
+
+  /** Fix rounding errors in fees and rewrite netAmount */
+  rewriteFeesAndNetAmount = (credit, debit) => {
+    if (!credit.collective || !debit.collective) {
+      if (!credit.collective) console.log('credit with no collective!!!!', credit.id);
+      if (!debit.collective) console.log('debit with no collective!!!!', credit.id);
+      return;
+    }
+
+    /* Recalculate Host Fee */
+    let hostFeePercent;
+    if (this.hasHostFee(credit) || this.hasHostFee(debit)) {
+      hostFeePercent = Math.round(-this.toNegative(
+        (credit.hostFeePercent || debit.hostFeePercent) * 100 / credit.amountInHostCurrency));
+    }
+    if (hostFeePercent != 5 && hostFeePercent != 10 && hostFeePercent !== 0)
+      console.log('Suspicious hostFee',
+                  credit.id,
+                  credit.amountInHostCurrency,
+                  credit.amountInHostCurrency * hostFeePercent / 100,
+                  hostFeePercent);
+
+    /* Recalculate Platform Fee */
+    const platformFeePercent = Math.round(-this.toNegative(
+      credit.platformFeeInHostCurrency * 100 / credit.amountInHostCurrency));
+    if (platformFeePercent != 5 && platformFeePercent !== 0)
+      console.log('Suspicious platformFee',
+                  credit.id,
+                  credit.amountInHostCurrency,
+                  credit.amountInHostCurrency * 0.05,
+                  platformFeePercent);
+
+    /* This */
+    credit.platformFeeInHostCurrency = this.toNegative(
+      paymentsLib.calcFee(credit.amountInHostCurrency, OC_FEE_PERCENT));
+    debit.platformFeeInHostCurrency = this.toNegative(
+      paymentsLib.calcFee(credit.amountInHostCurrency, OC_FEE_PERCENT));
+
+    credit.netAmountInCollectiveCurrency = transactionsLib.netAmount(credit);
+    debit.netAmountInCollectiveCurrency = -credit.amountInHostCurrency * credit.hostCurrencyFxRate;
+  }
+
+  /** Make sure two transactions are pairs of each other */
+  validatePair = (tr1, tr2) => {
+    if (tr1.TransactionGroup !== tr2.TransactionGroup) {
+      throw new Error('Wrong transaction pair detected');
+    }
+    if (tr1.ExpenseId !== tr2.ExpenseId) {
+      throw new Error('Wrong transaction pair detected: ExpenseId does not match');
+    }
+    if (tr1.OrderId !== tr2.OrderId) {
+      throw new Error('Wrong transaction pair detected: OrderId does not match');
+    }
   }
 
   /** Migrate one pair of transactions */
   migrate = async (tr1, tr2) => {
     console.log(tr1.TransactionGroup);
     console.log(tr2.TransactionGroup);
-    if (tr1.TransactionGroup !== tr2.TransactionGroup) {
-      throw new Error('Wrong transaction pair detected');
-    }
+    this.validatePair(tr1, tr2);
+
     const credit = tr1.type === 'CREDIT' ? tr1 : tr2;
     const debit =  tr1.type === 'DEBIT' ? tr1 : tr2;
-    if (tr1.ExpenseId !== null && tr1.ExpenseId === tr2.ExpenseId) {
-      console.log('  Expense.:', this.verify(tr1), this.verify(tr2));
-    } else if (tr1.OrderId !== null && tr1.OrderId === tr2.OrderId) {
+
+    if (tr1.ExpenseId !== null) {
+      // Both CREDIT & DEBIT transactions add up
+      if (transactionsLib.verify(credit) && transactionsLib.verify(debit)) {
+        console.log('Expense.: true, true');
+        return;
+      }
+
+      // this.rewriteFees(credit, debit);
+
+      console.log('  Expense.:', transactionsLib.verify(tr1), transactionsLib.verify(tr2));
+
+      if (!transactionsLib.verify(credit)) {
+        console.log(`| EDAU | CREDIT | ${credit.id} | ${credit.TransactionGroup} | ${transactionsLib.difference(credit)} |`);
+      }
+      if (!transactionsLib.verify(debit)) {
+        console.log(`| EDAU  | DEBIT | ${debit.id} | ${debit.TransactionGroup}  | ${transactionsLib.difference(debit)}  |`);
+      }
+    } else if (tr1.OrderId !== null) {
       this.ensureHostCurrencyFxRate(credit);
       this.ensureHostCurrencyFxRate(debit);
+      this.rewriteFees(credit, debit);
 
-      console.log('  Order...:', this.verify(credit), this.verify(debit));
+      // Both CREDIT & DEBIT transactions add up
+      if (transactionsLib.verify(credit) && transactionsLib.verify(debit)) {
+        console.log('Order...: true, true');
+        return;
+      }
+
+      // Something is off
+      console.log('  Order...:', transactionsLib.verify(credit), transactionsLib.verify(debit));
       if (!this.hasFees(tr1) && !this.hasFees(tr2)) {
         console.log('    No fees, skipping');
         return;
       }
 
-      this.rewriteFees(credit, debit);
+      this.rewriteFeesAndNetAmount(credit, debit);
 
-      if (!this.verify(credit)) {
-        console.log(`    doesn't add up | ${credit.id} | CREDIT | ${credit.TransactionGroup} | ${this.difference(credit)} |`);
+      if (!transactionsLib.verify(credit)) {
+        console.log(`| ODAU | CREDIT | ${credit.id} | ${credit.TransactionGroup} | ${transactionsLib.difference(credit)} |`);
       }
-      if (!this.verify(debit)) {
-        console.log(`    doesn't add up | ${debit.id}  | DEBIT  | ${debit.TransactionGroup}  | ${this.difference(debit)}  |`);
+      if (!transactionsLib.verify(debit)) {
+        console.log(`| ODAU | DEBIT  | ${debit.id}  | ${debit.TransactionGroup}  | ${transactionsLib.difference(debit)}  |`);
       }
 
       // if (!credit.hostFeeInHostCurrency)
@@ -98,20 +186,20 @@ class Migration {
       // if (!credit.paymentProcessorFeeInHostCurrency)
       //   console.log('    * WARNING: C:ppFee.......: ', credit.paymentProcessorFeeInHostCurrency);
     } else {
-      console.log('  WAT.....:', this.verify(tr1), this.verify(tr2));
+      console.log('  WAT.....:', transactionsLib.verify(tr1), transactionsLib.verify(tr2));
     }
 
-    console.log('    * C:amount......: ', credit.amountInHostCurrency);
-    console.log('    * C:netAmount...: ', credit.netAmountInCollectiveCurrency);
-    console.log('    * C:hostFee.....: ', credit.hostFeeInHostCurrency);
-    console.log('    * C:platformFee.: ', credit.platformFeeInHostCurrency);
-    console.log('    * C:ppFee.......: ', credit.paymentProcessorFeeInHostCurrency);
+    // console.log('    * C:amount......: ', credit.amountInHostCurrency);
+    // console.log('    * C:netAmount...: ', credit.netAmountInCollectiveCurrency);
+    // console.log('    * C:hostFee.....: ', credit.hostFeeInHostCurrency);
+    // console.log('    * C:platformFee.: ', credit.platformFeeInHostCurrency);
+    // console.log('    * C:ppFee.......: ', credit.paymentProcessorFeeInHostCurrency);
 
-    console.log('    * D:amount......: ', debit.amountInHostCurrency);
-    console.log('    * D:netAmount...: ', debit.netAmountInCollectiveCurrency);
-    console.log('    * D:hostFee.....: ', debit.hostFeeInHostCurrency);
-    console.log('    * D:platformFee.: ', debit.platformFeeInHostCurrency);
-    console.log('    * D:ppFee.......: ', debit.paymentProcessorFeeInHostCurrency);
+    // console.log('    * D:amount......: ', debit.amountInHostCurrency);
+    // console.log('    * D:netAmount...: ', debit.netAmountInCollectiveCurrency);
+    // console.log('    * D:hostFee.....: ', debit.hostFeeInHostCurrency);
+    // console.log('    * D:platformFee.: ', debit.platformFeeInHostCurrency);
+    // console.log('    * D:ppFee.......: ', debit.paymentProcessorFeeInHostCurrency);
   }
 
   /** Run the whole migration */
@@ -143,11 +231,11 @@ function parseCommandLineArguments() {
     addHelp: true,
     description: 'Charge due subscriptions'
   });
-  parser.addArgument(['-v', '--verbose'], {
-    help: 'Verbose output',
-    defaultValue: false,
+  parser.addArgument(['-q', '--quiet'], {
+    help: 'Silence output',
+    defaultValue: true,
     action: 'storeConst',
-    constant: true
+    constant: false
   });
   parser.addArgument(['--notdryrun'], {
     help: "Pass this flag when you're ready to run the script for real",
@@ -165,7 +253,7 @@ function parseCommandLineArguments() {
   const args = parser.parseArgs();
   return {
     dryRun: !args.notdryrun,
-    verbose: args.verbose,
+    verbose: !args.quiet,
     limit: args.limit,
     batchSize: args.batch_size || 100
   };
