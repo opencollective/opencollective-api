ddaba9a7237279c03f73ecf5c6d279c9ff98d921
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-08 12:18:57 +0100

    Fix migrations

--- migrations/20181107133924-add-virtualcards-origin-collective-to-paymentmethod-and-order.js
+++ migrations/20181107133924-add-virtualcards-origin-collective-to-paymentmethod-and-order.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const ORDERS_VC_COL_NAME = 'UsingVirtualCardFromCollectiveId';
+const ordersVCColSettings = Sequelize => ({
+  type: Sequelize.INTEGER,
+  references: { model: 'Collectives', key: 'id' },
+  onDelete: 'SET NULL',
+  onUpdate: 'CASCADE',
+  allowNull: true,
+  description:
+    'References the collective that created the virtual card used for this order',
+});
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface
@@ -13,26 +24,27 @@ module.exports = {
           'References the collective that created this payment method',
       })
       .then(() =>
-        queryInterface.addColumn('Orders', 'UsingVirtualCardFromCollectiveId', {
-          type: Sequelize.INTEGER,
-          references: { model: 'Collectives', key: 'id' },
-          onDelete: 'SET NULL',
-          onUpdate: 'CASCADE',
-          allowNull: true,
-          description:
-            'References the collective that created the virtual card used for this order',
-        }),
-      );
+        queryInterface.addColumn(
+          'Orders',
+          ORDERS_VC_COL_NAME,
+          ordersVCColSettings(Sequelize),
+        ),
+      )
+      .then(() => {
+        queryInterface.addColumn(
+          'OrderHistories',
+          ORDERS_VC_COL_NAME,
+          ordersVCColSettings(Sequelize),
+        );
+      });
   },
 
   down: queryInterface => {
     return queryInterface
       .removeColumn('PaymentMethods', 'CreatedByCollectiveId')
+      .then(() => queryInterface.removeColumn('Orders', ORDERS_VC_COL_NAME))
       .then(() =>
-        queryInterface.removeColumn(
-          'Orders',
-          'UsingVirtualCardFromCollectiveId',
-        ),
+        queryInterface.removeColumn('OrderHistories', ORDERS_VC_COL_NAME),
       );
   },
 };
