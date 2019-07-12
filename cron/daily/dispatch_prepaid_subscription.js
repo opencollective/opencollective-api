bd17a90f095fd6576e3ea1e3498e31ee2722cb80
flickz <omoyajowo2015@gmail.com>
2019-07-12 13:47:13 +0100

    feat(prepaidTier): update daily cron for dispatching prepaid subscription

--- cron/daily/dispatch_prepaid_subscription.js
+++ cron/daily/dispatch_prepaid_subscription.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+import fetch from 'isomorphic-fetch';
 import { map } from 'bluebird';
+import { Op } from 'sequelize';
+
 import '../../server/env';
 
 import models from '../../server/models';
 import * as libPayments from '../../server/lib/payments';
+import { getNextChargeAndPeriodStartDates } from '../../server/lib/subscriptions';
 import status from '../../server/constants/order_status';
 
 async function run() {
@@ -11,6 +15,8 @@ async function run() {
   const allOrders = await models.Order.findAll({
     where: {
       status: status.ACTIVE,
+      SubscriptionId: { [Op.ne]: null },
+      deletedAt: null,
     },
     include: [
       {
@@ -18,27 +24,35 @@ async function run() {
         where: { type: 'PREPAID' },
       },
       { model: models.Collective, as: 'fromCollective' },
+      { model: models.Collective, as: 'collective' },
+      {
+        model: models.Subscription,
+        where: {
+          isActive: true,
+          deletedAt: null,
+          deactivatedAt: null,
+          activatedAt: { [Op.lte]: new Date() },
+          nextChargeDate: { [Op.lte]: new Date() },
+        },
+      },
     ],
   });
 
-  map(allOrders, order => {
+  map(allOrders, async order => {
     // Amount shareable amongst dependencies
     const shareableAmount = order.totalAmount;
-    // Dependecies reciving money
-    // - get the url from `order.custonField.jsonUrl`
-    // - fetch the dependecies
-    const sampleDependecies = [
-      { weigh: 50, opencollective: { id: 43, name: 'Apex', slug: 'apex' } },
-      { weigh: 100, opencollective: { id: 10887, name: 'Nodejs Tech', slug: 'nodejs-tech' } },
-    ];
-    const sumOfWeights = sampleDependecies.reduce((sum, dependency) => dependency.weigh + sum, 0);
-    map(sampleDependecies, async dependency => {
+    const jsonUrl = order.data.customData.jsonUrl;
+    const depRecommendation = await fetchDependcies(jsonUrl);
+    const sumOfWeights = depRecommendation.reduce((sum, dependency) => dependency.weigh + sum, 0);
+
+    map(depRecommendation, async dependency => {
       // Check if the collective is avaliable
       const collective = await models.Collective.findByPk(dependency.opencollective.id);
-      const fromHostId = await order.fromCollective.getHostCollectiveId();
       const totalAmount = computeAmount(shareableAmount, sumOfWeights, dependency.weigh);
+      const pm = await order.collective.getPaymentMethod({ service: 'opencollective', type: 'prepaid' }, false);
+
       const orderData = {
-        createdByUserId: order.createdByUserId,
+        CreatedByUserId: order.CreatedByUserId,
         FromCollectiveId: order.FromCollectiveId,
         CollectiveId: collective.id,
         quantity: order.quantity,
@@ -47,19 +61,10 @@ async function run() {
         totalAmount,
         currency: order.currency,
         status: status.PENDING,
-        paymentMethod: {
-          primary: false,
-          initalBalance: shareableAmount - totalAmount,
-          type: 'prepaid',
-          CollectiveId: order.FromCollectiveId,
-          service: 'opencollective',
-          customerId: order.FromCollectiveId.slug,
-          currency: order.currency,
-          data: { HostCollectiveId: fromHostId },
-        },
       };
+
       const orderCreated = await models.Order.create(orderData);
-      await orderCreated.setPaymentMethod(orderData.paymentMethod);
+      await orderCreated.setPaymentMethod(pm.uuid);
       try {
         await libPayments.executeOrder(
           // order.fromCollective, Order needs instance of user here not collective
@@ -74,6 +79,9 @@ async function run() {
       }
     });
     // Update the original order subscription for next charge period
+    order.Subscription = Object.assign(order.Subscription, getNextChargeAndPeriodStartDates('success', order));
+    await order.Subscription.save();
+    await order.save();
   });
 }
 
@@ -81,6 +89,10 @@ const computeAmount = (totalAmount, sumOfWeights, dependencyWeight) => {
   // Express each weight as percentage
   const percentage = (dependencyWeight / sumOfWeights) * 100;
   return Math.round((percentage / 100) * totalAmount);
+};
+
+const fetchDependcies = jsonUrl => {
+  return fetch(jsonUrl).then(res => res.json());
 };
 
 run().catch(err => {
