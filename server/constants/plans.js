3c40c5fcf11c0ab25031942981f6ed26d817e25e
François Hodierne <francois@hodierne.net>
2020-01-09 12:50:56 +0100

    feat(plans): update plans

--- server/constants/plans.js
+++ server/constants/plans.js
@@ -1,17 +1,17 @@
 const plans = {
   // Legacy Plans (automatically set for accounts created before 2020)
   'legacy-custom': {
-    hostedCollectivesLimit: 99,
+    hostedCollectivesLimit: 100,
     addedFundsLimit: null,
     hostDashboard: true,
   },
   'legacy-large': {
-    hostedCollectivesLimit: 24,
+    hostedCollectivesLimit: 25,
     addedFundsLimit: null,
     hostDashboard: true,
   },
   'legacy-medium': {
-    hostedCollectivesLimit: 9,
+    hostedCollectivesLimit: 10,
     addedFundsLimit: null,
     hostDashboard: true,
   },
@@ -27,32 +27,39 @@ const plans = {
     hostDashboard: true,
   },
   custom: {
-    hostedCollectivesLimit: 99,
+    hostedCollectivesLimit: 100,
     addedFundsLimit: null,
     hostDashboard: true,
     slug: 'network-host-plan',
-    level: 4,
+    level: 50,
   },
   large: {
-    hostedCollectivesLimit: 24,
+    hostedCollectivesLimit: 25,
     addedFundsLimit: null,
     hostDashboard: true,
     slug: 'large-host-plan',
-    level: 3,
+    level: 40,
   },
   medium: {
-    hostedCollectivesLimit: 9,
+    hostedCollectivesLimit: 10,
     addedFundsLimit: null,
     hostDashboard: true,
     slug: 'medium-host-plan',
-    level: 2,
+    level: 30,
   },
   small: {
-    hostedCollectivesLimit: 1,
+    hostedCollectivesLimit: 5,
     addedFundsLimit: null,
     hostDashboard: false,
     slug: 'small-host-plan',
-    level: 1,
+    level: 20,
+  },
+  single: {
+    hostedCollectivesLimit: 1,
+    addedFundsLimit: null,
+    hostDashboard: false,
+    slug: 'single-host-plan',
+    level: 10,
   },
   // Special plan for everyone without a plan
   default: {
