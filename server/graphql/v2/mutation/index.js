c84cb3c95121ddfbcb1926593c49b021cbf95f6a
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2020-02-18 20:24:22 +0100

    feat(GQLV2): endpoint to create expenses

--- server/graphql/v2/mutation/index.js
+++ server/graphql/v2/mutation/index.js
@@ -1,9 +1,11 @@
 import commentMutations from './CommentMutations';
 import conversationMutations from './ConversationMutations';
+import expenseMutations from './ExpenseMutations';
 
 const mutation = {
   ...commentMutations,
   ...conversationMutations,
+  ...expenseMutations,
 };
 
 export default mutation;
