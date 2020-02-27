6a72492af534a965681970823be8f6a352cd0862
vinuch <vincentedeh42@gmail.com>
2020-02-27 14:47:13 -0800

    added review changes

--- server/graphql/v1/queries.js
+++ server/graphql/v1/queries.js
@@ -217,7 +217,7 @@ const queries = {
 
       const invoice = {
         title: get(host, 'settings.invoiceTitle'),
-        extraInfo: get(host, 'settings.extraInfo'),
+        extraInfo: get(host, 'settings.invoice.extraInfo'),
         HostCollectiveId: host.id,
         slug: args.invoiceSlug,
         year,
@@ -294,7 +294,7 @@ const queries = {
 
       const invoice = {
         title: get(host, 'settings.invoiceTitle'),
-        extraInfo: get(host, 'settings.extraInfo'),
+        extraInfo: get(host, 'settings.invoice.extraInfo'),
         HostCollectiveId: host.id,
         dateFrom: dateFrom,
         dateTo: dateTo,
@@ -351,7 +351,7 @@ const queries = {
       // Generate invoice
       const invoice = {
         title: get(transaction.host, 'settings.invoiceTitle'),
-        extraInfo: get(transaction.host, 'settings.extraInvoice'),
+        extraInfo: get(transaction.host, 'settings.invoice.extraInvoice'),
         HostCollectiveId: get(transaction.host, 'id'),
         slug: `transaction-${args.transactionUuid}`,
         currency: transaction.hostCurrency,
