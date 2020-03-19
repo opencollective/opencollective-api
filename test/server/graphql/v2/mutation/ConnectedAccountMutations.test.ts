947e1c26eb3ae57a673772d9dab12f48ac9aa2ef
Leonardo Kewitz <leo@opencollective.com>
2020-03-19 10:55:19 -0300

    refact: beef up token validation

--- test/server/graphql/v2/mutation/ConnectedAccountMutations.test.ts
+++ test/server/graphql/v2/mutation/ConnectedAccountMutations.test.ts
@@ -1,9 +1,20 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
+
 import { graphqlQueryV2 } from '../../../../utils';
 import { fakeCollective, fakeUser, fakeConnectedAccount } from '../../../../test-helpers/fake-data';
+import * as transferwise from '../../../../../server/lib/transferwise';
+import * as utils from '../../../../utils';
 import models from '../../../../../server/models';
 
 describe('server/graphql/v2/mutation/ConnectedAccountMutations', () => {
+  const sandbox = sinon.createSandbox();
+  beforeEach(() => {
+    sandbox.restore();
+    sandbox.stub(transferwise, 'getProfiles').resolves();
+  });
+  beforeEach(utils.resetTestDB);
+
   describe('createConnectedAccount', () => {
     const createConnectedAccountMutation = `
       mutation createConnectedAccount($connectedAccount: ConnectedAccountCreateInput!, $account: AccountReferenceInput!) {
@@ -14,14 +25,15 @@ describe('server/graphql/v2/mutation/ConnectedAccountMutations', () => {
         }
       }
     `;
-
     let user, collective;
     beforeEach(async () => {
       user = await fakeUser();
-      collective = await fakeCollective({ admin: user.collective });
+      collective = await fakeCollective({
+        admin: user.collective,
+      });
     });
 
-    it('creates the connected account with the linked attachments', async () => {
+    it('should create a new connected account', async () => {
       const connectedAccount = {
         refreshToken: 'fakeRefreshToken',
         settings: { a: true },
@@ -33,7 +45,10 @@ describe('server/graphql/v2/mutation/ConnectedAccountMutations', () => {
 
       const result = await graphqlQueryV2(
         createConnectedAccountMutation,
-        { connectedAccount, account: { legacyId: collective.id } },
+        {
+          connectedAccount,
+          account: { legacyId: collective.id },
+        },
         user,
       );
 
@@ -44,6 +59,57 @@ describe('server/graphql/v2/mutation/ConnectedAccountMutations', () => {
 
       const createdConnectedAccount = await models.ConnectedAccount.findByPk(result.data.createConnectedAccount.id);
       expect(createdConnectedAccount.toJSON()).to.deep.include(connectedAccount);
+    });
+
+    it('should fail if token already exists', async () => {
+      const connectedAccount = {
+        token: 'fakeToken',
+        service: 'transferwise',
+      };
+
+      await graphqlQueryV2(
+        createConnectedAccountMutation,
+        {
+          connectedAccount,
+          account: { legacyId: collective.id },
+        },
+        user,
+      );
+
+      const result = await graphqlQueryV2(
+        createConnectedAccountMutation,
+        {
+          connectedAccount,
+          account: { legacyId: collective.id },
+        },
+        user,
+      );
+
+      expect(result.errors).to.exist;
+      expect(result.errors[0].originalError.name).to.equal('ValidationFailed');
+      expect(result.errors[0].message).to.include('This token is already being used');
+    });
+
+    it('should fail if token is not valid', async () => {
+      const connectedAccount = {
+        token: 'fakeToken',
+        service: 'transferwise',
+      };
+
+      (transferwise.getProfiles as sinon.stub).rejects();
+
+      const result = await graphqlQueryV2(
+        createConnectedAccountMutation,
+        {
+          connectedAccount,
+          account: { legacyId: collective.id },
+        },
+        user,
+      );
+
+      expect(result.errors).to.exist;
+      expect(result.errors[0].originalError.name).to.equal('ValidationFailed');
+      expect(result.errors[0].message).to.include('The token is not a valid TransferWise token');
     });
   });
 
@@ -59,14 +125,20 @@ describe('server/graphql/v2/mutation/ConnectedAccountMutations', () => {
     let user, collective, connectedAccount;
     beforeEach(async () => {
       user = await fakeUser();
-      collective = await fakeCollective({ admin: user.collective });
+      collective = await fakeCollective({
+        admin: user.collective,
+      });
       connectedAccount = await fakeConnectedAccount({ CollectiveId: collective.id });
     });
 
     it('should force delete the connected account', async () => {
       const result = await graphqlQueryV2(
         deleteConnectedAccountMutation,
-        { connectedAccount: { legacyId: connectedAccount.id } },
+        {
+          connectedAccount: {
+            legacyId: connectedAccount.id,
+          },
+        },
         user,
       );
 
