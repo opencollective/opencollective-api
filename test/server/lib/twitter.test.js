371d33cf0028da30a4a64083fc7d2f2c107a0faf
François Hodierne <francois@hodierne.net>
2020-03-19 09:18:47 +0100

    test(twitter): stricter test / snapshot like

--- test/server/lib/twitter.test.js
+++ test/server/lib/twitter.test.js
@@ -24,14 +24,26 @@ describe('server/lib/twitter', () => {
 
     it('with no amount spent', () => {
       const tweet = twitter.compileTweet('monthlyStats', data);
-      expect(tweet).to.not.contain('we spent $0.');
+      expect(tweet).to.equal(`In December, 2 backers joined (@bakkenbaeck, @mziehlke) - you are the best! 🙌
+
+We received $1,277 from 82 backers. Our current balance is $1,200.
+
+Top backers: @webflowapp, @dalmaer, @stickermule
+
+Thank you! 🙏`);
     });
 
     it('with amount spent', () => {
       data.totalAmountSpent = '$542';
       data.topExpenseCategories = 'engineering and travel';
       const tweet = twitter.compileTweet('monthlyStats', data);
-      expect(tweet).to.contain('we spent $542 on engineering and travel.');
+      expect(tweet).to.equal(`In December, 2 backers joined (@bakkenbaeck, @mziehlke) - you are the best! 🙌
+
+We received $1,277 from 82 backers and we spent $542 on engineering and travel. Our current balance is $1,200.
+
+Top backers: @webflowapp, @dalmaer, @stickermule
+
+Thank you! 🙏`);
     });
 
     it('with no new backer', () => {
@@ -39,21 +51,35 @@ describe('server/lib/twitter', () => {
       data.totalAmountSpent = 0;
       data.newBackersTwitterHandles = '';
       const tweet = twitter.compileTweet('monthlyStats', data);
-      expect(tweet).to.not.contain('no new backer joined');
+      expect(tweet).to.equal(`In December, we received $1,277 from 82 backers. Our current balance is $1,200.
+
+Top backers: @webflowapp, @dalmaer, @stickermule
+
+Thank you! 🙏`);
     });
 
     it('with 1 new backer', () => {
       data.totalNewBackers = 1;
       data.newBackersTwitterHandles = '';
       const tweet = twitter.compileTweet('monthlyStats', data);
-      expect(tweet).to.contain('one new backer joined');
+      expect(tweet).to
+        .equal(`In December, one new backer joined. We received $1,277 from 82 backers. Our current balance is $1,200.
+
+Top backers: @webflowapp, @dalmaer, @stickermule
+
+Thank you! 🙏`);
     });
 
     it('with long new backers list', () => {
       data.totalNewBackers = 20;
       data.newBackersTwitterHandles = '@xdamman, @piamancini, @asood123, @opencollect, @storify, @znarf, @hipsterbrown';
       const tweet = twitter.compileTweet('monthlyStats', data);
-      expect(tweet).to.not.contain('Thank you');
+      expect(tweet).to
+        .equal(`In December, 20 backers joined (@xdamman, @piamancini, @asood123, @opencollect, @storify, @znarf, @hipsterbrown) - you are the best! 🙌
+
+We received $1,277 from 82 backers. Our current balance is $1,200.
+
+Top backers: @webflowapp, @dalmaer, @stickermule`);
     });
   });
 });
