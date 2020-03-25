27dd61150c2b3bef137a93b0306d8d65a0ef391e
Cedric Lusiba <cedriclusiba@gmail.com>
2020-03-25 16:17:02 +0300

    chore(api-errors): Implement Sentry on the API  - Implement sentry for rich error reporting [resolves 2997]

--- server/middleware/error_handler.js
+++ server/middleware/error_handler.js
@@ -2,47 +2,59 @@ import { map } from 'lodash';
 
 import errors from '../lib/errors';
 import logger from '../lib/logger';
+import { Sentry } from '../sentry';
 
 /**
  * error handler of the api
  */
 export default (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
+  Sentry.withScope(scope => {
+    scope.setExtras({ ip: req.ip });
+    scope.setExtras({ headers: req.headers });
+    scope.setExtras({ body: req.body });
+    scope.setTag('error-type', 'Rest Endpoints');
+    scope.setExtras({ params: req.params });
+    Sentry.captureMessage(err);
 
-  const { name } = err;
+    if (res.headersSent) {
+      return next(err);
+    }
 
-  if (name === 'UnauthorizedError') {
-    // because of jwt-express
-    err.code = err.status;
-  }
+    const { name } = err;
+    scope.setTag('error-name', name);
 
-  res.header('Cache-Control', 'no-cache');
+    if (name === 'UnauthorizedError') {
+      // because of jwt-express
+      err.code = err.status;
+    }
 
-  // Validation error.
-  const e = name && name.toLowerCase ? name.toLowerCase() : '';
+    res.header('Cache-Control', 'no-cache');
 
-  if (e.indexOf('validation') !== -1) {
-    err = new errors.ValidationFailed(
-      null,
-      map(err.errors, e => e.path),
-      err.message,
-    );
-  } else if (e.indexOf('uniqueconstraint') !== -1) {
-    err = new errors.ValidationFailed(
-      null,
-      map(err.errors, e => e.path),
-      'Unique Constraint Error.',
-    );
-  }
+    // Validation error.
+    const e = name && name.toLowerCase ? name.toLowerCase() : '';
 
-  if (!err.code || !Number.isInteger(err.code)) {
-    const code = err.type && err.type.indexOf('Stripe') > -1 ? 400 : 500;
-    err.code = err.status || code;
-  }
+    if (e.indexOf('validation') !== -1) {
+      err = new errors.ValidationFailed(
+        null,
+        map(err.errors, e => e.path),
+        err.message,
+      );
+    } else if (e.indexOf('uniqueconstraint') !== -1) {
+      err = new errors.ValidationFailed(
+        null,
+        map(err.errors, e => e.path),
+        'Unique Constraint Error.',
+      );
+    }
 
-  logger.error(`Express Error: ${err.message}`);
+    if (!err.code || !Number.isInteger(err.code)) {
+      const code = err.type && err.type.indexOf('Stripe') > -1 ? 400 : 500;
+      err.code = err.status || code;
+      scope.setTag('code', err.code);
+    }
 
-  res.status(err.code).send({ error: err });
+    logger.error(`Express Error: ${err.message}`);
+
+    res.status(err.code).send({ error: err });
+  });
 };
