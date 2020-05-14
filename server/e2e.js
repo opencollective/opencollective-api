f8ad5cf0eefc20aa3f77c60d1244e0fc4f766a52
Leonardo Kewitz <leo@opencollective.com>
2020-05-14 19:47:30 +0000

    refact: add e2e entrypoint

--- server/e2e.js
+++ server/e2e.js
@@ -1,6 +1,8 @@
+import './index';
+
 import sinon from 'sinon';
 
-import * as github from '../../server/lib/github';
+import * as github from './lib/github';
 
 // Stubs for 20-github-e2e-create-collective
 sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub', 'faketoken').resolves();
