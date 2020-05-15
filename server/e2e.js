be564175012d1f360a7b68e7382e1edccda358fe
Leonardo Kewitz <leo@opencollective.com>
2020-05-15 17:36:02 +0000

    wip

--- server/e2e.js
+++ server/e2e.js
@@ -5,6 +5,13 @@ import sinon from 'sinon';
 
 import * as github from './lib/github';
 
-// Stubs for 20-github-e2e-create-collective
-sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
-sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
+// GitHub
+sinon.stub(github, 'checkGithubAdmin');
+github.checkGithubAdmin.withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
+github.checkGithubAdmin.withArgs('demo/dummy', 'foofoo').throws();
+github.checkGithubAdmin.resolves();
+
+sinon.stub(github, 'checkGithubStars');
+github.checkGithubStars.withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
+github.checkGithubStars.withArgs('demo/dummy', 'foofoo').throws();
+github.checkGithubStars.resolves();
