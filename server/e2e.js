cbf3ee98857b8afc8825a9f1e31327919e9176d1
Kate Beard <asouthernbellein@gmail.com>
2020-05-15 12:06:48 +0100

    Sinon resolves true

--- server/e2e.js
+++ server/e2e.js
@@ -5,5 +5,5 @@ import sinon from 'sinon';
 import * as github from './lib/github';
 
 // Stubs for 20-github-e2e-create-collective
-sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub', 'faketoken').resolves();
-sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub', 'faketoken').resolves();
+sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub', 'faketoken').resolves(true);
+sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub', 'faketoken').resolves(true);
