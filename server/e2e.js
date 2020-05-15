5cb55c7e4291a27a91400d0ceb8e5b3d9199c6b9
Kate Beard <asouthernbellein@gmail.com>
2020-05-15 13:09:10 +0100

    Proper nock args

--- server/e2e.js
+++ server/e2e.js
@@ -5,5 +5,5 @@ import sinon from 'sinon';
 import * as github from './lib/github';
 
 // Stubs for 20-github-e2e-create-collective
-sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub', 'faketoken').resolves(true);
-sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub', 'faketoken').resolves(true);
+sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
+sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub/adblockpluschrome', 'foofoo').resolves();
--- test/mocks/e2e.js
+++
@@ -1,7 +0,0 @@
-import sinon from 'sinon';
-
-import * as github from '../../server/lib/github';
-
-// Stubs for 20-github-e2e-create-collective
-sinon.stub(github, 'checkGithubAdmin').withArgs('testuseradmingithub', 'faketoken').resolves(true);
-sinon.stub(github, 'checkGithubStars').withArgs('testuseradmingithub', 'faketoken').resolves(true);
