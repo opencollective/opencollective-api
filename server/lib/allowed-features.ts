1b7e0319faef75707f3e5f5255cf256f75b7e5d2
Benjamin Piouffle <benjamin@opencollective.com>
2020-06-05 10:15:15 +0200

    feat(Collectives): Add 'Features' endpoint

--- server/lib/allowed-features.ts
+++ server/lib/allowed-features.ts
@@ -35,12 +35,49 @@ export const hasOptedOutOfFeature = (collective, feature): boolean => {
 export const hasOptedInForFeature = (collective, feature): boolean => {
   const optOutFlag = OPT_IN_FEATURE_FLAGS[feature];
   return get(collective, optOutFlag) === true;
+  [FEATURE.RECEIVE_EXPENSES]: [types.COLLECTIVE, types.EVENT],
+  [FEATURE.UPDATES]: [types.COLLECTIVE, types.ORGANIZATION],
+};
+
+/**
+ * A map of paths to retrieve the value of a feature flag from a collective
+ */
+const hasFeature = (collective, feature: FEATURE): boolean => {
+export const OPTIN_FEATURE_FLAGS = {
+  [FEATURE.CONVERSATIONS]: 'settings.features.conversations',
+  [FEATURE.COLLECTIVE_GOALS]: 'settings.collectivePage.showGoals',
+  [FEATURE.UPDATES]: 'settings.features.updates',
+  [FEATURE.TRANSFERWISE]: 'settings.features.transferwise',
+  [FEATURE.PAYPAL_PAYOUTS]: 'settings.features.paypalPayouts',
+};
+
+/**
+ * Returns true if feature is allowed for this collective type, false otherwise.
+ */
+export const isFeatureAllowedForCollectiveType = (
+  collectiveType: types,
+  isHost: boolean,
+  feature: FEATURE,
+): boolean => {
+  // Check if allowed for type
+  const allowedTypes = FeatureAllowedForTypes[feature];
+  const allowedForType = allowedTypes ? allowedTypes.includes(collectiveType) : true;
+  if (!allowedForType) {
+    return false;
+  }
+
+  // Check if allowed for hosts
+  if (feature === FEATURE.TRANSFERWISE && !isHost) {
+    return false;
+  }
+
+  return true;
 };
 
 /**
  * Check if the given feature is activated for collective.
  */
-const hasFeature = (collective, feature: FEATURE): boolean => {
+export const hasFeature = (collective, feature: FEATURE): boolean => {
   if (!collective) {
     return false;
   }
@@ -52,6 +89,31 @@ const hasFeature = (collective, feature: FEATURE): boolean => {
 
   // Check opt-out flags
   return !hasOptedOutOfFeature(collective, feature);
+  // Allow Host Collectives to receive expenses
+  if (feature === FEATURE.RECEIVE_EXPENSES && collective.isHostAccount) {
+    return true;
+  }
+
+  // Check collective type
+  if (!isFeatureAllowedForCollectiveType(collective.type, collective.isHostAccount, feature)) {
+    return false;
+  }
+
+  // Check opt-in flags
+  const activationFlag = OPTIN_FEATURE_FLAGS[feature];
+  if (activationFlag) {
+    return Boolean(get(collective, activationFlag, false));
+  }
+
+  switch (feature) {
+    case FEATURE.RECEIVE_FINANCIAL_CONTRIBUTIONS:
+      return (
+        (collective.isActive && collective.type === types.COLLECTIVE) ||
+        (collective.isHost && collective.type === types.ORGANIZATION)
+      );
+    default:
+      return true;
+  }
 };
 
 export { FEATURE };
