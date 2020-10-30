5d725fa838964cc07144ef694a2b5b034a3e40c5
Leo Kewitz <leokewitz@gmail.com>
2020-10-30 14:39:03 -0300

    feat: add worker to automatically invoice owed platform fees and tips

--- server/constants/transactions.js
+++ server/constants/transactions.js
@@ -10,3 +10,15 @@ export const FEES_ON_TOP_TRANSACTION_PROPERTIES = {
   hostCurrency: 'USD',
   currency: 'USD',
 };
+
+export const FEES_ON_TOP_SETTLEMENT_EXPENSE_PROPERTIES = {
+  FromCollectiveId: 8686,
+  PayoutMethodId: 2955,
+  payeeLocation: {
+    address: '340 S Lemon Ave #3717, Walnut, CA 91789',
+    country: 'US',
+  },
+  // Pia's account
+  UserId: 30,
+  lastEditedById: 30,
+};
