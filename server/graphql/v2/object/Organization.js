9011e857487260ce07c2ce6a4c63953b96a47505
Leo Kewitz <leokewitz@gmail.com>
2020-11-18 15:15:31 -0300

    fix: add AccountWithContributionsFields to Organizations
    Because of donations to self-hosted collectives

--- server/graphql/v2/object/Organization.js
+++ server/graphql/v2/object/Organization.js
@@ -1,17 +1,19 @@
 import { GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql';
 
 import { Account, AccountFields } from '../interface/Account';
+import { AccountWithContributions, AccountWithContributionsFields } from '../interface/AccountWithContributions';
 
 import { Host } from './Host';
 
 export const Organization = new GraphQLObjectType({
   name: 'Organization',
   description: 'This represents an Organization account',
-  interfaces: () => [Account],
+  interfaces: () => [Account, AccountWithContributions],
   isTypeOf: collective => collective.type === 'ORGANIZATION',
   fields: () => {
     return {
       ...AccountFields,
+      ...AccountWithContributionsFields,
       balance: {
         description: 'Amount of money in cents in the currency of the collective currently available to spend',
         deprecationReason: '2020/04/09 - Should not have been introduced. Use stats.balance.value',
