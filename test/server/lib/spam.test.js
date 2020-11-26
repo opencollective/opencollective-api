15d25601a4377a58b7932e468bcacf2c64bd8f8a
Leo Kewitz <leokewitz@gmail.com>
2020-11-26 10:14:23 -0300

    fix: inconsistent spam.lib.test

--- test/server/lib/spam.test.js
+++ test/server/lib/spam.test.js
@@ -14,7 +14,7 @@ describe('server/lib/spam', () => {
   let clock;
 
   before(() => {
-    clock = sinon.useFakeTimers(new Date('2020-01-01'));
+    clock = sinon.useFakeTimers(new Date('2020-01-01T00:00:00.000Z'));
   });
 
   after(() => {
