eea1fd7fba3fe3be46b5e641e0a833a7dc42172b
Benjamin Piouffle <benjamin@opencollective.com>
2021-03-11 09:32:03 +0100

    enhancement(Braintree): Apply suggestions
    Co-authored-by: Leo Kewitz <leo@opencollective.com>

--- server/constants/paymentMethods.ts
+++ server/constants/paymentMethods.ts
@@ -14,7 +14,7 @@ export enum PAYMENT_METHOD_TYPE {
   COLLECTIVE = 'collective',
   HOST = 'host',
   ADAPTIVE = 'adaptive',
-  PAYPAL = 'paypal',
+  PAYPAL = 'paypal', // Braintree service supports multiple payment methods including PayPal
   GIFT_CARD = 'giftcard',
   MANUAL = 'manual',
 }
