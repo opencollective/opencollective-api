b3760fd785fbbd5b8b9b802a87fc9c85667fca63
Benjamin Piouffle <benjamin@opencollective.com>
2021-03-16 09:45:29 +0100

    fix(PaymentMethodService): Enum values

--- server/graphql/v2/enum/PaymentMethodService.ts
+++ server/graphql/v2/enum/PaymentMethodService.ts
@@ -5,6 +5,6 @@ import { PAYMENT_METHOD_SERVICE } from '../../../constants/paymentMethods';
 export const PaymentMethodService = new GraphQLEnumType({
   name: 'PaymentMethodService',
   values: Object.keys(PAYMENT_METHOD_SERVICE).reduce((values, key) => {
-    return { ...values, [key]: {} };
+    return { ...values, [key]: { value: PAYMENT_METHOD_SERVICE[key] } };
   }, {}),
 });
