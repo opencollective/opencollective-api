46bcf419cdd8dd9d32abca4ef5c729053628f02f
Leo Kewitz <leokewitz@gmail.com>
2021-04-08 22:36:17 -0300

    refact: add methods and refactor privacy provider

--- server/lib/privacy.ts
+++ server/lib/privacy.ts
@@ -3,6 +3,7 @@
 import Axios from 'axios';
 import config from 'config';
 import Debug from 'debug';
+import { find } from 'lodash';
 
 import { Card, PagingParams, PrivacyResponse, Transaction } from '../types/privacy';
 
@@ -18,7 +19,7 @@ export const listTransactions = async (
   status: 'all' | 'approvals' | 'declines' = 'all',
 ): Promise<PrivacyResponse<Transaction[]>> => {
   const url = `/v1/transaction/${status}`;
-  debug(`${url}: ${card_token}`);
+  debug(`GET ${url}: ${card_token}`);
 
   const response = await axios.get(url, {
     headers: { Authorization: `api-key ${token}` },
@@ -28,13 +29,9 @@ export const listTransactions = async (
   return response.data;
 };
 
-export const listCards = async (
-  token: string,
-  card_token?: string,
-  paging?: PagingParams,
-): Promise<PrivacyResponse<Card[]>> => {
+export const listCards = async (token: string, card_token?: string, paging?: PagingParams): Promise<Card[]> => {
   const url = `/v1/card`;
-  debug(`${url}`);
+  debug(`GET ${url}`);
 
   const response = await axios.get(url, {
     headers: { Authorization: `api-key ${token}` },
@@ -42,4 +39,37 @@ export const listCards = async (
   });
 
   return response.data?.data;
+};
+
+export const findCard = async (token: string, cardProperties: Partial<Card>): Promise<Card> => {
+  debug(`Searching for card ${JSON.stringify(cardProperties)}`);
+  let page = 1;
+  let keepGoing = true;
+  while (keepGoing) {
+    const cards = await listCards(token, undefined, { page, page_size: 500 });
+    debug(`got page ${page} with ${cards.length} cards...`);
+    if (cards.length == 0) {
+      return undefined;
+    }
+    const card = find(cards, cardProperties);
+    if (card) {
+      keepGoing = false;
+      return card;
+    }
+    page++;
+  }
+};
+
+export const updateCard = async (
+  token: string,
+  cardProperties: Partial<Card> | { card_token: string },
+): Promise<Card> => {
+  const url = `/v1/card`;
+  debug(`PUT ${url}`);
+
+  const response = await axios.put(url, cardProperties, {
+    headers: { Authorization: `api-key ${token}` },
+  });
+
+  return response.data;
 };
