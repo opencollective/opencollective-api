26d2ce2080a31365cfb18ec5f00a31376ab23dea
Sudharaka <sudharakap@mun.ca>
2021-04-21 08:26:14 -0700

    fix: simplify the logic related to the expense creation restrictions

--- server/graphql/common/expenses.ts
+++ server/graphql/common/expenses.ts
@@ -531,11 +531,9 @@ export async function createExpense(
     throw new Unauthorized('Missing expense.collective.id');
   }
 
-  if (remoteUser.rolesByCollectiveId) {
-    const isMember = Object.keys(remoteUser.rolesByCollectiveId).includes(String(expenseData.collective.id));
-    if (expenseData.collective.settings?.disablePublicExpenseSubmission && !isMember) {
-      throw new Error('You must be a member of the collective to create new expense');
-    }
+  const isMember = Boolean(remoteUser.rolesByCollectiveId[String(expenseData.collective.id)]);
+  if (expenseData.collective.settings?.disablePublicExpenseSubmission && !isMember) {
+    throw new Error('You must be a member of the collective to create new expense');
   }
 
   const itemsData = expenseData.items;
