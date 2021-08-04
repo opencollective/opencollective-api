daaea0c88a016b7be78b61783b4f4186dfd031f8
François Hodierne <francois@hodierne.net>
2021-08-04 17:42:40 +0200

    implement thegivingblock paymentMethod

--- server/paymentProviders/index.ts
+++ server/paymentProviders/index.ts
@@ -2,6 +2,7 @@ import opencollective from './opencollective';
 import paypal from './paypal';
 import privacy from './privacy';
 import stripe from './stripe';
+import thegivingblock from './thegivingblock';
 import transferwise from './transferwise';
 
 export default {
@@ -9,5 +10,6 @@ export default {
   paypal,
   privacy,
   stripe,
+  thegivingblock,
   transferwise,
 };
--- server/paymentProviders/thegivingblock/index.js
+++ server/paymentProviders/thegivingblock/index.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
+import models from '../../models';
+
 // const baseUrl = `https://public-api.tgbwidget.com/v1`;
 const baseUrl = `https://public-api-nstaging.tgbwidget.com/v1`;
 
@@ -10,6 +12,20 @@ export async function login(login, password) {
 
   const response = await fetch(`${baseUrl}/login`, { method: 'POST', body });
   const result = await response.json();
+
+  // console.log(result);
+
+  return result.data;
+}
+
+export async function refresh(refreshToken) {
+  const body = new URLSearchParams();
+  body.set('refreshToken', refreshToken);
+
+  const response = await fetch(`${baseUrl}/refresh-tokens`, { method: 'POST', body });
+  const result = await response.json();
+
+  // console.log(result);
 
   return result.data;
 }
@@ -25,19 +41,64 @@ export async function getOrganizationsList(accessToken) {
   return result.data;
 }
 
-export async function createDepositAddress(accessToken) {
+export async function createDepositAddress(accessToken, { organizationId, pledgeAmount, pledgeCurrency } = {}) {
   const headers = {
     Authorization: `Bearer ${accessToken}`,
   };
 
   const body = new URLSearchParams();
-  body.set('organizationId', 99);
   body.set('isAnonymous', true);
-  body.set('pledgeCurrency', 'BTC');
-  body.set('pledgeAmount', '0.0001');
+  body.set('organizationId', organizationId);
+  body.set('pledgeAmount', pledgeAmount);
+  body.set('pledgeCurrency', pledgeCurrency);
 
   const response = await fetch(`${baseUrl}/deposit-address`, { method: 'POST', body, headers });
   const result = await response.json();
 
+  // console.log(result);
+
   return result.data;
 }
+
+const crypto = {
+  features: {
+    recurring: false,
+    waitToCharge: false,
+  },
+
+  processOrder: async order => {
+    // fetch hostCollectiveId
+    const host = await order.collective.getHostCollective();
+
+    // retrieve credentials
+    const account = await models.ConnectedAccount.findOne({
+      where: { CollectiveId: host.id, service: 'thegivingblock' },
+    });
+
+    // refresh credentials
+    const result = await refresh(account.data.refreshToken);
+    account.data.accessToken = result.accessToken;
+    account.data.refreshToken = result.refreshToken;
+    await account.save();
+
+    // create wallet address
+    const { depositAddress } = await createDepositAddress(account.data.accessToken, {
+      organizationId: account.organizationId,
+      pledgeAmount: order.data.pledgeAmount,
+      pledgeCurrency: order.data.pledgeCurrency,
+    });
+
+    // update payment method
+    order.paymentMethod.data = order.paymentMethod.data || {};
+    order.paymentMethod.data.depositAddress = depositAddress;
+    await order.paymentMethod.save();
+
+    return order;
+  },
+};
+
+export default {
+  types: {
+    crypto,
+  },
+};
