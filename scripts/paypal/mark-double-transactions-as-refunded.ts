e990d8d08bfa529a622b748d5f9fb4864021aae2
Benjamin Piouffle <benjamin@opencollective.com>
2021-08-20 14:42:49 +0200

    PayPal: Refund duplicate transactions (#6470)
    * fix(PayPal): Refund duplicate transactions
    
    * enhancement(PayPal/duplicates): Group by PayPalId

---
+++ scripts/paypal/mark-double-transactions-as-refunded.ts
@@ -0,0 +1,65 @@
+/**
+ * This script can be used whenever PayPal webhooks event types change to update
+ * Host's connected accounts.
+ */
+
+import '../../server/env';
+
+import logger from '../../server/lib/logger';
+import { createRefundTransaction } from '../../server/lib/payments';
+import models, { sequelize } from '../../server/models';
+
+/**
+ * Refund the wrongly recorded double transactions for PayPal payment, to make sure
+ * they will be deducted from the next settlement.
+ */
+const main = async (): Promise<void> => {
+  const doubleTransactionsInfos = await sequelize.query(
+    `
+    SELECT t."OrderId", COALESCE(t."data" -> 'paypalSale' ->> 'id', t."data" -> 'capture' ->> 'id') AS "paypalId", array_agg(DISTINCT t.id) AS "transactionIds"
+    FROM "Transactions" t 
+    INNER JOIN "PaymentMethods" pm ON t."PaymentMethodId" = pm.id
+    INNER JOIN "Orders" o ON t."OrderId" = o.id
+    WHERE pm.service = 'paypal'
+    AND t.kind = 'CONTRIBUTION'
+    AND t."type" = 'CREDIT'
+    AND t."deletedAt" IS NULL
+    AND t."isRefund" IS FALSE
+    AND t."RefundTransactionId" IS NULL
+    GROUP BY t."OrderId", COALESCE(t."data" -> 'paypalSale' ->> 'id', t."data" -> 'capture' ->> 'id')
+    HAVING count(t.id) > 1
+    ORDER BY t."OrderId" DESC
+  `,
+    { raw: true, type: sequelize.QueryTypes.SELECT },
+  );
+
+  for (const doubleTransactionInfo of doubleTransactionsInfos) {
+    const sortedTransactionIds = [...doubleTransactionInfo.transactionIds].sort();
+    const transactionIdsToRefund = sortedTransactionIds.slice(1); // Keep the first transaction
+    const transactionsToRefund = await models.Transaction.findAll({ where: { id: transactionIdsToRefund } });
+
+    if (process.env.DRY) {
+      logger.info(
+        `Would refund ${transactionsToRefund.length} transactions for order ${doubleTransactionInfo.OrderId}`,
+      );
+    } else {
+      await Promise.all(
+        transactionsToRefund.map(async transaction => {
+          await createRefundTransaction(
+            transaction,
+            transaction.paymentProcessorFeeInHostCurrency,
+            { refundedFromDoubleTransactionsScript: true },
+            null,
+          );
+        }),
+      );
+    }
+  }
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
