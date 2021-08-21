afd4b419db7d38ec1e2b8432e8cb56b9002937dc
Sudharaka <sudharakap@mun.ca>
2021-08-20 19:05:18 -0700

    fix: correct script for changing previously inserted wrong BACKER roles

--- scripts/change-event-members-from-backers-to-attendees.js
+++ scripts/change-event-members-from-backers-to-attendees.js
@@ -1,20 +1,21 @@
-import { types } from '../server/constants/collectives';
 import MemberRoles from '../server/constants/roles';
-import tiers from '../server/constants/tiers';
-import models, { Op, sequelize } from '../server/models';
+import models, { sequelize } from '../server/models';
 
 async function run() {
-  const members = await models.Member.findAll({
-    where: {
-      role: { [Op.ne]: MemberRoles.ATTENDEE },
+  const members = await sequelize.query(
+    `SELECT m.* FROM "Members" m
+      INNER JOIN "Tiers" t ON m."TierId" = t.id
+      INNER JOIN "Collectives" c ON m."CollectiveId" = c.id
+    WHERE t."type" = 'TICKET'
+      AND m."role" != 'ATTENDEE'
+      AND c."type" = 'EVENT'
+    ORDER BY "createdAt" DESC`,
+    {
+      type: sequelize.QueryTypes.SELECT,
+      model: models.Member,
+      mapToModel: true,
     },
-    include: [
-      { model: models.Tier, where: { type: tiers.TICKET }, as: 'Tier' },
-      { model: models.Collective, where: { type: types.EVENT }, as: 'collective' },
-    ],
-    order: [['createdAt', 'DESC']],
-    paranoid: false,
-  });
+  );
 
   for (const member of members) {
     await member.update({ role: MemberRoles.ATTENDEE });
