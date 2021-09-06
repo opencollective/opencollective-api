c20a6306edb93530157dc1934d722024c9008e97
François Hodierne <francois@hodierne.net>
2021-09-06 22:17:23 +0200

    debt: deprecate Platform Fees in Added Funds

--- server/graphql/v2/mutation/AddFundsMutations.ts
+++ server/graphql/v2/mutation/AddFundsMutations.ts
@@ -16,7 +16,11 @@ export const addFundsMutation = {
     amount: { type: new GraphQLNonNull(AmountInput) },
     description: { type: new GraphQLNonNull(GraphQLString) },
     hostFeePercent: { type: new GraphQLNonNull(GraphQLFloat) },
-    platformFeePercent: { type: GraphQLFloat, description: 'Can only be set if root' },
+    platformFeePercent: {
+      type: GraphQLFloat,
+      description: 'Can only be set if root',
+      deprecationReason: '2020-09-06: Platform Fees are deprecated',
+    },
   },
   resolve: async (_, args, req: express.Request): Promise<Record<string, unknown>> => {
     const account = await fetchAccountWithReference(args.account, { throwIfMissing: true });
@@ -29,8 +33,6 @@ export const addFundsMutation = {
 
     if (args.hostFeePercent < 0 || args.hostFeePercent > 100) {
       throw new ValidationFailed('hostFeePercent should be a value between 0 and 100.');
-    } else if (args.platformFeePercent < 0 || args.platformFeePercent > 100) {
-      throw new ValidationFailed('platformFeePercent should be a value between 0 and 100.');
     }
 
     return addFundsToCollectiveLegacy(
@@ -40,7 +42,6 @@ export const addFundsMutation = {
         fromCollective: fromAccount,
         description: args.description,
         hostFeePercent: args.hostFeePercent,
-        platformFeePercent: args.platformFeePercent,
       },
       req.remoteUser,
     );
