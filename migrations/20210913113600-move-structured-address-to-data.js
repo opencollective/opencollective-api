220678e9d2ea62b402365b1749741d5419664fa9
Leo Kewitz <leo@opencollective.com>
2021-09-14 10:36:30 -0300

    Update migrations/20210913113600-move-structured-address-to-data.js
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- migrations/20210913113600-move-structured-address-to-data.js
+++ migrations/20210913113600-move-structured-address-to-data.js
@@ -5,14 +5,9 @@ module.exports = {
     // Move address to data
     await queryInterface.sequelize.query(`
       UPDATE "Collectives"
-      SET "data" = JSONB_SET("data", '{address}', "settings" -> 'address')
-      WHERE "settings" -> 'address' IS NOT NULL
-    `);
-
-    // Delete address from settings
-    await queryInterface.sequelize.query(`
-      UPDATE "Collectives"
-      SET "settings" = JSONB_STRIP_NULLS(JSONB_SET("settings", '{address}', 'null', false))
+      SET
+        "data" = JSONB_SET(COALESCE("data", '{}'), '{address}', "settings" -> 'address'),
+        "settings" = "settings" #- '{address}'
       WHERE "settings" -> 'address' IS NOT NULL
     `);
   },
