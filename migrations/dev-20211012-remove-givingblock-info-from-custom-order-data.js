85e5ce5c22c623da8359a3427d77df879ca6365e
Sudharaka <sudharakap@mun.ca>
2021-10-15 07:51:28 -0700

    fix: simplify the revert migration script as pointed in code review

--- migrations/dev-20211012-remove-givingblock-info-from-custom-order-data.js
+++ migrations/dev-20211012-remove-givingblock-info-from-custom-order-data.js
@@ -14,24 +14,12 @@ module.exports = {
 
   down: async queryInterface => {
     await queryInterface.sequelize.query(`
-      UPDATE "Orders" SET data = jsonb_set(data, '{customData}', '{}', true)
-      WHERE data->>'customData' IS NULL;
-    `);
-
-    await queryInterface.sequelize.query(`
-      UPDATE "Orders" SET data = jsonb_set(data, '{customData, pledgeAmount}', to_jsonb((data->>'thegivingblock')::json->>'pledgeAmount'), true)
-      WHERE (data->>'thegivingblock')::json->>'pledgeAmount' IS NOT NULL
-        AND (data->>'thegivingblock')::json->>'pledgeCurrency' IS NOT NULL;
-    `);
-
-    await queryInterface.sequelize.query(`
-      UPDATE "Orders" SET data = jsonb_set(data, '{customData, pledgeCurrency}', to_jsonb((data->>'thegivingblock')::json->>'pledgeCurrency'), true)
-      WHERE (data->>'thegivingblock')::json->>'pledgeAmount' IS NOT NULL
-        AND (data->>'thegivingblock')::json->>'pledgeCurrency' IS NOT NULL;
-    `);
-
-    await queryInterface.sequelize.query(`
-      UPDATE "Orders" SET data = jsonb(data - 'thegivingblock');
+      UPDATE "Orders"
+      SET data = jsonb_set("data", '{customData}', "data" -> 'thegivingblock', TRUE)
+        #- '{thegivingblock, pledgeAmount}'
+        #- '{thegivingblock, pledgeCurrency}'
+      WHERE "data" -> 'thegivingblock' ->> 'pledgeAmount' IS NOT NULL
+        AND "data" -> 'thegivingblock' ->> 'pledgeCurrency' IS NOT NULL;
     `);
   },
 };
