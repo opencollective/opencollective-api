d599ff8d73104a198857d4a03b03740e71e7438e
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-11-04 09:17:51 -0700

    feat: Add `expenseTypes` to the Collective Settings List (#6740)
    * feat: add expenseTypes to the collective settings list
    
    * feat: add migration script to remove existing fundingRequest strings from settings
    
    * fix: update graphql after rebase

--- migrations/20211026074425-remove-givingblock-info-from-custom-order-data.js
+++ migrations/20211026074425-remove-givingblock-info-from-custom-order-data.js
@@ -1,7 +1,16 @@
 'use strict';
 
+import { hasCompletedMigration, removeMigration } from './lib/helpers';
 module.exports = {
   up: async queryInterface => {
+    // Migration was renamed
+    const scriptName = 'dev-20211012-remove-givingblock-info-from-custom-order-data.js';
+    if (await hasCompletedMigration(queryInterface, scriptName)) {
+      console.info(`Skipping execution of script as it's already executed: ${scriptName}`);
+      await removeMigration(queryInterface, scriptName);
+      return;
+    }
+
     await queryInterface.sequelize.query(`
       UPDATE "Orders"
       SET data = jsonb_set("data", '{thegivingblock}', jsonb_build_object('pledgeAmount', data -> 'customData' ->> 'pledgeAmount', 'pledgeCurrency', data -> 'customData' ->> 'pledgeCurrency'), TRUE)
