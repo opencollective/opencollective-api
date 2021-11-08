281b05ecd2d460d37d1d759c75c3a04a190eb8a6
Forster-Groux <groux.forster@gmail.com>
2021-11-08 11:22:06 +0100

    add webhook for Stripe Virtual Card transaction (#6783)
    * add webhook for stripe virtual card transactions
    
    * add stripe webhook logs

--- server/controllers/webhooks.ts
+++ server/controllers/webhooks.ts
@@ -8,7 +8,7 @@ import transferwiseWebhookHandler from '../paymentProviders/transferwise/webhook
 
 export async function stripeWebhook(req: Request, res: Response, next: NextFunction): Promise<void> {
   await paymentProviders.stripe
-    .webhook(req.body)
+    .webhook(req)
     .then(() => res.sendStatus(200))
     .catch(next);
 }
