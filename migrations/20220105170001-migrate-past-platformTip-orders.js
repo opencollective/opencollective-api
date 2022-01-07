c83ea8e8dcbc06341c1d73e308d51d40bc6597de
Leo Kewitz <leo@opencollective.com>
2022-01-07 15:41:19 -0300

    fix: migration of past platformTip orders to consider transaction.data.platformTipEligible

--- migrations/20220105170001-migrate-past-platformTip-orders.js
+++ migrations/20220105170001-migrate-past-platformTip-orders.js
@@ -9,9 +9,36 @@ module.exports = {
         SET
           "platformTipAmount" = ("data"->'platformFee')::int,
           "platformTipEligible" = true
-        WHERE "data"->>'isFeesOnTop' = 'true';
+        WHERE "data"->>'isFeesOnTop' = 'true' AND ("data"->'platformFee')::int > 0;
       `,
     );
+
+    await queryInterface.sequelize.query(
+      `
+        UPDATE
+          "Orders" as o
+        SET
+          "platformTipAmount" = (o."data"->'platformFee')::int,
+          "platformTipEligible" = true
+        FROM
+          "Transactions" as t
+        WHERE
+          t."OrderId" = o."id"
+          AND t."kind" = 'CONTRIBUTION'
+          AND t."type" = 'CREDIT'
+          AND t."data"->>'platformTipEligible' = 'true';
+      `,
+    );
+
+    await queryInterface.sequelize.query(`
+      UPDATE
+        "Orders"
+      SET
+        "platformTipEligible" = (data->>'isFeesOnTop')::boolean
+      WHERE
+        "id" <= 93864 AND (data->>'isFeesOnTop') IS NOT NULL
+      ;
+    `);
   },
 
   down: async () => {
