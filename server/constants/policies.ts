11b7553eec7c962fcb002f74379400659cad4f71
Leo Kewitz <leo@opencollective.com>
2022-02-02 13:48:07 -0300

    feat: add Collective policy base methods

---
+++ server/constants/policies.ts
@@ -0,0 +1,6 @@
+enum POLICIES {
+  // When enabled, the author (the user that submitted and not necessarily the benefactor) of an Expense, cannot Approve the same expense.
+  EXPENSE_AUTHOR_CANNOT_APPROVE = 'EXPENSE_AUTHOR_CANNOT_APPROVE',
+}
+
+export default POLICIES;
