67c14af41c13abc559473db197e5116c8d40d5dd
Benjamin Piouffle <benjamin@opencollective.com>
2022-02-15 15:57:12 +0100

    perf(loaders): select attributes on host query (#7163)

--- server/graphql/v2/object/Expense.js
+++ server/graphql/v2/object/Expense.js
@@ -155,10 +155,7 @@ const Expense = new GraphQLObjectType({
           if (expense.HostCollectiveId) {
             return req.loaders.Collective.byId.load(expense.HostCollectiveId);
           } else {
-            const collective = await req.loaders.Collective.byId.load(expense.CollectiveId);
-            if (collective.HostCollectiveId) {
-              return req.loaders.Collective.byId.load(collective.HostCollectiveId);
-            }
+            return req.loaders.Collective.host.load(expense.CollectiveId);
           }
         },
       },
