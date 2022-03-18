c4bf00b5bec396f47bdb7c1424c45a90642f5044
François Hodierne <francois@hodierne.net>
2022-03-18 13:56:42 +0100

    graphql(v2): implement search capabilities in GraphQL v2

--- server/graphql/v2/query/collection/AccountsQuery.ts
+++ server/graphql/v2/query/collection/AccountsQuery.ts
@@ -1,8 +1,9 @@
 import { GraphQLBoolean, GraphQLList, GraphQLNonNull, GraphQLString } from 'graphql';
 
+import { searchCollectivesInDB } from '../../../../lib/search';
 import models, { Op, sequelize } from '../../../../models';
 import { AccountCollection } from '../../collection/AccountCollection';
-import { AccountType } from '../../enum';
+import { AccountType, AccountTypeToModelMapping } from '../../enum';
 import { PaymentMethodService } from '../../enum/PaymentMethodService';
 import { CHRONOLOGICAL_ORDER_INPUT_DEFAULT_VALUE, ChronologicalOrderInput } from '../../input/ChronologicalOrderInput';
 import { CollectionArgs, CollectionReturnType } from '../../interface/Collection';
@@ -11,17 +12,26 @@ const AccountsQuery = {
   type: new GraphQLNonNull(AccountCollection),
   args: {
     ...CollectionArgs,
+    searchTerm: {
+      type: GraphQLString,
+      description: 'Search accounts related to this term based on name, description, tags, slug, and location',
+    },
     tag: {
       type: new GraphQLList(GraphQLString),
-      description: 'Only accounts that match these tags',
+      description: 'Only accounts that match these tags (ignored if used together with searchTerm)',
     },
     type: {
       type: new GraphQLList(AccountType),
-      description: 'Only accounts that match these account types',
+      description:
+        'Only return accounts that match these account types (COLLECTIVE, FUND, EVENT, PROJECT, ORGANIZATION or INDIVIDUAL)',
+    },
+    isHost: {
+      type: GraphQLBoolean,
+      description: 'Only return Fiscal Hosts accounts if true',
     },
     isActive: {
-      description: 'Only return active collectives',
       type: GraphQLBoolean,
+      description: 'Only return "active" accounts with Financial Contributions enabled if true.',
     },
     hasCustomContributionsEnabled: {
       type: GraphQLBoolean,
@@ -31,6 +41,11 @@ const AccountsQuery = {
       type: new GraphQLList(PaymentMethodService),
       description: 'Only accounts that support one of these payment services will be returned',
     },
+    skipRecentAccounts: {
+      type: GraphQLBoolean,
+      description: 'Whether to skip recent suspicious accounts (48h)',
+      defaultValue: false,
+    },
     orderBy: {
       type: new GraphQLNonNull(ChronologicalOrderInput),
       description: 'The order of results',
@@ -39,6 +54,24 @@ const AccountsQuery = {
   },
   async resolve(_: void, args): Promise<CollectionReturnType> {
     const { offset, limit } = args;
+
+    if (args.searchTerm) {
+      const cleanTerm = args.searchTerm.trim();
+
+      const extraParameters = {
+        types: args.type?.length ? args.type.map(value => AccountTypeToModelMapping[value]) : null,
+        hostCollectiveIds: null, // not supported
+        isHost: args.isHost ? true : null,
+        onlyActive: args.isActive ? true : null,
+        skipRecentAccounts: args.skipRecentAccounts,
+        hasCustomContributionsEnabled: args.hasCustomContributionsEnabled,
+      };
+
+      const [accounts, totalCount] = await searchCollectivesInDB(cleanTerm, offset, limit, extraParameters);
+
+      return { nodes: accounts, totalCount, limit, offset };
+    }
+
     const where = {};
 
     // Bind arguments
@@ -47,7 +80,7 @@ const AccountsQuery = {
     }
 
     if (args.type?.length) {
-      where['type'] = args.type;
+      where['type'] = args.type.map(value => AccountTypeToModelMapping[value]);
     }
 
     if (typeof args.isActive === 'boolean') {
