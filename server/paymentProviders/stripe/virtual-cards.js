96d7af7e6b180d3fe2b610e25d9fbaa884396f55
François Travais <francois@travais.fr>
2022-03-22 08:07:40 +0100

    refactor(virtualcard): rename virtual card `expireDate` to `expiryDate`

--- server/paymentProviders/stripe/virtual-cards.js
+++ server/paymentProviders/stripe/virtual-cards.js
@@ -12,7 +12,7 @@ import { getOrCreateVendor, getVirtualCardForTransaction, persistTransaction } f
 
 const providerName = 'stripe';
 
-export const assignCardToCollective = async (cardNumber, expireDate, cvv, name, collectiveId, host, userId) => {
+export const assignCardToCollective = async (cardNumber, expiryDate, cvv, name, collectiveId, host, userId) => {
   const connectedAccount = await host.getAccountForPaymentProvider(providerName);
 
   const stripe = getStripeClient(host.slug, connectedAccount.token);
@@ -28,8 +28,8 @@ export const assignCardToCollective = async (cardNumber, expireDate, cvv, name, 
     if (
       stripeCard.number === cardNumber &&
       stripeCard.cvc === cvv &&
-      stripeCard['exp_month'] === parseInt(expireDate.slice(0, 2)) &&
-      stripeCard['exp_year'] === parseInt(expireDate.slice(-4))
+      stripeCard['exp_month'] === parseInt(expiryDate.slice(0, 2)) &&
+      stripeCard['exp_year'] === parseInt(expiryDate.slice(-4))
     ) {
       matchingCard = stripeCard;
       break;
@@ -285,7 +285,7 @@ const createCard = (stripeCard, name, collectiveId, hostId, userId) => {
     last4: stripeCard.last4,
     privateData: {
       cardNumber: stripeCard.number,
-      expireDate: `${stripeCard['exp_month']}/${stripeCard['exp_year']}`,
+      expiryDate: `${stripeCard['exp_month']}/${stripeCard['exp_year']}`,
       cvv: stripeCard.cvc,
     },
     data: omit(stripeCard, ['number', 'cvc', 'exp_year', 'exp_month']),
