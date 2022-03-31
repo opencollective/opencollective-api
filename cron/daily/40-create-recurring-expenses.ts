c3c6df7b3e755663ab816b44a03c474559265380
Leo Kewitz <leo@opencollective.com>
2022-03-31 16:09:49 -0300

    fix: remove DRY option from cron script (#7357)

--- cron/daily/40-create-recurring-expenses.ts
+++ cron/daily/40-create-recurring-expenses.ts
@@ -3,11 +3,6 @@ import '../../server/env';
 import logger from '../../server/lib/logger';
 import models, { sequelize } from '../../server/models';
 
-const DRY = process.env.DRY !== 'false';
-if (DRY) {
-  logger.info('Running dry, changes are not going to be persisted to the DB.');
-}
-
 const run = async () => {
   const recurringExpensesDue = await models.RecurringExpense.getRecurringExpensesDue();
   logger.info(`Found ${recurringExpensesDue.length} recurring expenses due.`);
@@ -15,11 +10,9 @@ const run = async () => {
     logger.info(
       `\nRecurringExpense #${recurringExpense.id} (${recurringExpense.interval}): Last time paid ${recurringExpense.lastDraftedAt}`,
     );
-    if (!DRY) {
-      await recurringExpense.createNextExpense().catch(e => {
-        logger.error(`Error creating recurring expense #${recurringExpense.id}:`, e);
-      });
-    }
+    await recurringExpense.createNextExpense().catch(e => {
+      logger.error(`Error creating recurring expense #${recurringExpense.id}:`, e);
+    });
   }
 };
 
