1f243c000eb0f1f40b92cd0b89067290fd82f812
Leo Kewitz <leo@opencollective.com>
2022-04-01 13:46:45 -0300

    fix: improve index with deletedAt conditional
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- migrations/20220401120000-add-missing-expense-indexes.js
+++ migrations/20220401120000-add-missing-expense-indexes.js
@@ -3,10 +3,10 @@
 module.exports = {
   up: async queryInterface => {
     await queryInterface.sequelize.query(`
-      CREATE INDEX CONCURRENTLY IF NOT EXISTS expenses_collective_id ON public."Expenses" USING btree ("CollectiveId", "deletedAt");
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS expenses_collective_id ON public."Expenses" USING btree ("CollectiveId") WHERE "deletedAt" IS NULL;
     `);
     await queryInterface.sequelize.query(`
-      CREATE INDEX CONCURRENTLY IF NOT EXISTS expenses_from_collective_id ON public."Expenses" USING btree ("FromCollectiveId", "deletedAt"); 
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS expenses_from_collective_id ON public."Expenses" USING btree ("FromCollectiveId") WHERE "deletedAt" IS NULL; 
     `);
   },
 
