0b5bba10100781ba225b3bfc2b28959f8605ffe3
Leo Kewitz <leo@opencollective.com>
2022-05-10 16:44:57 -0300

    feat: add COLLECTIVE_MINIMUM_ADMINS policy

--- server/constants/policies.ts
+++ server/constants/policies.ts
@@ -1,6 +1,8 @@
 enum POLICIES {
   // When enabled, the author (the user that submitted and not necessarily the benefactor) of an Expense, cannot Approve the same expense.
   EXPENSE_AUTHOR_CANNOT_APPROVE = 'EXPENSE_AUTHOR_CANNOT_APPROVE',
+  // When enabled, restrict who can apply for fiscal host.
+  COLLECTIVE_MINIMUM_ADMINS = 'COLLECTIVE_MINIMUM_ADMINS',
 }
 
 export default POLICIES;
