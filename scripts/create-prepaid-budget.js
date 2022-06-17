f2c6668f53ed79010ada7af019927635be4023c8
François Hodierne <francois@hodierne.net>
2022-06-17 12:16:44 +0200

    small script to create prepaid budget from a transaction to the host (#7663)

---
+++ scripts/create-prepaid-budget.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import '../server/env';
+
+import { createPrepaidPaymentMethod } from '../server/lib/prepaid-budget';
+import models, { sequelize } from '../server/models';
+
+async function run() {
+  if (process.argv.length < 3) {
+    console.error('Usage: npm run script ./scripts/create-prepaid-budget.js TRANSACTION_ID');
+    process.exit(1);
+  }
+
+  const TRANSACTION_ID = process.argv[2];
+
+  const originalCreditTransaction = await models.Transaction.findByPk(TRANSACTION_ID);
+  if (originalCreditTransaction) {
+    await createPrepaidPaymentMethod(originalCreditTransaction);
+  } else {
+    console.log('Could not find original credit transaction.');
+  }
+
+  await sequelize.close();
+}
+
+run();
