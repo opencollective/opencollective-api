54f9d0459171109f4776dabd4be2758e2596c6ca
Benjamin Piouffle <benjamin@opencollective.com>
2022-06-27 14:14:40 +0200

    feat(Oauth): manage applications for account (#7649)

--- server/graphql/v2/input/ApplicationCreateInput.ts
+++ server/graphql/v2/input/ApplicationCreateInput.ts
@@ -3,6 +3,8 @@ import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
 import { ApplicationType } from '../enum';
 import URL from '../scalar/URL';
 
+import { AccountReferenceInput } from './AccountReferenceInput';
+
 export const ApplicationCreateInput = new GraphQLInputObjectType({
   name: 'ApplicationCreateInput',
   description: 'Input type for Application',
@@ -11,5 +13,9 @@ export const ApplicationCreateInput = new GraphQLInputObjectType({
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     redirectUri: { type: URL },
+    account: {
+      type: AccountReferenceInput,
+      description: 'The account to use as the owner of the application. Defaults to currently logged in user.',
+    },
   }),
 });
