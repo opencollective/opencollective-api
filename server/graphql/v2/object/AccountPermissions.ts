40af1b5d6fdb4a9373e9349b82f4a35204598642
Benjamin Piouffle <benjamin@opencollective.com>
2022-07-13 16:24:54 +0200

    feat(Account): permissions resolver (#7743)

---
+++ server/graphql/v2/object/AccountPermissions.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+
+import { checkScope } from '../../common/scope-check';
+import { getIdEncodeResolver, IDENTIFIER_TYPES } from '../identifiers';
+
+import { Permission, PermissionFields } from './Permission';
+
+const AccountPermissions = new GraphQLObjectType({
+  name: 'AccountPermissions',
+  description: 'Fields for the user permissions on an account',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: getIdEncodeResolver(IDENTIFIER_TYPES.ACCOUNT),
+    },
+    addFunds: {
+      type: new GraphQLNonNull(Permission),
+      description: 'Whether the current user can mark this order as expired',
+      resolve(collective, _, req: express.Request): PermissionFields {
+        return { allowed: checkScope(req, 'host') && req.remoteUser.isHostAdmin(collective) };
+      },
+    },
+  }),
+});
+
+export default AccountPermissions;
