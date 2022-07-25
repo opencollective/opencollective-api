1b4eeec90a28cda2bc7fbc26fd7daa5e081a401a
Leo Kewitz <leo@opencollective.com>
2022-07-25 18:13:04 -0300

    refact: setEmailNotification mutation to receive active state from the frontend
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- server/graphql/v2/mutation/ActivitySubscriptionsMutations.ts
+++ server/graphql/v2/mutation/ActivitySubscriptionsMutations.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { GraphQLNonNull } from 'graphql';
+import { GraphQLBoolean, GraphQLNonNull } from 'graphql';
 
 import Channels from '../../../constants/channels';
 import models from '../../../models';
@@ -9,15 +9,16 @@ import { AccountReferenceInput, fetchAccountWithReference } from '../input/Accou
 import { ActivitySubscription } from '../object/ActivitySubscription';
 
 const notificationMutations = {
-  toggleEmailNotification: {
+  setEmailNotification: {
     type: ActivitySubscription,
-    description: 'Toggle email notification subscription for requesting logged-in user',
+    description: 'Set email notification subscription for requesting logged-in user',
     args: {
       type: { type: new GraphQLNonNull(ActivityAndClassesType) },
       account: {
         type: AccountReferenceInput,
         description: 'Scope account which this notification preference is applied to',
       },
+      active: { type: new GraphQLNonNull(GraphQLBoolean) },
     },
     async resolve(_: void, args, req: express.Request) {
       checkRemoteUserCanUseAccount(req);
@@ -25,15 +26,7 @@ const notificationMutations = {
       const collective =
         args.account && (await fetchAccountWithReference(args.account, { loaders: req.loaders, throwIfMissing: true }));
 
-      const where = { UserId: req.remoteUser.id, type: args.type };
-      if (collective) {
-        where['CollectiveId'] = collective.id;
-      }
-
-      const existing = await models.Notification.findOne({ where });
-      const alreadyUnsubscribed = existing?.active === false;
-
-      if (alreadyUnsubscribed) {
+      if (args.active) {
         return models.Notification.subscribe(args.type, Channels.EMAIL, req.remoteUser.id, collective?.id || null);
       } else {
         return models.Notification.unsubscribe(args.type, Channels.EMAIL, req.remoteUser.id, collective?.id || null);
