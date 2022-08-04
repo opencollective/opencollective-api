c75af2ed8a74569033b01da112b2bea96f5fc22c
KT <ka14406@protonmail.com>
2022-08-04 12:02:21 -0400

    feat: add ActivitiesCollection query

---
+++ server/graphql/v2/collection/ActivityCollection.ts
@@ -0,0 +1,18 @@
+import { GraphQLList, GraphQLObjectType } from 'graphql';
+
+import { Collection, CollectionFields } from '../interface/Collection';
+import { Activity } from '../object/Activity';
+
+export const ActivityCollection = new GraphQLObjectType({
+    name: 'ActivityCollection',
+    interfaces: [Collection],
+    description: 'A collection of "Activities"',
+    fields: () => {
+      return {
+        ...CollectionFields,
+        nodes: {
+          type: new GraphQLList(Activity),
+        },
+      };
+    },
+  });
---
+++ server/graphql/v2/query/collection/ActivitiesCollectionQuery.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { GraphQLNonNull } from 'graphql';
+import models from '../../../../models';
+import { ActivityCollection } from '../../collection/ActivityCollection'
+import { AccountReferenceInput, fetchAccountWithReference } from '../../input/AccountReferenceInput';
+import { CollectionArgs } from '../../interface/Collection';
+
+const ActivitiesCollectionArgs = {
+  limit: { ...CollectionArgs.limit, defaultValue: 100 },
+  offset: CollectionArgs.offset,
+  account: {
+    type: AccountReferenceInput,
+    description: 'The account associated with the Activity'
+  }
+}
+
+const ActivitiesCollectionQuery = {
+    type: new GraphQLNonNull(ActivityCollection),
+    args: ActivitiesCollectionArgs,
+    async resolve(_: void, args, req: express.Request): Promise<any> {
+      const { offset, limit } = args;
+      const account = (args.account && (await fetchAccountWithReference(args.account)));
+      const where = {};
+      if (account?.id) {
+        where['CollectiveId'] = account.id;
+      }
+      const result = await models.Activity.findAndCountAll({ where, offset, limit });
+      return {
+        nodes: result.rows,
+        totalCount: result.count,
+        limit: args.limit,
+        offset: args.offset,
+      }
+
+    },
+  };
+  
+  export default ActivitiesCollectionQuery;
