ad7633a3307a48a55ff7bb5c8dd18dea32ab9537
Henrique <hdiniz@users.noreply.github.com>
2022-09-12 16:29:28 -0300

    Feat: add Tier mutation inputs

---
+++ server/graphql/v2/input/TierUpdateInput.ts
@@ -0,0 +1,60 @@
+import {
+  GraphQLBoolean,
+  GraphQLInputObjectType,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLString,
+} from 'graphql';
+import { GraphQLNonEmptyString } from 'graphql-scalars';
+
+import { TierAmountType, TierType } from '../enum';
+import { TierFrequency } from '../enum/TierFrequency';
+
+import { AmountInput } from './AmountInput';
+
+export const TierUpdateInput = new GraphQLInputObjectType({
+  name: 'TierUpdateInput',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The public id identifying the tier (ie: dgm9bnk8-0437xqry-ejpvzeol-jdayw5re)',
+    },
+    amount: {
+      type: new GraphQLNonNull(AmountInput),
+    },
+    name: {
+      type: GraphQLNonEmptyString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    button: {
+      type: GraphQLString,
+    },
+    goal: {
+      type: AmountInput,
+    },
+    type: {
+      type: new GraphQLNonNull(TierType),
+    },
+    amountType: {
+      type: new GraphQLNonNull(TierAmountType),
+    },
+    frequency: {
+      type: new GraphQLNonNull(TierFrequency),
+    },
+    presets: {
+      type: new GraphQLList(new GraphQLNonNull(GraphQLInt)),
+    },
+    maxQuantity: {
+      type: GraphQLInt,
+    },
+    minimumAmount: {
+      type: AmountInput,
+    },
+    useStandalonePage: {
+      type: GraphQLBoolean,
+    },
+  }),
+});
