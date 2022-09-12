47d941b3c8c0cc47d2c93696e332c5a1c30e4868
Leo Kewitz <leo@opencollective.com>
2022-09-12 15:57:40 -0300

    test: add contributionsAmount test suite

---
+++ test/server/graphql/v2/object/AccountStats.test.js
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import gqlV2 from 'fake-tag';
+
+import { fakeCollective, fakeOrder, fakeTransaction } from '../../../../test-helpers/fake-data';
+import { graphqlQueryV2, resetTestDB } from '../../../../utils';
+
+const accountQuery = gqlV2/* GraphQL */ `
+  query Account($slug: String!, $includeChildren: Boolean!) {
+    account(slug: $slug) {
+      id
+      stats {
+        contributionsAmount(includeChildren: $includeChildren) {
+          label
+          count
+          amount {
+            value
+            valueInCents
+            currency
+          }
+        }
+      }
+    }
+  }
+`;
+
+describe('server/graphql/v2/object/AccountStats', () => {
+  before(resetTestDB);
+
+  let collective, project;
+  beforeEach(async () => {
+    collective = await fakeCollective({ type: 'COLLECTIVE' });
+    await fakeTransaction({ CollectiveId: collective.id, kind: 'CONTRIBUTION', amount: 100000 });
+    const order = await fakeOrder({ CollectiveId: collective.id, amount: 50000 }, { withSubscription: true });
+    await fakeTransaction({ CollectiveId: collective.id, kind: 'CONTRIBUTION', amount: 50000, OrderId: order.id });
+
+    project = await fakeCollective({ ParentCollectiveId: collective.id, type: 'PROJECT' });
+    await fakeTransaction({ CollectiveId: project.id, kind: 'CONTRIBUTION', amount: 10000 });
+  });
+
+  describe('contributionsAmount', () => {
+    it('should return contributionsAmount for recurring vs. one-time donations', async () => {
+      const result = await graphqlQueryV2(accountQuery, { slug: collective.slug, includeChildren: false });
+      const contributionsAmount = result.data.account.stats.contributionsAmount;
+
+      expect(contributionsAmount).to.containSubset([{ label: 'one-time', amount: { value: 1000 } }]);
+      expect(contributionsAmount).to.containSubset([{ label: 'recurring', amount: { value: 500 } }]);
+    });
+
+    it('should include children collective donations', async () => {
+      const result = await graphqlQueryV2(accountQuery, { slug: collective.slug, includeChildren: true });
+      const contributionsAmount = result.data.account.stats.contributionsAmount;
+
+      expect(contributionsAmount).to.containSubset([{ label: 'one-time', amount: { value: 1100 } }]);
+      expect(contributionsAmount).to.containSubset([{ label: 'recurring', amount: { value: 500 } }]);
+    });
+
+    it('should ignore moved funds from parent and children collective', async () => {
+      await fakeTransaction(
+        { CollectiveId: project.id, FromCollectiveId: collective.id, kind: 'CONTRIBUTION', amount: 20000 },
+        { createDoubleEntry: true },
+      );
+      const result = await graphqlQueryV2(accountQuery, { slug: collective.slug, includeChildren: true });
+      const contributionsAmount = result.data.account.stats.contributionsAmount;
+
+      expect(contributionsAmount).to.containSubset([{ label: 'one-time', amount: { value: 1100 } }]);
+      expect(contributionsAmount).to.containSubset([{ label: 'recurring', amount: { value: 500 } }]);
+    });
+  });
+});
