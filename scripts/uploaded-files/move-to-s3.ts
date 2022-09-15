52f97c84239501ab9dfd1985229fb8928514715e
Benjamin Piouffle <benjamin@opencollective.com>
2022-09-15 19:26:57 +0200

    feat: Script to upload all images from longDescription to S3

---
+++ scripts/uploaded-files/move-to-s3.ts
@@ -0,0 +1,147 @@
+/**
+ * Upload some images that were linked directly in the past to S3, in order to be compliant with the new CSP.
+ * See https://github.com/opencollective/opencollective/issues/5728
+ */
+
+import { Command } from 'commander';
+import config from 'config';
+import { last } from 'lodash';
+import fetch from 'node-fetch';
+import { v1 as uuid } from 'uuid';
+
+import { uploadToS3 } from '../../server/lib/awsS3';
+import models, { Op } from '../../server/models';
+import { isValidUploadedImage } from '../../server/lib/images';
+
+const uploadImageToS3FromUrl = async url => {
+  const parsedUrl = new URL(url);
+  const ext = last(parsedUrl.pathname.split('.'));
+  if (!ext) {
+    throw new Error('Could not figure out the extension for image', url);
+  }
+
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    throw new Error('Failed to fetch');
+  }
+
+  const buffer = await response.buffer();
+  const filename = [uuid(), ext].join('.');
+  const result = await uploadToS3({
+    Bucket: config.aws.s3.bucket,
+    Key: filename,
+    Body: buffer,
+    ACL: 'public-read',
+  });
+
+  return result.Location;
+};
+
+const replaceAllExternalImages = async (content: string, options): Promise<string> => {
+  const imageMatches = content.matchAll(/<img\s.*?src=(?:'|")([^'">]+)(?:'|")/gi);
+  const externalImages = Array.from(imageMatches || [])
+    .map(match => match[1])
+    .filter(url => url.match(/\.(svg|jpg|png|gif)/i))
+    .filter(url => !isValidUploadedImage(url, { ignoreInNonProductionEnv: false }));
+
+  if (!externalImages.length) {
+    console.log('No image to replace');
+    return content;
+  }
+
+  if (!options.run) {
+    console.log(
+      `Would have replaced ${Array.from(imageMatches)
+        .map(m => m[1])
+        .join(', ')}`,
+    );
+    return content;
+  }
+
+  let newContent = content;
+  for (const url of externalImages) {
+    try {
+      // Upload image to S3
+      console.log('Uploading image', url);
+      const s3Url = await uploadImageToS3FromUrl(url);
+      // Replace image in long description
+      console.log(`Replace ${url} with ${s3Url}`);
+      newContent = newContent.replaceAll(url, s3Url);
+    } catch (e) {
+      console.error('Error while uploading image', url, e);
+    }
+  }
+
+  return newContent;
+};
+
+const main = async options => {
+  let collective;
+  if (options.collective) {
+    collective = await models.Collective.findBySlug(options.collective);
+    if (!collective) {
+      throw new Error(`Collective ${options.collective} not found`);
+    }
+  }
+
+  // Postgres regex used to find images that are not hosted on S3
+  const postgresExternalImageRegex =
+    '<img.+src="((?!https?://opencollective-production.s3.us-west-1.amazonaws.com/).+)"';
+
+  // Move from account's long descriptions
+  const collectives = await models.Collective.findAll({
+    where: {
+      id: collective ? collective.id : { [Op.ne]: null },
+      longDescription: { [Op.iRegexp]: postgresExternalImageRegex },
+    },
+  });
+
+  for (const collective of collectives) {
+    console.log('Processing collective', collective.slug);
+    const newLongDescription = await replaceAllExternalImages(collective.longDescription, options);
+    if (newLongDescription !== collective.longDescription) {
+      console.log('Updating collective', collective.slug);
+      try {
+        await collective.update({ longDescription: newLongDescription });
+      } catch (e) {
+        console.error('Error while updating collective', collective.slug, e);
+      }
+    }
+  }
+
+  // Tiers
+  const tiers = await models.Tier.findAll({
+    where: {
+      CollectiveId: collective ? collective.id : { [Op.ne]: null },
+      longDescription: { [Op.iRegexp]: postgresExternalImageRegex },
+    },
+  });
+
+  for (const tier of tiers) {
+    console.log('Processing tier', tier.slug);
+    const newLongDescription = await replaceAllExternalImages(tier.longDescription, options);
+    if (newLongDescription !== tier.longDescription) {
+      console.log('Updating tier', tier.slug);
+      try {
+        await tier.update({ longDescription: newLongDescription });
+      } catch (e) {
+        console.error('Error while updating tier', tier.slug, e);
+      }
+    }
+  }
+
+  process.exit(0);
+};
+
+const options = new Command()
+  .option('--run', 'Trigger the changes')
+  .option('--collective <collectiveSlug>', 'Only run for this collective')
+  .parse()
+  .opts();
+
+main(options)
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
