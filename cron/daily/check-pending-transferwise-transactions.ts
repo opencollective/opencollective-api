0e52db598563ad1f1a4ee68aa80a1139b0c6192d
Leo Kewitz <leo@opencollective.com>
2022-09-16 12:15:34 -0300

    refact: Wise worker and webhook consistency

--- cron/daily/check-pending-transferwise-transactions.ts
+++ cron/daily/check-pending-transferwise-transactions.ts
@@ -4,48 +4,43 @@ import '../../server/env';
 import moment from 'moment';
 import { Op } from 'sequelize';
 
-import activities from '../../server/constants/activities';
 import status from '../../server/constants/expense_status';
+import logger from '../../server/lib/logger';
 import { reportErrorToSentry } from '../../server/lib/sentry';
 import * as transferwiseLib from '../../server/lib/transferwise';
 import models from '../../server/models';
 import { PayoutMethodTypes } from '../../server/models/PayoutMethod';
 import transferwise from '../../server/paymentProviders/transferwise';
+import { handleTransferStateChange } from '../../server/paymentProviders/transferwise/webhook';
+import { TransferStateChangeEvent } from '../../server/types/transferwise';
 
 async function processExpense(expense) {
-  console.log(`\nProcessing expense #${expense.id}...`);
+  logger.info(`Processing expense #${expense.id}...`);
   const host = await expense.collective.getHostCollective();
   if (!host) {
-    throw new Error(`Could not find the host embursing the expense.`);
+    throw new Error(`Could not find the host embursing the expense #${expense.id}.`);
   }
   const [connectedAccount] = await host.getConnectedAccounts({
     where: { service: 'transferwise', deletedAt: null },
   });
   if (!connectedAccount) {
-    throw new Error(`Host is not connected to Transferwise.`);
-  }
-  const [transaction] = expense.Transactions;
-  if (!transaction) {
-    throw new Error(`Could not find any transactions associated with expense.`);
+    throw new Error(`Host #${host.id} is not connected to Transferwise.`);
   }
   const token = await transferwise.getToken(connectedAccount);
   const transfer = await transferwiseLib.getTransfer(token, expense.data.transfer.id);
-  if (transfer.status === 'processing') {
-    console.warn(`Transfer is still being processed, nothing to do but wait.`);
-  } else if (expense.status === status.PROCESSING && transfer.status === 'outgoing_payment_sent') {
-    console.log(`Transfer sent, marking expense as paid.`);
-    await expense.setPaid(expense.lastEditedById);
-    const user = await models.User.findByPk(expense.lastEditedById);
-    await expense.createActivity(activities.COLLECTIVE_EXPENSE_PAID, user);
-  } else if (transfer.status === 'funds_refunded') {
-    console.warn(`Transfer ${transfer.status}, setting status to Error and deleting existing transactions.`);
-    await models.Transaction.destroy({ where: { ExpenseId: expense.id } });
-    await expense.setError(expense.lastEditedById);
-    await expense.createActivity(activities.COLLECTIVE_EXPENSE_ERROR, null, { isSystem: true });
+  if (
+    (expense.status === status.PROCESSING && transfer.status === 'outgoing_payment_sent') ||
+    transfer.status === 'funds_refunded'
+  ) {
+    logger.info(`Wise: Transfer updated.`, transfer);
+    await handleTransferStateChange({
+      // eslint-disable-next-line camelcase
+      data: { resource: { id: transfer.id, type: 'transfer' }, current_state: transfer.status },
+    } as TransferStateChangeEvent);
   }
 }
 /**
- * Updates the status of expenses being processed through Transferwise.
+ * Updates the status of expenses being processed through Wise.
  * This process is redundant and it works as a fallback for the Webhook endpoint.
  */
 export async function run() {
@@ -62,14 +57,19 @@ export async function run() {
           },
         },
       ],
+      data: { transfer: { [Op.ne]: null } },
     },
     include: [
       { model: models.Collective, as: 'collective' },
-      { model: models.Transaction, where: { data: { transfer: { [Op.ne]: null } } } },
-      { model: models.PayoutMethod, as: 'PayoutMethod', where: { type: PayoutMethodTypes.BANK_ACCOUNT } },
+      {
+        model: models.PayoutMethod,
+        as: 'PayoutMethod',
+        required: true,
+        where: { type: PayoutMethodTypes.BANK_ACCOUNT },
+      },
     ],
   });
-  console.log(`There are ${expenses.length} TransferWise transactions to verify...`);
+  logger.info(`There are ${expenses.length} Wise transactions to verify...`);
 
   for (const expense of expenses) {
     await processExpense(expense).catch(e => {
