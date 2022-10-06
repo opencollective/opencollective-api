3ac46bfb37e7d08be2c4252effc3411c5b2f36b8
Henrique <hdiniz@users.noreply.github.com>
2022-10-06 19:04:47 -0300

    fix inmemory cache ttl

--- server/lib/cache/memory.js
+++ server/lib/cache/memory.js
@@ -7,7 +7,7 @@ const makeMemoryProvider = opts => {
     delete: async key => lruCache.delete(key),
     get: async key => lruCache.get(key),
     has: async key => lruCache.has(key),
-    set: async (key, value, expirationInSeconds) => lruCache.set(key, value, expirationInSeconds * 1000),
+    set: async (key, value, expirationInSeconds) => lruCache.set(key, value, { ttl: expirationInSeconds * 1000 }),
   };
 };
 
