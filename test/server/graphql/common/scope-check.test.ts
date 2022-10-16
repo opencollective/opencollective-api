c7c961bc7095b994330a382f391523d140f9c9a2
Nitin Kumar <snitin315@gmail.com>
2022-10-16 08:01:02 +0530

    test: add unit tests for OAuth scopes

--- test/server/graphql/common/scope-check.test.ts
+++ test/server/graphql/common/scope-check.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { checkScope, enforceScope } from '../../../../server/graphql/common/scope-check';
+import { checkRemoteUserCanRoot, checkScope, enforceScope } from '../../../../server/graphql/common/scope-check';
 import { fakeApplication, fakeUser, fakeUserToken } from '../../../test-helpers/fake-data';
 import { makeRequest, resetTestDB } from '../../../utils';
 
@@ -42,6 +42,24 @@ describe('server/graphql/v2/mutation/AccountMutations', () => {
     });
     it(`Throw error if doesn't have the scope`, async () => {
       expect(() => enforceScope(req, 'root')).to.throw(`The User Token is not allowed for operations in scope "root".`);
+    });
+  });
+  describe('checkRemoteUserCanRoot', () => {
+    it(`Execute without errors if not using OAuth (aka. if there's no req.userToken)`, async () => {
+      req.userToken = null;
+      expect(() => checkRemoteUserCanRoot(req)).to.not.throw();
+    });
+    it(`Execute without errors if the scope is allowed by the user token`, async () => {
+      const userTokenWithScopeRoot = await fakeUserToken({ scope: ['root'] });
+      req.userToken = userTokenWithScopeRoot;
+      expect(() => checkRemoteUserCanRoot(req)).to.not.throw();
+    });
+    it(`Throws when not authenticated`, async () => {
+      req.remoteUser = null;
+      expect(() => checkRemoteUserCanRoot(req)).to.throw(`You need to be logged in.`);
+    });
+    it(`Throws if the scope is not available on the token`, async () => {
+      expect(() => checkRemoteUserCanRoot(req)).to.throw(`The User Token is not allowed for operations in scope "root".`);
     });
   });
 });
