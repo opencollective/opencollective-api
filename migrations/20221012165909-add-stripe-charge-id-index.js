cfec911309701cba23c67d9c69a8edf56da8b18e
jamev <jamesv@riseup.net>
2022-10-20 12:23:25 -0400

    feat(stripe-dispute-orders): Change index to use #>> json operator which matches Sequelize query.

--- migrations/20221012165909-add-stripe-charge-id-index.js
+++ migrations/20221012165909-add-stripe-charge-id-index.js
@@ -3,12 +3,16 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(`
-      CREATE INDEX CONCURRENTLY stripe_charge_id 
-      ON "Transactions" (((data -> 'charge' ->> 'id')::text)) 
-      WHERE (data -> 'charge' ->> 'id') IS NOT NULL`);
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__stripe_charge_id"
+      ON "Transactions"
+      USING BTREE ((data#>>'{charge,id}') DESC)
+      WHERE data#>>'{charge,id}' IS NOT NULL;
+    `);
   },
 
   async down(queryInterface) {
-    await queryInterface.sequelize.query(`DROP INDEX IF EXISTS "stripe_charge_id"`);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__stripe_charge_id"
+    `);
   },
 };
