7620ca39ab28c120bb61d12aee5c69caaea2d836
Henrique <hdiniz@users.noreply.github.com>
2022-10-24 16:55:31 -0300

    Resolve policies using default policies

--- server/graphql/v2/object/Policies.ts
+++ server/graphql/v2/object/Policies.ts
@@ -1,6 +1,9 @@
 import { GraphQLBoolean, GraphQLInt, GraphQLObjectType } from 'graphql';
 
 import POLICIES from '../../../constants/policies';
+import { VirtualCardLimitIntervals } from '../../../constants/virtual-cards';
+import { getPolicy } from '../../../lib/policies';
+import { checkScope } from '../../common/scope-check';
 import { PolicyApplication } from '../enum/PolicyApplication';
 
 export const Policies = new GraphQLObjectType({
@@ -8,6 +11,11 @@ export const Policies = new GraphQLObjectType({
   fields: () => ({
     [POLICIES.EXPENSE_AUTHOR_CANNOT_APPROVE]: {
       type: GraphQLBoolean,
+      resolve(account, req) {
+        if (req.remoteUser?.isAdminOfCollective(account) && checkScope(req, 'account')) {
+          return getPolicy(account, POLICIES.EXPENSE_AUTHOR_CANNOT_APPROVE);
+        }
+      },
     },
     [POLICIES.COLLECTIVE_MINIMUM_ADMINS]: {
       type: new GraphQLObjectType({
@@ -18,6 +26,26 @@ export const Policies = new GraphQLObjectType({
           freeze: { type: GraphQLBoolean },
         }),
       }),
+      resolve(account) {
+        return getPolicy(account, POLICIES.COLLECTIVE_MINIMUM_ADMINS);
+      },
+    },
+    [POLICIES.MAXIMUM_VIRTUAL_CARD_LIMIT_AMOUNT_FOR_INTERVAL]: {
+      name: POLICIES.MAXIMUM_VIRTUAL_CARD_LIMIT_AMOUNT_FOR_INTERVAL,
+      type: new GraphQLObjectType({
+        name: POLICIES.MAXIMUM_VIRTUAL_CARD_LIMIT_AMOUNT_FOR_INTERVAL,
+        fields: () => ({
+          [VirtualCardLimitIntervals.ALL_TIME]: { type: GraphQLInt },
+          [VirtualCardLimitIntervals.DAILY]: { type: GraphQLInt },
+          [VirtualCardLimitIntervals.MONTHLY]: { type: GraphQLInt },
+          [VirtualCardLimitIntervals.PER_AUTHORIZATION]: { type: GraphQLInt },
+          [VirtualCardLimitIntervals.WEEKLY]: { type: GraphQLInt },
+          [VirtualCardLimitIntervals.YEARLY]: { type: GraphQLInt },
+        }),
+      }),
+      resolve(account) {
+        return getPolicy(account, POLICIES.MAXIMUM_VIRTUAL_CARD_LIMIT_AMOUNT_FOR_INTERVAL);
+      },
     },
   }),
 });
