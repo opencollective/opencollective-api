ae0d4729fd4949769cec1cd1ba343afdd2f3e50d
Gustav Larsson <gustav.larsson@gmail.com>
2022-10-28 16:57:21 +0200

    fix(Virtual cards): Fix available limit always showing full balance (#8120)

--- server/lib/stripe.ts
+++ server/lib/stripe.ts
@@ -92,32 +92,35 @@ export const retrieveChargeWithRefund = async (chargeId, stripeAccount) => {
   return { charge, refund, dispute };
 };
 
-export const getSpendingLimitIntervalDates = spendingLimitInterval => {
+export const getSpendingLimitIntervalDates = (spendingLimitInterval: VirtualCardLimitIntervals) => {
   const now = moment().utc(true);
-  let renewedOn, renewsOn;
 
   // Stripe spending limit intervals start on UTC midnight for daily, Sunday at midnight UTC for weekly and 1st of the month or year
   // https://stripe.com/docs/api/issuing/cards/object#issuing_card_object-spending_controls-spending_limits-interval
   switch (spendingLimitInterval) {
     case VirtualCardLimitIntervals.DAILY:
-      renewedOn = now.startOf('day');
-      renewsOn = now.add(1, 'day').startOf('day');
-      break;
+      return {
+        renewedOn: now.startOf('day').toISOString(),
+        renewsOn: now.add(1, 'day').startOf('day').toISOString(),
+      };
     case VirtualCardLimitIntervals.WEEKLY:
-      renewedOn = now.startOf('isoWeek');
-      renewsOn = now.add(1, 'week').startOf('isoWeek');
-      break;
+      return {
+        renewedOn: now.startOf('isoWeek').toISOString(),
+        renewsOn: now.add(1, 'week').startOf('isoWeek').toISOString(),
+      };
     case VirtualCardLimitIntervals.MONTHLY:
-      renewedOn = now.startOf('month');
-      renewsOn = now.add(1, 'month').startOf('month');
-      break;
+      return {
+        renewedOn: now.startOf('month').toISOString(),
+        renewsOn: now.add(1, 'month').startOf('month').toISOString(),
+      };
     case VirtualCardLimitIntervals.YEARLY:
-      renewedOn = now.startOf('year');
-      renewsOn = now.add(1, 'year').startOf('year');
-      break;
+      return {
+        renewedOn: now.startOf('year').toISOString(),
+        renewsOn: now.add(1, 'year').startOf('year').toISOString(),
+      };
     case VirtualCardLimitIntervals.ALL_TIME:
     case VirtualCardLimitIntervals.PER_AUTHORIZATION:
+    default:
+      return {};
   }
-
-  return { renewedOn: renewedOn?.toISOString(), renewsOn: renewsOn?.toISOString() };
 };
