151453eae3e2ad3bab3cefb273fd98c5782cac47
Leo Kewitz <leo@opencollective.com>
2022-12-08 15:04:27 -0300

    refact: update links to direct to manage-contributions

--- test/server/lib/url-utils.test.ts
+++ test/server/lib/url-utils.test.ts
@@ -8,16 +8,12 @@ describe('server/lib/url-utils', () => {
   describe('getEditRecurringContributionsUrl', () => {
     it('generates link for user', async () => {
       const user = await fakeUser();
-      expect(getEditRecurringContributionsUrl(user.collective)).to.equal(
-        `${config.host.website}/recurring-contributions`,
-      );
+      expect(getEditRecurringContributionsUrl(user.collective)).to.equal(`${config.host.website}/manage-contributions`);
     });
 
     it('generates link for organization', async () => {
       const org = await fakeOrganization();
-      expect(getEditRecurringContributionsUrl(org)).to.equal(
-        `${config.host.website}/${org.slug}/recurring-contributions`,
-      );
+      expect(getEditRecurringContributionsUrl(org)).to.equal(`${config.host.website}/${org.slug}/manage-contributions`);
     });
   });
 });
