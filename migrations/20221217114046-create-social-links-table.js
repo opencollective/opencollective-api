6eba5de9a6b0719e1be8446e6b9621cdd1cea1d6
Henrique <hdiniz@users.noreply.github.com>
2022-12-19 06:30:05 -0300

    Add social link models

---
+++ migrations/20221217114046-create-social-links-table.js
@@ -0,0 +1,91 @@
+'use strict';
+
+import { SocialLinkType } from '../server/models/SocialLink';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('SocialLinks', {
+      CollectiveId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Collectives', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: SocialLinkType.OTHER,
+        primaryKey: true,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      order: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+
+    await queryInterface.addIndex('SocialLinks', ['CollectiveId']);
+
+    await queryInterface.sequelize.query(`
+      INSERT INTO "SocialLinks"("CollectiveId", type, url, "order")
+      SELECT c.id, 'OTHER', trim(c."website"), 0
+      FROM "Collectives" c
+      WHERE c."deletedAt" is NULL and trim(coalesce(c."website", '')) <> ''
+      ON CONFLICT DO NOTHING
+    `);
+
+    await queryInterface.sequelize.query(`
+      INSERT INTO "SocialLinks"("CollectiveId", type, url, "order")
+      SELECT c.id, 'TWITTER', CONCAT('https://twitter.com/', trim(c."twitterHandle")), 1
+      FROM "Collectives" c
+      WHERE c."deletedAt" is NULL and trim(coalesce(c."twitterHandle", '')) <> ''
+      ON CONFLICT DO NOTHING
+    `);
+
+    await queryInterface.sequelize.query(`
+      INSERT INTO "SocialLinks"("CollectiveId", type, url, "order")
+      SELECT c.id, 'GITHUB', trim(c."repositoryUrl"), 2
+      FROM "Collectives" c
+      WHERE c."deletedAt" is NULL and c."repositoryUrl" like 'https://github.com%'
+      ON CONFLICT DO NOTHING
+    `);
+
+    await queryInterface.sequelize.query(`
+      INSERT INTO "SocialLinks"("CollectiveId", type, url, "order")
+      SELECT c.id, 'GITLAB', trim(c."repositoryUrl"), 2
+      FROM "Collectives" c
+      WHERE c."deletedAt" is NULL and c."repositoryUrl" like 'https://gitlab.com%'
+      ON CONFLICT DO NOTHING
+    `);
+
+    await queryInterface.sequelize.query(`
+      INSERT INTO "SocialLinks"("CollectiveId", type, url, "order")
+      SELECT c.id, 'GIT_REPOSITORY', trim(c."repositoryUrl"), 2
+      FROM "Collectives" c
+      WHERE c."deletedAt" is NULL and c."repositoryUrl" not like 'https://github.com%'
+      and c."repositoryUrl" not like 'https://gitlab.com%'
+      ON CONFLICT DO NOTHING
+    `);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('SocialLinks');
+  },
+};
---
+++ server/models/SocialLink.ts
@@ -0,0 +1,80 @@
+import type { CreationOptional, InferAttributes, InferCreationAttributes, NonAttribute } from 'sequelize';
+
+import sequelize, { DataTypes, Model } from '../lib/sequelize';
+
+import Collective from './Collective';
+
+export enum SocialLinkType {
+  TWITTER = 'TWITTER',
+  TUMBLR = 'TUMBLR',
+  MASTODON = 'MASTODON',
+  MATTERMOST = 'MATTERMOST',
+  FACEBOOK = 'FACEBOOK',
+  INSTAGRAM = 'INSTAGRAM',
+  DISCORD = 'DISCORD',
+  YOUTUBE = 'YOUTUBE',
+  GITHUB = 'GITHUB',
+  GITLAB = 'GITLAB',
+  GIT_REPOSITORY = 'GIT_REPOSITORY',
+  OTHER = 'OTHER',
+}
+
+class SocialLink extends Model<InferAttributes<SocialLink>, InferCreationAttributes<SocialLink>> {
+  public declare CollectiveId: number;
+  public declare type: SocialLinkType;
+  public declare url: string;
+  public declare order: number;
+  public declare createdAt: CreationOptional<Date>;
+  public declare updatedAt: CreationOptional<Date>;
+
+  public declare collective?: NonAttribute<typeof Collective>;
+}
+
+SocialLink.init(
+  {
+    CollectiveId: {
+      type: DataTypes.INTEGER,
+      references: { model: 'Collectives', key: 'id' },
+      primaryKey: true,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: SocialLinkType.OTHER,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        isUrl: {
+          msg: 'Social link URL must be a valid URL',
+        },
+      },
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'SocialLinks',
+  },
+);
+
+export default SocialLink;
