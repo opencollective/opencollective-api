f8ba44282933f6bc77da5119e7c2434792b1ccf6
Liam Considine <liam.considine@gusto.com>
2022-12-31 15:31:59 -0500

    WIP refactoring restore scripts looking for automated tests because my heroku permissions arent setup

--- scripts/db_copy_to_localhost.sh
+++ scripts/db_copy_to_localhost.sh
@@ -33,48 +33,8 @@ fi
 
 echo "DB dump saved in ${DBDUMPS_DIR}${FILENAME}"
 
-if psql "${LOCALDBNAME}" -c '\q' 2>/dev/null; then
-  echo "Dropping ${LOCALDBNAME}"
-  dropdb "${LOCALDBNAME}"
-fi
-
-echo "Creating ${LOCALDBNAME}"
-createdb "${LOCALDBNAME}"
-
 echo Restore...
-
-# The first time we run it, we will trigger FK constraints errors
-set +e
-pg_restore --no-acl -n public -O -c -d "${LOCALDBNAME}" "${DBDUMPS_DIR}${FILENAME}" 2>/dev/null
-set -e
-
-echo Restore...
-
-# So we run it twice :-)
-pg_restore --no-acl -n public -O -c -d "${LOCALDBNAME}" "${DBDUMPS_DIR}${FILENAME}"
-
-echo "DB restored to postgres://localhost/${LOCALDBNAME}"
-
-# cool trick: all stdout ignored in this block
-{
-  set +e
-  # We make sure the user $LOCALDBUSER has access; could fail
-  psql "${LOCALDBNAME}" -c "CREATE ROLE ${LOCALDBUSER} WITH login;" 2>/dev/null
-  set -e
-
-  # Change ownership of all tables
-  tables=`psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" "${LOCALDBNAME}"`
-
-  for tbl in $tables ; do
-    psql "${LOCALDBNAME}" -c "alter table \"${tbl}\" owner to ${LOCALDBUSER};"
-  done
-
-  # Change ownership of the database
-  psql "${LOCALDBNAME}" -c "alter database ${LOCALDBNAME} owner to ${LOCALDBUSER};"
-
-  psql "${LOCALDBNAME}" -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${LOCALDBUSER};"
-
-} | tee >/dev/null
+./scripts/db_restore.sh -d $LOCALDBNAME -U $LOCALDBUSER -f ${DBDUMPS_DIR}${FILENAME}
 
 echo "
 ---------
