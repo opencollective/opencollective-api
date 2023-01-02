6d34707e95f4d05128f902f87c2304509d48fbaa
Liam Considine <liam.considine@gusto.com>
2023-01-02 10:10:56 -0500

    Mistakenly took out some of the table ownership config that was not done in db_restore call

--- scripts/db_copy_to_localhost.sh
+++ scripts/db_copy_to_localhost.sh
@@ -36,6 +36,27 @@ echo "DB dump saved in ${DBDUMPS_DIR}${FILENAME}"
 echo Restore...
 ./scripts/db_restore.sh -d $LOCALDBNAME -U $LOCALDBUSER -f ${DBDUMPS_DIR}${FILENAME}
 
+# cool trick: all stdout ignored in this block
+{
+  set +e
+  # We make sure the user $LOCALDBUSER has access; could fail
+  psql "${LOCALDBNAME}" -c "CREATE ROLE ${LOCALDBUSER} WITH login;" 2>/dev/null
+  set -e
+
+  # Change ownership of all tables
+  tables=`psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" "${LOCALDBNAME}"`
+
+  for tbl in $tables ; do
+    psql "${LOCALDBNAME}" -c "alter table \"${tbl}\" owner to ${LOCALDBUSER};"
+  done
+
+  # Change ownership of the database
+  psql "${LOCALDBNAME}" -c "alter database ${LOCALDBNAME} owner to ${LOCALDBUSER};"
+
+  psql "${LOCALDBNAME}" -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${LOCALDBUSER};"
+
+} | tee >/dev/null
+
 echo "
 ---------
 All done!
