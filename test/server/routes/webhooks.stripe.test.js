c9577452b98ac923b767be7e44ddf2313a78395b
Henrique <hdiniz@users.noreply.github.com>
2023-01-06 08:16:16 -0300

    Move issuing webhook events to use each account webhook secret (#8368)
    * Move issuing webhook events to use each account webhook secret
    
    * Log unhandled issuing webhook event

--- test/server/routes/webhooks.stripe.test.js
+++ test/server/routes/webhooks.stripe.test.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import _ from 'lodash';
-import { assert, createSandbox, stub } from 'sinon';
+import { createSandbox } from 'sinon';
 import request from 'supertest';
 
 import { Service } from '../../../server/constants/connected_account';
 import app from '../../../server/index';
-import * as stripeLib from '../../../server/lib/stripe';
+import stripe from '../../../server/lib/stripe';
 import originalStripeMock from '../../mocks/stripe';
 import { fakeConnectedAccount } from '../../test-helpers/fake-data';
 import { resetTestDB } from '../../utils';
@@ -62,46 +62,20 @@ describe('server/routes/webhooks.stripe', () => {
         account: 'acc_mock',
       };
 
-      sandbox.stub(stripeLib, 'StripeCustomToken').returns({
-        webhooks: {
-          constructEvent: stub().returns(event),
-        },
-      });
+      sandbox.stub(stripe.webhooks, 'constructEvent').returns(event);
 
       await request(expressApp).post('/webhooks/stripe').send(event).expect(200);
     });
 
     it('should return HTTP 500 if event is not signed', async () => {
       const event = {
-        type: 'issuing_card.updated',
+        type: 'payment_intent.updated',
         account: 'acc_mock',
       };
 
-      sandbox.stub(stripeLib, 'StripeCustomToken').returns({
-        webhooks: {
-          constructEvent: stub().throws(new Error('bad signature')),
-        },
-      });
+      sandbox.stub(stripe.webhooks, 'constructEvent').throws(new Error('bad signature'));
 
       await request(expressApp).post('/webhooks/stripe').send(event).expect(500);
-    });
-
-    it('should return HTTP 500 if account is not recognized', async () => {
-      const event = {
-        type: 'issuing_card.updated',
-        account: 'acc_mock_test',
-      };
-
-      const constructEventMock = stub();
-      sandbox.stub(stripeLib, 'StripeCustomToken').returns({
-        webhooks: {
-          constructEvent: constructEventMock,
-        },
-      });
-
-      await request(expressApp).post('/webhooks/stripe').send(event).expect(500);
-
-      assert.notCalled(constructEventMock);
     });
   });
 });
