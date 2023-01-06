39f74a6be7c2ce76a4ef41a9e100820f1716c706
Benjamin Piouffle <benjamin@opencollective.com>
2023-01-06 11:01:55 +0100

    debt(emails): remove log in buttons (#8263)

--- server/models/Collective.js
+++ server/models/Collective.js
@@ -2014,7 +2014,6 @@ Collective.prototype.sendNewMemberEmail = async function (user, role, member, se
       recipient: {
         collective: memberUser.collective.activity,
       },
-      loginLink: `${config.host.website}/signin?next=/${memberUser.collective.slug}/admin`,
     },
     { bcc: remoteUser.email },
   );
