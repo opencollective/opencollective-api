5a53f4b8f93f95d8f1e7caba7b9593398907b767
Benjamin Piouffle <benjamin@opencollective.com>
2023-01-11 11:36:31 +0100

    fix(migrations): more resilient enrich expense paid activity

--- migrations/20230106140927-enrich-expense-paid-activities.js
+++ migrations/20230106140927-enrich-expense-paid-activities.js
@@ -4,11 +4,15 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(`
-      UPDATE "Activities"
-      SET "TransactionId" = ("data" #>> '{transaction, id}')::integer
-      WHERE "type" = 'collective.expense.paid'
-      AND "TransactionId" IS NULL
-      AND "data" #>> '{transaction, id}' IS NOT NULL
+      UPDATE "Activities" a
+      SET
+        "TransactionId" = (a."data" #>> '{transaction, id}')::integer,
+        "data" = jsonb_set(a."data", '{migration-20230106140927}', 'true')
+      FROM "Transactions" t
+      WHERE a."type" = 'collective.expense.paid'
+      AND a."TransactionId" IS NULL
+      AND a."data" #>> '{transaction, id}' IS NOT NULL
+      AND (a."data" #>> '{transaction, id}')::integer = t.id
     `);
   },
 
