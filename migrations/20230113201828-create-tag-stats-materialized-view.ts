b7240960d68177ae30a140f2cd549f333a4fd076
Gustav Larsson <gustav.larsson@gmail.com>
2023-01-19 10:24:11 +0100

    Add more filters in CollectiveTagStats materialized view for more relevant results

--- migrations/20230113201828-create-tag-stats-materialized-view.ts
+++ migrations/20230113201828-create-tag-stats-materialized-view.ts
@@ -5,7 +5,15 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(`
     CREATE MATERIALIZED VIEW IF NOT EXISTS "CollectiveTagStats" AS
-    SELECT DISTINCT unnest(tags) AS tag, COUNT(*) AS count FROM "Collectives" GROUP BY tag ORDER BY count DESC
+      SELECT DISTINCT unnest(tags) AS tag, COUNT(*) AS count 
+      FROM "Collectives"
+      WHERE "deletedAt" IS NULL
+      AND "isIncognito" IS NOT TRUE
+      AND ("data" ->> 'isGuest')::boolean IS NOT TRUE 
+      AND (COALESCE(("data"#>>'{spamReport,score}')::float, 0) <= 0.2 OR "createdAt" < (NOW() - interval '2 day'))
+      AND (("data" ->> 'hideFromSearch'::text)::boolean) IS NOT TRUE
+      GROUP BY tag 
+      ORDER BY count DESC
   `);
   },
 
