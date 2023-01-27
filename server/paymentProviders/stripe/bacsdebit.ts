a7c8385f0bbfdf1323ed2019ed34cd3b6d93761e
Henrique <hdiniz@users.noreply.github.com>
2023-01-26 21:40:37 -0300

    Add bacs debit payment method

---
+++ server/paymentProviders/stripe/bacsdebit.ts
@@ -0,0 +1,85 @@
+import config from 'config';
+import { pick, toUpper } from 'lodash';
+import type Stripe from 'stripe';
+
+import OrderStatuses from '../../constants/order_status';
+import logger from '../../lib/logger';
+import { getApplicationFee } from '../../lib/payments';
+import { reportMessageToSentry } from '../../lib/sentry';
+import stripe, { convertToStripeAmount } from '../../lib/stripe';
+import models from '../../models';
+
+import { APPLICATION_FEE_INCOMPATIBLE_CURRENCIES, refundTransaction, refundTransactionOnlyInDatabase } from './common';
+
+const processOrder = async (order: typeof models.Order): Promise<void> => {
+  const hostStripeAccount = await order.collective.getHostStripeAccount();
+  const host = await order.collective.getHostCollective();
+  const isPlatformRevenueDirectlyCollected = APPLICATION_FEE_INCOMPATIBLE_CURRENCIES.includes(toUpper(host.currency))
+    ? false
+    : host?.settings?.isPlatformRevenueDirectlyCollected ?? true;
+  const applicationFee = await getApplicationFee(order, host);
+
+  const paymentIntentParams: Stripe.PaymentIntentCreateParams = {
+    customer: order.paymentMethod.customerId,
+    currency: order.currency,
+    amount: convertToStripeAmount(order.currency, order.totalAmount),
+    description: order.description,
+    metadata: {
+      from: `${config.host.website}/${order.fromCollective.slug}`,
+      to: `${config.host.website}/${order.collective.slug}`,
+      orderId: order.id,
+    },
+    // eslint-disable-next-line camelcase
+    payment_method: order.paymentMethod?.data?.stripePaymentMethodId,
+    mandate: order.paymentMethod?.stripeMandate?.id,
+    // eslint-disable-next-line camelcase
+    payment_method_types: ['bacs_debit'],
+  };
+
+  if (applicationFee && isPlatformRevenueDirectlyCollected && hostStripeAccount.username !== config.stripe.accountId) {
+    // eslint-disable-next-line camelcase
+    paymentIntentParams.application_fee_amount = convertToStripeAmount(order.currency, applicationFee);
+  }
+
+  try {
+    let paymentIntent = await stripe.paymentIntents.create(paymentIntentParams, {
+      stripeAccount: hostStripeAccount.username,
+    });
+
+    await order.update({
+      data: { ...order.data, paymentIntent: { id: paymentIntent.id, status: paymentIntent.status } },
+    });
+
+    paymentIntent = await stripe.paymentIntents.confirm(paymentIntent.id, {
+      stripeAccount: hostStripeAccount.username,
+    });
+
+    await order.update({
+      status: OrderStatuses.PROCESSING,
+      data: { ...order.data, paymentIntent },
+    });
+
+    if (paymentIntent.status === 'processing') {
+      return;
+    } else if (paymentIntent.status !== 'succeeded') {
+      logger.error('Unknown error with Stripe Payment Intent.');
+      reportMessageToSentry('Unknown error with Stripe Payment Intent', { extra: { paymentIntent } });
+      throw new Error('Something went wrong with the payment, please contact support@opencollective.com.');
+    }
+  } catch (e) {
+    const sanitizedError = pick(e, ['code', 'message', 'requestId', 'statusCode']);
+    const errorMessage = `Error processing Stripe bacs_debit: ${e.message}`;
+    logger.error(errorMessage, sanitizedError);
+    throw new Error(errorMessage);
+  }
+};
+
+export default {
+  features: {
+    recurring: true,
+    waitToCharge: false,
+  },
+  processOrder,
+  refundTransaction,
+  refundTransactionOnlyInDatabase,
+};
--- server/paymentProviders/stripe/webhook.ts
+++ server/paymentProviders/stripe/webhook.ts
@@ -568,6 +568,9 @@ function formatPaymentMethodName(paymentMethod: Stripe.PaymentMethod) {
     case 'card': {
       return paymentMethod.card.last4;
     }
+    case PAYMENT_METHOD_TYPE.BACS_DEBIT: {
+      return `${paymentMethod.bacs_debit.sort_code} ****${paymentMethod.bacs_debit.last4}`;
+    }
     default: {
       return '';
     }
@@ -593,7 +596,7 @@ function mapStripePaymentMethodExtraData(pm: Stripe.PaymentMethod): object {
 async function paymentMethodAttached(event: Stripe.Event) {
   const stripePaymentMethod = event.data.object as Stripe.PaymentMethod;
 
-  if (!['us_bank_account', 'sepa_debit'].includes(stripePaymentMethod.type)) {
+  if (!['us_bank_account', 'sepa_debit', 'bacs_debit'].includes(stripePaymentMethod.type)) {
     return;
   }
 
