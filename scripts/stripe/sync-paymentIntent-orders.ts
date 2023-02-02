74606a3121c1eb028ebcc18a8ae98cb9f1523a79
Leo Kewitz <leo@opencollective.com>
2023-02-02 12:52:17 +0100

    feat: add script to resync stripe orders

---
+++ scripts/stripe/sync-paymentIntent-orders.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env ./node_modules/.bin/babel-node
+import '../../server/env';
+
+import { compact, toNumber } from 'lodash';
+import type Stripe from 'stripe';
+
+import stripe from '../../server/lib/stripe';
+import models from '../../server/models';
+import { paymentIntentFailed, paymentIntentSucceeded } from '../../server/paymentProviders/stripe/webhook';
+
+const IS_DRY = !!process.env.DRY;
+
+const processOrder = async order => {
+  console.log(`Processing order ${order.id}...`);
+  if (!order.data?.paymentIntent?.id) {
+    console.log(`Order ${order.id} has no paymentIntent`);
+    return;
+  }
+  const hostStripeAccount = await order.collective.getHostStripeAccount();
+  const stripeAccount = hostStripeAccount.username;
+  const paymentIntent = await stripe.paymentIntents.retrieve(order.data.paymentIntent.id, {
+    stripeAccount,
+  });
+  console.log(`Order ${order.id} paymentIntent status: ${paymentIntent.status}`);
+
+  const charge = (paymentIntent as any).charges?.data?.[0] as Stripe.Charge;
+  if (charge && paymentIntent.status === 'succeeded') {
+    console.log(`Order ${order.id} has charge: ${charge.id}`);
+    const transaction = await models.Transaction.findOne({
+      where: { OrderId: order.id, data: { charge: { id: charge.id } } },
+    });
+    if (transaction) {
+      console.log(`Order ${order.id} already processed charge ${charge.id}`);
+      return;
+    }
+
+    console.log(`Order ${order.id} is missing charge ${charge.id}, re-processing payment intent...`);
+    if (!IS_DRY) {
+      await paymentIntentSucceeded({ account: stripeAccount, data: { object: paymentIntent } } as any);
+    }
+  } else if (paymentIntent.status === 'canceled') {
+    if (!IS_DRY) {
+      console.log(`Order ${order.id} is canceled, re-processing payment intent...`);
+      await paymentIntentFailed({ account: stripeAccount, data: { object: paymentIntent } } as any);
+    }
+  }
+};
+
+const main = async () => {
+  const transactionIds = compact(process.argv.slice(2).map(toNumber));
+  for (const id of transactionIds) {
+    const order = await models.Order.findByPk(id, {
+      include: [{ model: models.Collective, as: 'collective' }],
+    });
+    await processOrder(order);
+  }
+};
+
+main()
+  .then(() => process.exit())
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
