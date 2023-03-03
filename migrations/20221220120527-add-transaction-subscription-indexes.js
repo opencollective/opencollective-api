b10e2c18385d00e8bd2d7e4ac8a1ad83547c363b
François Hodierne <francois@hodierne.net>
2023-03-03 17:20:15 +0100

    remove index

--- migrations/20221220120527-add-transaction-subscription-indexes.js
+++ migrations/20221220120527-add-transaction-subscription-indexes.js
@@ -4,11 +4,9 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.addIndex('Transactions', ['CollectiveId', 'createdAt', 'type']);
-    await queryInterface.addIndex('Subscriptions', ['isActive']);
   },
 
   async down(queryInterface) {
     await queryInterface.removeIndex('Transactions', ['CollectiveId', 'createdAt', 'type']);
-    await queryInterface.removeIndex('Subscriptions', ['isActive']);
   },
 };
