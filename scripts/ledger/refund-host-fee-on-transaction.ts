0b65f7c0bcc0248ab5276c9df1f60939a24d24ef
Benjamin Piouffle <benjamin@opencollective.com>
2023-03-22 09:33:49 +0100

    feat: Add script to refund host fees

---
+++ scripts/ledger/refund-host-fee-on-transaction.ts
@@ -0,0 +1,65 @@
+#!/usr/bin/env ./node_modules/.bin/babel-node
+import '../../server/env';
+
+import { Command } from 'commander';
+import { v4 as uuid } from 'uuid';
+
+import { refundHostFee } from '../../server/lib/payments';
+import models from '../../server/models';
+
+const DRY_RUN = process.env.DRY_RUN !== 'false';
+
+if (DRY_RUN) {
+  console.log('DRY RUN: No changes will be made');
+}
+
+const program = new Command()
+  .description('Helper to refund host fees on a given transaction')
+  .arguments('TransactionGroup UserCollectiveSlug')
+  .parse();
+
+const main = async () => {
+  const transactionGroup = program.args[0];
+  const userCollectiveSlug = program.args[1];
+  const user = await models.User.findOne({
+    include: [
+      {
+        association: 'collective',
+        attributes: [],
+        where: { slug: userCollectiveSlug },
+        required: true,
+      },
+    ],
+  });
+
+  if (!user) {
+    throw new Error(`User ${userCollectiveSlug} not found`);
+  }
+
+  // Any kind would do the job since refund is then calling `transaction.getHostFeeTransaction()` but
+  // we want to stay as close as possible to the original flow
+  const transaction = await models.Transaction.findOne({
+    where: { TransactionGroup: transactionGroup, type: 'CREDIT', kind: ['CONTRIBUTION', 'ADDED_FUNDS'] },
+  });
+
+  if (!transaction) {
+    throw new Error(`Transaction ${transactionGroup} not found`);
+  }
+
+  console.log(
+    `Refunding host fee for transaction ${transactionGroup} (${transaction.description}) on behalf of ${userCollectiveSlug}`,
+  );
+  if (!DRY_RUN) {
+    await refundHostFee(transaction, user, 0, uuid(), null);
+  }
+};
+
+// Only run script if called directly (to allow unit tests)
+if (!module.parent) {
+  main()
+    .then(() => process.exit(0))
+    .catch(e => {
+      console.error(e);
+      process.exit(1);
+    });
+}
--- server/lib/payments.js
+++ server/lib/payments.js
@@ -264,6 +264,56 @@ export const refundPaymentProcessorFeeToCollective = async (transaction, refundT
   });
 };
 
+export async function refundHostFee(transaction, user, refundedPaymentProcessorFee, transactionGroup, data) {
+  const hostFeeTransaction = await transaction.getHostFeeTransaction();
+  const buildRefund = transaction => {
+    return {
+      ...buildRefundForTransaction(transaction, user, data, refundedPaymentProcessorFee),
+      TransactionGroup: transactionGroup,
+    };
+  };
+
+  if (hostFeeTransaction) {
+    const hostFeeRefund = buildRefund(hostFeeTransaction);
+    const hostFeeRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeRefund);
+    await associateTransactionRefundId(hostFeeTransaction, hostFeeRefundTransaction, data);
+
+    // Refund Host Fee Share
+    const hostFeeShareTransaction = await transaction.getHostFeeShareTransaction();
+    if (hostFeeShareTransaction) {
+      const hostFeeShareRefund = buildRefund(hostFeeShareTransaction);
+      const hostFeeShareRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeShareRefund);
+      await associateTransactionRefundId(hostFeeShareTransaction, hostFeeShareRefundTransaction, data);
+
+      // Refund Host Fee Share Debt
+      const hostFeeShareDebtTransaction = await transaction.getHostFeeShareDebtTransaction();
+      if (hostFeeShareDebtTransaction) {
+        const hostFeeShareSettlement = await models.TransactionSettlement.findOne({
+          where: {
+            TransactionGroup: transaction.TransactionGroup,
+            kind: TransactionKind.HOST_FEE_SHARE_DEBT,
+          },
+        });
+        let hostFeeShareRefundSettlementStatus = TransactionSettlementStatus.OWED;
+        if (hostFeeShareSettlement.status === TransactionSettlementStatus.OWED) {
+          // If the Host Fee Share is not INVOICED or SETTLED, we don't need to care about recording it.
+          // Otherwise, the Host Fee Share refund will be marked as OWED and deduced from the next invoice
+          await hostFeeShareSettlement.update({ status: TransactionSettlementStatus.SETTLED });
+          hostFeeShareRefundSettlementStatus = TransactionSettlementStatus.SETTLED;
+        }
+
+        const hostFeeShareDebtRefund = buildRefund(hostFeeShareDebtTransaction);
+        const hostFeeShareDebtRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeShareDebtRefund);
+        await associateTransactionRefundId(hostFeeShareDebtTransaction, hostFeeShareDebtRefundTransaction, data);
+        await TransactionSettlement.createForTransaction(
+          hostFeeShareDebtRefundTransaction,
+          hostFeeShareRefundSettlementStatus,
+        );
+      }
+    }
+  }
+}
+
 /** Create refund transactions
  *
  * This function creates the negative transactions after refunding an
@@ -376,46 +426,7 @@ export async function createRefundTransaction(
   }
 
   // Refund Host Fee
-  const hostFeeTransaction = await transaction.getHostFeeTransaction();
-  if (hostFeeTransaction) {
-    const hostFeeRefund = buildRefund(hostFeeTransaction);
-    const hostFeeRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeRefund);
-    await associateTransactionRefundId(hostFeeTransaction, hostFeeRefundTransaction, data);
-
-    // Refund Host Fee Share
-    const hostFeeShareTransaction = await transaction.getHostFeeShareTransaction();
-    if (hostFeeShareTransaction) {
-      const hostFeeShareRefund = buildRefund(hostFeeShareTransaction);
-      const hostFeeShareRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeShareRefund);
-      await associateTransactionRefundId(hostFeeShareTransaction, hostFeeShareRefundTransaction, data);
-
-      // Refund Host Fee Share Debt
-      const hostFeeShareDebtTransaction = await transaction.getHostFeeShareDebtTransaction();
-      if (hostFeeShareDebtTransaction) {
-        const hostFeeShareSettlement = await models.TransactionSettlement.findOne({
-          where: {
-            TransactionGroup: transaction.TransactionGroup,
-            kind: TransactionKind.HOST_FEE_SHARE_DEBT,
-          },
-        });
-        let hostFeeShareRefundSettlementStatus = TransactionSettlementStatus.OWED;
-        if (hostFeeShareSettlement.status === TransactionSettlementStatus.OWED) {
-          // If the Host Fee Share is not INVOICED or SETTLED, we don't need to care about recording it.
-          // Otherwise, the Host Fee Share refund will be marked as OWED and deduced from the next invoice
-          await hostFeeShareSettlement.update({ status: TransactionSettlementStatus.SETTLED });
-          hostFeeShareRefundSettlementStatus = TransactionSettlementStatus.SETTLED;
-        }
-
-        const hostFeeShareDebtRefund = buildRefund(hostFeeShareDebtTransaction);
-        const hostFeeShareDebtRefundTransaction = await models.Transaction.createDoubleEntry(hostFeeShareDebtRefund);
-        await associateTransactionRefundId(hostFeeShareDebtTransaction, hostFeeShareDebtRefundTransaction, data);
-        await TransactionSettlement.createForTransaction(
-          hostFeeShareDebtRefundTransaction,
-          hostFeeShareRefundSettlementStatus,
-        );
-      }
-    }
-  }
+  await refundHostFee(transaction, user, refundedPaymentProcessorFee, transactionGroup);
 
   // Refund contribution
   const creditTransactionRefund = buildRefund(transaction);
