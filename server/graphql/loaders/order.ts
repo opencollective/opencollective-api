df151b6f016aa25e308aac17781f188f0636852c
JoyLubega <jollankent@gmail.com>
2023-03-28 21:03:26 +0200

    Fix prettier

--- server/graphql/loaders/order.ts
+++ server/graphql/loaders/order.ts
@@ -1,7 +1,7 @@
 import DataLoader from 'dataloader';
 
 import ACTIVITY from '../../constants/activities';
-import models, { Op} from '../../models';
+import models, { Op } from '../../models';
 import { Activity } from '../../models/Activity';
 
 import { sortResultsArray } from './helpers';
@@ -10,20 +10,18 @@ import { sortResultsArray } from './helpers';
  * Load all activities for an order
  */
 export const generateOrderActivitiesLoader = (): DataLoader<number, Activity[]> => {
-    return new DataLoader(async (orderIDs: number[]) => {
-      const activities = await models.Activity.findAll({
-        order: [['createdAt', 'ASC']],
-        where: {
-          OrderId: {
-            [Op.in]: orderIDs,
-          },
-          type: {
-            [Op.in]: [
-              ACTIVITY.COLLECTIVE_TRANSACTION_CREATED,
-            ],
-          },
+  return new DataLoader(async (orderIDs: number[]) => {
+    const activities = await models.Activity.findAll({
+      order: [['createdAt', 'ASC']],
+      where: {
+        OrderId: {
+          [Op.in]: orderIDs,
         },
-      });
-      return sortResultsArray(orderIDs, activities, activity => activity.OrderId);
+        type: {
+          [Op.in]: [ACTIVITY.COLLECTIVE_TRANSACTION_CREATED],
+        },
+      },
     });
-  };
+    return sortResultsArray(orderIDs, activities, activity => activity.OrderId);
+  });
+};
