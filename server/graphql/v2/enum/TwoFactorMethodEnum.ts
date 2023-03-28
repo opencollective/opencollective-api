384299144f1ae00e80ee086f56eed3739c35fdeb
Henrique <hdiniz@users.noreply.github.com>
2023-03-28 14:40:22 -0300

    fix impl

--- server/graphql/v2/enum/TwoFactorMethodEnum.ts
+++ server/graphql/v2/enum/TwoFactorMethodEnum.ts
@@ -10,7 +10,7 @@ export const TwoFactorMethodEnum = new GraphQLEnumType({
     .reduce((acc, key) => {
       return {
         ...acc,
-        [TwoFactorMethod[key]]: {
+        [key]: {
           value: TwoFactorMethod[key],
         },
       };
