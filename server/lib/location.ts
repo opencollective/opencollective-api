c81ef101854fb5ebd2eca5b58b512e9d36bacd99
Gustav Larsson <gustav.larsson@gmail.com>
2023-03-30 10:16:32 +0200

    Fix hasUpdatedField to only be true if a new value is set (and is not equal to old value)

--- server/lib/location.ts
+++ server/lib/location.ts
@@ -4,6 +4,6 @@ import { Location } from '../types/Location';
 
 export const mustUpdateLocation = (existingLocation: Location, newLocation: Location) => {
   const fields = ['country', 'name', 'address', 'lat', 'long', 'structured'];
-  const hasUpdatedField = field => !isEqual(newLocation?.[field], existingLocation?.[field]);
+  const hasUpdatedField = field => newLocation?.[field] && !isEqual(newLocation[field], existingLocation?.[field]);
   return fields.some(hasUpdatedField);
 };
