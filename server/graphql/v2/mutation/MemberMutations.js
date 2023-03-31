9500704572fac75795ab90eacc142c8118331781
Sudharaka <sudharakap@mun.ca>
2023-03-31 01:39:23 -0700

    fix: correct the count in removing admins from event/project

--- server/graphql/v2/mutation/MemberMutations.js
+++ server/graphql/v2/mutation/MemberMutations.js
@@ -1,6 +1,7 @@
 import { GraphQLBoolean, GraphQLNonNull, GraphQLString } from 'graphql';
 import { GraphQLDateTime } from 'graphql-scalars';
 import { pick } from 'lodash';
+import { Op } from 'sequelize';
 
 import ActivityTypes from '../../../constants/activities';
 import POLICIES from '../../../constants/policies';
@@ -238,7 +239,7 @@ const memberMutations = {
         const host = await account.getHostCollective();
         if (host) {
           const adminCount = await models.Member.count({
-            where: { CollectiveId: account.id, role: MemberRoles.ADMIN },
+            where: { CollectiveId: { [Op.or]: [account.id, account.ParentCollectiveId] }, role: MemberRoles.ADMIN },
           });
 
           const policy = await getPolicy(host, POLICIES.COLLECTIVE_MINIMUM_ADMINS);
