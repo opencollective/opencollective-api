e3639e54f25663f7a77f51659b7fa494cb97ff3c
Sudharaka Palamakumbura <sudharakap@mun.ca>
2023-04-04 10:49:45 -0700

    Correct Count When Removing Admins from Child Collectives (#8761)
    * fix: correct the count in removing admins from event/project
    
    * feat: compact array to eliminate falsy values
    
    Co-authored-by: Leo Kewitz <leo@opencollective.com>
    
    ---------
    
    Co-authored-by: Leo Kewitz <leo@opencollective.com>

--- server/graphql/v2/mutation/MemberMutations.js
+++ server/graphql/v2/mutation/MemberMutations.js
@@ -1,6 +1,7 @@
 import { GraphQLBoolean, GraphQLNonNull, GraphQLString } from 'graphql';
 import { GraphQLDateTime } from 'graphql-scalars';
-import { pick } from 'lodash';
+import { compact, pick } from 'lodash';
+import { Op } from 'sequelize';
 
 import ActivityTypes from '../../../constants/activities';
 import POLICIES from '../../../constants/policies';
@@ -238,7 +239,10 @@ const memberMutations = {
         const host = await account.getHostCollective();
         if (host) {
           const adminCount = await models.Member.count({
-            where: { CollectiveId: account.id, role: MemberRoles.ADMIN },
+            where: {
+              CollectiveId: { [Op.or]: compact([account.id, account.ParentCollectiveId]) },
+              role: MemberRoles.ADMIN,
+            },
           });
 
           const policy = await getPolicy(host, POLICIES.COLLECTIVE_MINIMUM_ADMINS);
