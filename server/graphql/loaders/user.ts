4e92973a51f9a5c9753d8b20e4f3dcdf47a968fb
Henrique <hdiniz@users.noreply.github.com>
2023-04-19 09:03:27 -0300

    Add user has 2fa loader

--- server/graphql/loaders/user.ts
+++ server/graphql/loaders/user.ts
@@ -1,6 +1,6 @@
 import DataLoader from 'dataloader';
 
-import models from '../../models';
+import models, { sequelize } from '../../models';
 import User from '../../models/User';
 
 import { sortResultsSimple } from './helpers';
@@ -11,3 +11,23 @@ export const generateUserByCollectiveIdLoader = (): DataLoader<number, User> => 
     return sortResultsSimple(collectiveIds, users, user => user.CollectiveId);
   });
 };
+
+export const userHasTwoFactorAuthEnabled: DataLoader<number, boolean> = new DataLoader(async (userIds: number[]) => {
+  const results: { id: number; hasTwoFactorAuthEnabled: boolean }[] = await sequelize.query(
+    `
+    SELECT u.id "UserId", (count(utfm.id) > 0) "hasTwoFactorAuthEnabled"
+    FROM "Users" u
+    LEFT JOIN "UserTwoFactorMethods" utfm ON u.id = utfm."UserId" AND utfm."deletedAt" IS NULL
+    WHERE u.id IN (:userIds)
+    GROUP BY u.id
+  `,
+    {
+      type: sequelize.QueryTypes.SELECT,
+      replacements: {
+        userIds,
+      },
+    },
+  );
+
+  return sortResultsSimple(userIds, results, result => result.UserId).map(result => result.hasTwoFactorAuthEnabled);
+});
