f1a3e4a57b1d1f6d7fb7f6b5cd577a8b2951f12d
Leo Kewitz <leo@opencollective.com>
2023-04-20 20:04:10 +0200

    fix: migration bugs

--- migrations/20230413114100-fix-wise-inconsistent-amountInHostCurrency-transactions.js
+++ migrations/20230413114100-fix-wise-inconsistent-amountInHostCurrency-transactions.js
@@ -6,35 +6,51 @@ module.exports = {
     await queryInterface.sequelize.query(`
       BEGIN;
       CREATE TEMP TABLE tmp_impacted_debits AS (
-        SELECT t."TransactionGroup"
-        FROM "Transactions" t
-        INNER JOIN "Expenses" e ON t."ExpenseId" = e."id"
-        WHERE t."type" = 'DEBIT'
-        AND t."RefundTransactionId" IS NULL
-        AND t."deletedAt" IS NULL
-        AND t."HostCollectiveId" != 98478 -- Ignore SCN
-        AND e.data IS NOT NULL
-        AND t.data ->> 'transfer' IS NOT NULL
-        AND (
-          (
-            -- First bug: sourceValue not a reliable source of truth
-            (e."data"#>>'{paymentOption,sourceAmount}')::FLOAT - (e.data #>> '{paymentOption,fee,total}')::FLOAT != (e."data"#>>'{transfer,sourceValue}')::FLOAT AND (
-              (
-                "feesPayer" = 'PAYEE'
-                AND t."amountInHostCurrency" = ROUND(-100 * (t."data"#>>'{transfer,sourceValue}')::FLOAT) - t."paymentProcessorFeeInHostCurrency"
-              ) OR (
-                "feesPayer" = 'COLLECTIVE'
-                AND t."amountInHostCurrency" != ROUND(-100 * (t."data" #>> '{transfer,sourceValue}')::FLOAT)
-              )
+        SELECT
+          t."TransactionGroup"
+        FROM
+          "Transactions" t
+        INNER JOIN "Expenses" e ON
+          t."ExpenseId" = e."id"
+        WHERE
+          t."type" = 'DEBIT'
+          AND t."RefundTransactionId" IS NULL
+          AND t."deletedAt" IS NULL
+          AND t."HostCollectiveId" != 98478
+          -- Ignore SCN
+          AND e.data IS NOT NULL
+          AND t.data ->> 'transfer' IS NOT NULL
+          AND (
+            (
+              -- First bug: sourceValue not a reliable source of truth
+            (
+                e."data"#>>'{paymentOption,sourceAmount}'
+              )::FLOAT - (
+                e.data #>> '{paymentOption,fee,total}'
+              )::FLOAT != (
+                e."data"#>>'{transfer,sourceValue}'
+              )::FLOAT
+                AND (
+                  (
+                    "feesPayer" = 'PAYEE'
+                      AND t."amountInHostCurrency" = ROUND(-100 * (t."data"#>>'{transfer,sourceValue}')::FLOAT) - t."paymentProcessorFeeInHostCurrency"
+                  )
+                    OR (
+                      "feesPayer" = 'COLLECTIVE'
+                        AND t."amountInHostCurrency" != ROUND(-100 * (t."data" #>> '{transfer,sourceValue}')::FLOAT)
+                    )
+                )
             )
-          ) OR (
-            -- Expenses impacted by the feesPayer=PAYEE bug
-            e."feesPayer" = 'PAYEE'
-            AND ROUND(-100 * (e."data" #>> '{paymentOption,sourceAmount}')::FLOAT) > e.amount
+              OR (
+                -- Expenses impacted by the feesPayer=PAYEE bug
+                e."feesPayer" = 'PAYEE'
+                  AND ROUND(-100 * (e."data" #>> '{paymentOption,sourceAmount}')::FLOAT) > e.amount
+              )
           )
-        )
-        GROUP BY t.id
-        ORDER BY t.id DESC
+        GROUP BY
+          t.id
+        ORDER BY
+          t.id DESC
       );
 
 
@@ -47,10 +63,11 @@ module.exports = {
         AND "deletedAt" IS NULL
       );
 
-      UPDATE "Transactions"
-        "amountInHostCurrency" = ROUND(((e.data #>> '{paymentOption,sourceAmount}')::FLOAT - (e.data #>> '{paymentOption,fee,total}')::FLOAT) * -100.0),
+      UPDATE "Transactions" SET
+        "amountInHostCurrency" = ROUND(((e.data#>>'{paymentOption,sourceAmount}')::FLOAT - (e.data #>> '{paymentOption,fee,total}')::FLOAT) * -100.0),
         "amount" = ROUND(ROUND(((e.data #>> '{paymentOption,sourceAmount}')::FLOAT - (e.data #>> '{paymentOption,fee,total}')::FLOAT) * -100.0) / t."hostCurrencyFxRate"),
         "netAmountInCollectiveCurrency" = ROUND((e.data #>> '{paymentOption,sourceAmount}')::FLOAT * -100 / t."hostCurrencyFxRate"),
+        "paymentProcessorFeeInHostCurrency" = ROUND((e.data #>> '{paymentOption,fee,total}')::FLOAT * -100),
         "data" = jsonb_set(
           t.data,
           '{expenseToHostFxRate}',
@@ -58,6 +75,7 @@ module.exports = {
         ) || jsonb_build_object('fieldsBeforeMigration20230413114100', jsonb_build_object(
           'netAmountInCollectiveCurrency', t."netAmountInCollectiveCurrency",
           'amountInHostCurrency', t."amountInHostCurrency",
+          'paymentProcessorFeeInHostCurrency', t."paymentProcessorFeeInHostCurrency",
           'amount', t.amount,
           'expenseToHostFxRate', t.data -> 'expenseToHostFxRate'
         ))
@@ -67,11 +85,12 @@ module.exports = {
         e."id" = t."ExpenseId"
       WHERE
         "Transactions"."id" = t."id" AND t.type = 'DEBIT'; 
-      
-      UPDATE "Transactions"
+
+      UPDATE "Transactions" SET
         "amount" = ROUND((e.data #>> '{paymentOption,sourceAmount}')::FLOAT * 100 / t."hostCurrencyFxRate"),
-        "amountInHostCurrency" = ROUND(ROUND((e.data #>> '{paymentOption,sourceAmount}')::FLOAT * 100)),
+        "amountInHostCurrency" = ROUND(ROUND((e.data #>> '{paymentOption,sourceAmount}')::FLOAT * 100 * COALESCE(t."hostCurrencyFxRate", 1))),
         "netAmountInCollectiveCurrency" = ROUND(ROUND(((e.data #>> '{paymentOption,sourceAmount}')::FLOAT - (e.data #>> '{paymentOption,fee,total}')::FLOAT) * 100.0) / t."hostCurrencyFxRate"),
+        "paymentProcessorFeeInHostCurrency" = ROUND((e.data #>> '{paymentOption,fee,total}')::FLOAT * -100 * COALESCE((t."data"->'oppositeTransactionHostCurrencyFxRate')::FLOAT, 1)),
         "data" = jsonb_set(
           t.data,
           '{expenseToHostFxRate}',
@@ -79,6 +98,7 @@ module.exports = {
         ) || jsonb_build_object('fieldsBeforeMigration20230413114100', jsonb_build_object(
           'netAmountInCollectiveCurrency', t."netAmountInCollectiveCurrency",
           'amountInHostCurrency', t."amountInHostCurrency",
+          'paymentProcessorFeeInHostCurrency', t."paymentProcessorFeeInHostCurrency",
           'amount', t.amount,
           'expenseToHostFxRate', t.data -> 'expenseToHostFxRate'
         ))
@@ -99,6 +119,7 @@ module.exports = {
       SET
         "amount" = (data -> 'fieldsBeforeMigration20230413114100' ->> 'amount')::integer,
         "amountInHostCurrency" = (data -> 'fieldsBeforeMigration20230413114100' ->> 'amountInHostCurrency')::integer,
+        "paymentProcessorFeeInHostCurrency" = (data -> 'fieldsBeforeMigration20230413114100' ->> 'paymentProcessorFeeInHostCurrency')::integer,
         "netAmountInCollectiveCurrency" = (data -> 'fieldsBeforeMigration20230413114100' ->> 'netAmountInCollectiveCurrency')::integer,
         "data" = (t.data - 'fieldsBeforeMigration20230413114100') || jsonb_build_object('expenseToHostFxRate', data -> 'fieldsBeforeMigration20230413114100' -> 'expenseToHostFxRate')
       WHERE "data" -> 'fieldsBeforeMigration20230413114100' IS NOT NULL;
