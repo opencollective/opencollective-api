faa8d4f5453aaf0f585ecd962984e73a8c502cbc
Leo Kewitz <leo@opencollective.com>
2023-04-24 18:54:52 +0200

    fix: memory cache reset function call

--- server/lib/cache/memory.js
+++ server/lib/cache/memory.js
@@ -3,7 +3,7 @@ import { LRUCache } from 'lru-cache';
 const makeMemoryProvider = opts => {
   const cache = new LRUCache(opts);
   return {
-    clear: async () => cache.reset(),
+    clear: async () => cache.clear(),
     delete: async key => cache.delete(key),
     get: async key => cache.get(key),
     has: async key => cache.has(key),
