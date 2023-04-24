05086c0d591696be515b3a5613b0cb6a870afbad
Leo Kewitz <leo@opencollective.com>
2023-04-24 09:53:49 +0200

    Fix: Migrate Wise transactions affected by #8825 (#8837)
    * chore: migrate Wise transactions
    
    * enhancement(scripts): add options to validate ledger
    
    * fix: disable postgis in db_copy_to_localhost script
    
    * fix: migration bugs
    
    * fix: ignore OCNZ settlements bug
    
    * refact: apply suggestions from code review
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>
    
    ---------
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- scripts/ledger/validate-transactions.ts
+++ scripts/ledger/validate-transactions.ts
@@ -11,23 +11,41 @@ import { uniq } from 'lodash';
 
 import models, { Op } from '../../server/models';
 
-const program = new Command().description('Helper to validate transactions').arguments('TransactionIds').parse();
+const program = new Command()
+  .description('Helper to validate transactions')
+  .option('--data <dataKey>', 'Filter by data, ex: --data myFieldInData')
+  .option('--group <transactionGroup>', 'Filter by transaction group')
+  .option('--id <transactionIds>', 'Filter by transaction id')
+  .parse();
 
 const main = async () => {
-  const transactionIds = uniq(program.args[0].split(','));
-  const transactions = await models.Transaction.findAll({ where: { id: { [Op.in]: transactionIds } } });
-  if (transactions.length !== transactionIds.length) {
-    throw new Error('Missing transactions');
+  const options = program.opts();
+  const where = {};
+  if (options.data) {
+    where['data'] = { [options.data]: { [Op.ne]: null } };
+  } else if (options.group) {
+    where['TransactionGroup'] = uniq(options.group.split(','));
+  } else if (options.ids) {
+    where['id'] = uniq(program.args[0].split(','));
+  } else {
+    throw new Error('Missing filtering option');
+  }
+
+  const transactions = await models.Transaction.findAll({ where });
+  if (!transactions.length) {
+    console.log('No transaction to validate');
+    return;
   }
 
   for (const transaction of transactions) {
     try {
       await models.Transaction.validate(transaction);
-      console.log(`Transaction #${transaction.id} is valid`);
     } catch (e) {
       console.error(`Transaction #${transaction.id} is invalid: ${e.message}`);
     }
   }
+
+  console.log(`Done! Validated ${transactions.length} transactions`);
 };
 
 // Only run script if called directly (to allow unit tests)
