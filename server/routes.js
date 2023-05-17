f92bc797ff726e954714af18a18910c76c0e0807
François Hodierne <francois@hodierne.net>
2023-05-17 12:27:52 +0200

    config: disable persistedQueries following Apollo warning in production (#8919)

--- server/routes.js
+++ server/routes.js
@@ -247,6 +247,7 @@ export default async app => {
 
   const graphqlServerOptions = {
     introspection: true,
+    persistedQueries: false,
     playground: isDevelopment,
     csrfPrevention: { requestHeaders: ['Authorization'] },
     ...graphqlProtection,
