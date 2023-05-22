2614ec9d9edbb0f86e63a56d456f0d169046904b
Bryan Elee <rxbryn@gmail.com>
2023-05-22 09:23:16 +0100

    typescript: convert Application to typescript (#8918)
    * typescript: convert Application to typescript
    
    * chore(Application): remove code comments
    
    ---------
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- server/models/Application.ts
+++ server/models/Application.ts
@@ -1,13 +1,59 @@
 import { randomBytes } from 'crypto';
 
 import { isNil, merge } from 'lodash';
-import { DataTypes } from 'sequelize';
+import type { CreationOptional, ForeignKey, InferAttributes, InferCreationAttributes, NonAttribute } from 'sequelize';
 
 import { crypto } from '../lib/encryption';
-import sequelize from '../lib/sequelize';
+import sequelize, { DataTypes, Model } from '../lib/sequelize';
 
-const Application = sequelize.define(
-  'Application',
+import User from './User';
+
+export type ApplicationType = 'apiKey' | 'oAuth';
+
+class Application extends Model<InferAttributes<Application>, InferCreationAttributes<Application>> {
+  public declare readonly id: CreationOptional<number>;
+  public declare CollectiveId: number;
+  public declare CreatedByUserId: ForeignKey<User['id']>;
+  public declare type: ApplicationType;
+  public declare apiKey: string;
+  public declare clientId: string;
+  public declare clientSecret: string;
+  public declare callbackUrl: string;
+  public declare name: string;
+  public declare description: string;
+  public declare disabled: boolean;
+  public declare createdAt: CreationOptional<Date>;
+  public declare updatedAt: CreationOptional<Date>;
+  public declare deletedAt: CreationOptional<Date>;
+  public declare data: JSON;
+
+  get info(): NonAttribute<Partial<Application>> {
+    return {
+      name: this.name,
+      description: this.description,
+      apiKey: this.apiKey,
+      clientId: this.clientId,
+      clientSecret: this.clientSecret,
+      callbackUrl: this.callbackUrl,
+    };
+  }
+
+  get publicInfo(): NonAttribute<Partial<Application>> {
+    return {
+      id: this.id,
+      name: this.name,
+      description: this.description,
+      type: this.type,
+      CreatedByUserId: this.CreatedByUserId,
+      CollectiveId: this.CollectiveId,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+      deletedAt: this.deletedAt,
+    };
+  }
+}
+
+Application.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -48,7 +94,7 @@ const Application = sequelize.define(
         const encrypted = this.getDataValue('clientSecret');
         return isNil(encrypted) ? null : crypto.decrypt(encrypted);
       },
-      set(value) {
+      set(value: string) {
         this.setDataValue('clientSecret', crypto.encrypt(value));
       },
     },
@@ -81,37 +127,13 @@ const Application = sequelize.define(
     },
   },
   {
+    sequelize,
+    modelName: 'Application',
     paranoid: true,
-
-    getterMethods: {
-      info() {
-        return {
-          name: this.name,
-          description: this.description,
-          apiKey: this.apiKey,
-          clientId: this.clientId,
-          clientSecret: this.clientSecret,
-          callbackUrl: this.callbackUrl,
-        };
-      },
-      publicInfo() {
-        return {
-          id: this.id,
-          name: this.name,
-          description: this.description,
-          type: this.type,
-          CreatedByUserId: this.CreatedByUserId,
-          CollectiveId: this.CollectiveId,
-          createdAt: this.createdAt,
-          updatedAt: this.updatedAt,
-          deletedAt: this.deletedAt,
-        };
-      },
-    },
   },
 );
 
-Application.create = props => {
+Application.create = (props?): Promise<ReturnType<typeof Application.create>> => {
   if (props.type === 'apiKey') {
     props = merge(props, {
       apiKey: randomBytes(20).toString('hex'),
