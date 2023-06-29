6d381495a7366c011afcbaca18aad7bfcceeadba
Henrique <hdiniz@users.noreply.github.com>
2023-06-29 09:56:30 -0300

    Add VirtualCardRequest model

---
+++ migrations/20230629114015-add-VirtualCardRequests-table.js
@@ -0,0 +1,85 @@
+'use strict';
+
+import { VirtualCardRequestStatus } from '../server/models/VirtualCardRequest';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('VirtualCardRequests', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      purpose: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      notes: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      status: {
+        allowNull: false,
+        defaultValue: VirtualCardRequestStatus.PENDING,
+        type: Sequelize.STRING,
+      },
+      currency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      spendingLimitAmount: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      spendingLimitInterval: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'Users' },
+        allowNull: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      HostCollectiveId: {
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'Collectives' },
+        allowNull: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      CollectiveId: {
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'Collectives' },
+        allowNull: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.addIndex('VirtualCardRequests', ['HostCollectiveId', 'CollectiveId'], {
+      where: { deletedAt: null },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('VirtualCardRequests');
+  },
+};
---
+++ server/models/VirtualCardRequest.ts
@@ -0,0 +1,174 @@
+import {
+  BelongsToGetAssociationMixin,
+  CreationOptional,
+  DataTypes,
+  ForeignKey,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  NonAttribute,
+} from 'sequelize';
+
+import { VirtualCardLimitIntervals } from '../constants/virtual-cards';
+import sequelize from '../lib/sequelize';
+
+import Collective from './Collective';
+import User from './User';
+
+export enum VirtualCardRequestStatus {
+  PENDING = 'PENDING',
+}
+
+export default class VirtualCardRequest extends Model<
+  InferAttributes<VirtualCardRequest>,
+  InferCreationAttributes<VirtualCardRequest>
+> {
+  declare id: CreationOptional<number>;
+  declare purpose: string;
+  declare notes: string;
+  declare status: VirtualCardRequestStatus;
+  declare currency: string;
+  declare spendingLimitAmount: number;
+  declare spendingLimitInterval: VirtualCardLimitIntervals;
+
+  declare UserId: ForeignKey<User['id']>;
+  declare User?: User;
+  declare getUser: BelongsToGetAssociationMixin<User>;
+
+  declare HostCollectiveId: ForeignKey<Collective['id']>;
+  declare Host?: NonAttribute<Collective>;
+  declare getHost: BelongsToGetAssociationMixin<Collective>;
+
+  declare CollectiveId: ForeignKey<Collective['id']>;
+  declare Collective?: NonAttribute<Collective>;
+  declare getCollective: BelongsToGetAssociationMixin<Collective>;
+
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+  declare deletedAt: CreationOptional<Date>;
+
+  get info(): NonAttribute<
+    Pick<
+      VirtualCardRequest,
+      | 'id'
+      | 'purpose'
+      | 'notes'
+      | 'status'
+      | 'currency'
+      | 'spendingLimitAmount'
+      | 'spendingLimitInterval'
+      | 'UserId'
+      | 'CollectiveId'
+      | 'HostCollectiveId'
+      | 'createdAt'
+      | 'updatedAt'
+      | 'deletedAt'
+    >
+  > {
+    return {
+      id: this.id,
+      purpose: this.purpose,
+      notes: this.notes,
+      status: this.status,
+      currency: this.currency,
+      spendingLimitAmount: this.spendingLimitAmount,
+      spendingLimitInterval: this.spendingLimitInterval,
+      UserId: this.UserId,
+      CollectiveId: this.CollectiveId,
+      HostCollectiveId: this.HostCollectiveId,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+      deletedAt: this.deletedAt,
+    };
+  }
+}
+
+VirtualCardRequest.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    purpose: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(val: string) {
+        this.setDataValue('purpose', val?.trim());
+      },
+      validate: {
+        len: [1, 60],
+        notEmpty: true,
+      },
+    },
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      set(val: string) {
+        this.setDataValue('notes', val?.trim());
+      },
+      validate: {
+        len: [1, 3000],
+        notEmpty: true,
+      },
+    },
+    status: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: VirtualCardRequestStatus.PENDING,
+    },
+    currency: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    spendingLimitAmount: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    spendingLimitInterval: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    UserId: {
+      type: DataTypes.INTEGER,
+      references: { key: 'id', model: 'Users' },
+      allowNull: true,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    HostCollectiveId: {
+      type: DataTypes.INTEGER,
+      references: { key: 'id', model: 'Collectives' },
+      allowNull: true,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    CollectiveId: {
+      type: DataTypes.INTEGER,
+      references: { key: 'id', model: 'Collectives' },
+      allowNull: true,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'VirtualCardRequests',
+    paranoid: true,
+  },
+);
