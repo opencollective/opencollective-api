9d2c5d7494c2d5422e9e84430870a7570da103c0
Leo Kewitz <leo@opencollective.com>
2023-08-18 11:03:57 +0200

    fix: block free subscriptions

--- server/graphql/v1/mutations/orders.js
+++ server/graphql/v1/mutations/orders.js
@@ -211,6 +211,8 @@ export async function createOrder(order, req) {
 
   if (order.interval && (!order.paymentMethod || order.paymentMethod?.type === PAYMENT_METHOD_TYPE.MANUAL)) {
     throw new ValidationFailed('Manual payment methods cannot be used for subscriptions');
+  } else if (order.interval && order.totalAmount === 0) {
+    throw new ValidationFailed('Subscriptions cannot be free');
   }
 
   await checkOrdersLimit(order, reqIp, reqMask);
