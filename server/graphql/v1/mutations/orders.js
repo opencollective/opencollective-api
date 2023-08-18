5db266f1880eb907ac729ccd0c081e63bc826329
Leo Kewitz <leo@opencollective.com>
2023-08-18 07:01:37 -0300

    Block manual payments from creating recurring contributions (#9252)
    * fix: block manual payments from creating recurring contributions
    
    * fix: block free subscriptions

--- server/graphql/v1/mutations/orders.js
+++ server/graphql/v1/mutations/orders.js
@@ -209,6 +209,12 @@ export async function createOrder(order, req) {
     await checkGuestContribution(order, loaders);
   }
 
+  if (order.interval && (!order.paymentMethod || order.paymentMethod?.type === PAYMENT_METHOD_TYPE.MANUAL)) {
+    throw new ValidationFailed('Manual payment methods cannot be used for subscriptions');
+  } else if (order.interval && order.totalAmount === 0) {
+    throw new ValidationFailed('Subscriptions cannot be free');
+  }
+
   await checkOrdersLimit(order, reqIp, reqMask);
   await orderFraudProtection(req, order).catch(error => {
     reportErrorToSentry(error, { transactionName: 'orderFraudProtection', user: req.remoteUser });
