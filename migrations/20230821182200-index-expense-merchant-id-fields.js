df6d5bb86eb850f357173aae93699ca52a007b20
Leo Kewitz <leo@opencollective.com>
2023-08-22 10:11:33 -0300

    refact: use friendly index names
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- migrations/20230821182200-index-expense-merchant-id-fields.js
+++ migrations/20230821182200-index-expense-merchant-id-fields.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface) {
     // Stripe issued Virtual Card transactions
     await queryInterface.sequelize.query(`
-      CREATE INDEX CONCURRENTLY IF NOT EXISTS "expenses_data_transactionId"
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "expenses_data_stripe_virtual_card_transaction_id"
       ON "Expenses"
       USING BTREE (("data"#>>'{transactionId}') ASC)
       WHERE "data"#>>'{transactionId}' IS NOT NULL
@@ -14,7 +14,7 @@ module.exports = {
 
     // PayPal transactions
     await queryInterface.sequelize.query(`
-      CREATE INDEX CONCURRENTLY IF NOT EXISTS "expenses_data_transaction_id"
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "expenses_data_paypal_transaction_id"
       ON "Expenses"
       USING BTREE (("data"#>>'{transaction_id}') ASC)
       WHERE "data"#>>'{transaction_id}' IS NOT NULL
