08be01378624b063d990ee227ada29f9e78bb3c2
Benjamin Piouffle <benjamin@opencollective.com>
2023-09-22 12:22:38 +0100

    chore(Account): add support for contact permission

--- server/graphql/v2/object/AccountPermissions.ts
+++ server/graphql/v2/object/AccountPermissions.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 
+import FEATURE from '../../../constants/feature';
+import { canUseFeature } from '../../../lib/user-permissions';
 import { checkScope } from '../../common/scope-check';
 import { getIdEncodeResolver, IDENTIFIER_TYPES } from '../identifiers';
 
@@ -16,9 +18,20 @@ const GraphQLAccountPermissions = new GraphQLObjectType({
     },
     addFunds: {
       type: new GraphQLNonNull(GraphQLPermission),
-      description: 'Whether the current user can mark this order as expired',
+      description: 'Whether the current user can add funds to this account',
       resolve(collective, _, req: express.Request): PermissionFields {
         return { allowed: (checkScope(req, 'host') && req.remoteUser?.isAdmin(collective.HostCollectiveId)) || false };
+      },
+    },
+    contact: {
+      type: new GraphQLNonNull(GraphQLPermission),
+      description: 'Whether the current user can contact this account',
+      resolve(collective, _, req: express.Request): PermissionFields {
+        if (!canUseFeature(req.remoteUser, FEATURE.CONTACT_COLLECTIVE)) {
+          return { allowed: false, reason: 'You are not authorized to contact other accounts' };
+        } else {
+          return { allowed: collective.canContact() };
+        }
       },
     },
   }),
