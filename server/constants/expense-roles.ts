8c39a0cc2c47e97c9334010c5af1ddb7e93f084b
Benjamin Piouffle <benjamin@opencollective.com>
2023-11-06 14:45:45 +0100

    Breakdown AccountingCategories values by role in data (#9434)
    * enhancement(AccountingCategories): breakdown values by role in data
    
    * refact(Expenses): move expense roles to a constant

---
+++ server/constants/expense-roles.ts
@@ -0,0 +1,9 @@
+/**
+ * Defines the possible roles for a user on an expense.
+ */
+
+export enum ExpenseRoles {
+  hostAdmin = 'hostAdmin',
+  collectiveAdmin = 'collectiveAdmin',
+  submitter = 'submitter',
+}
