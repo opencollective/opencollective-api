466a91324dd550c99a086e98e4be121773bb17fc
François Hodierne <francois@hodierne.net>
2023-11-10 16:49:13 +0100

    Add orphan transaction check (#9472)
    * add a check to make sure there is no orphan transaction
    
    * update message for check

--- checks/model/transactions.js
+++ checks/model/transactions.js
@@ -42,8 +42,30 @@ async function checkDeletedCollectives({ fix = false } = {}) {
   }
 }
 
+async function checkOrphanTransactions() {
+  const message = 'No orphan Transaction without a primary Transaction (EXPENSE, CONTRIBUTION, ADDED_FUNDS)';
+
+  const results = await sequelize.query(
+    `SELECT COUNT(*)
+     FROM "Transactions" t1
+     INNER JOIN "Transactions" t2 ON t1."TransactionGroup" = t2."TransactionGroup"
+     AND t2."kind" NOT IN ('EXPENSE', 'CONTRIBUTION', 'ADDED_FUNDS') AND t2."deletedAt" IS NULL
+     LEFT JOIN "Transactions" t3 ON t3."TransactionGroup" = t2."TransactionGroup"
+     AND t3."kind" IN ('EXPENSE', 'CONTRIBUTION', 'ADDED_FUNDS') AND t3."deletedAt" IS NULL
+     WHERE t1."kind" IN ('EXPENSE', 'CONTRIBUTION', 'ADDED_FUNDS') AND t1."deletedAt" IS NOT NULL
+     AND t3."id" IS NULL`,
+    { type: sequelize.QueryTypes.SELECT, raw: true },
+  );
+
+  if (results[0].count > 0) {
+    // Not fixable
+    throw new Error(message);
+  }
+}
+
 export async function checkTransactions({ fix = false } = {}) {
   await checkDeletedCollectives({ fix });
+  await checkOrphanTransactions();
 }
 
 if (!module.parent) {
