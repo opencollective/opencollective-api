e7eb0fb5bd17c74d44ba1e9a51b085768b21872c
Leo Kewitz <leo@opencollective.com>
2023-11-29 16:11:57 +0100

    refact: redis client factory

--- server/lib/redis.ts
+++ server/lib/redis.ts
@@ -4,41 +4,63 @@ import { createClient, RedisClientType } from 'redis';
 
 import logger from './logger';
 
-let redisClient;
+export enum RedisInstanceType {
+  DEFAULT = 'DEFAULT',
+  TIMELINE = 'TIMELINE',
+}
 
-export async function createRedisClient(): Promise<RedisClientType> {
-  if (!redisClient) {
-    const url = get(config, 'redis.serverUrl');
-    if (!url) {
-      return;
-    }
+const RedisInstanceKey = {
+  [RedisInstanceType.DEFAULT]: 'redis.serverUrl',
+  [RedisInstanceType.TIMELINE]: 'redis.serverUrlTimeline',
+};
 
-    const redisOptions = { url };
-    if (redisOptions.url.includes('rediss://')) {
-      redisOptions['socket'] = { tls: true, rejectUnauthorized: false };
-    }
+const redisClients: Record<string, RedisClientType> = {};
 
-    redisClient = createClient(redisOptions);
-    try {
-      redisClient.on('error', err => logger.error(`Redis error`, err));
-      redisClient.on('reconnecting', () => logger.info(`Redis reconnecting`));
-      redisClient.on('connect', () => logger.info(`Redis connected`));
-      redisClient.on('ready', () => logger.info(`Redis ready`));
-      redisClient.on('end', () => logger.info(`Redis connection closed`));
-
-      await redisClient.connect();
-    } catch (err) {
-      logger.error(`Redis connection error`, err);
-      redisClient = null;
-    }
+export async function createRedisClient(
+  instanceType: RedisInstanceType = RedisInstanceType.DEFAULT,
+): Promise<RedisClientType> {
+  const url = get(config, RedisInstanceKey[instanceType]);
+  // Fallback to default instance if the requested instance is not configured
+  if (instanceType !== RedisInstanceType.DEFAULT && !url) {
+    logger.warn(`Redis instance ${instanceType} is not configured, falling back to default instance`);
+    instanceType = RedisInstanceType.DEFAULT;
   }
 
-  return redisClient;
+  // Return the existing client if it exists
+  if (redisClients[instanceType]) {
+    return redisClients[instanceType];
+  }
+  // Return null if the instance is not configured
+  else if (!url) {
+    return;
+  }
+
+  const redisOptions = { url };
+  if (redisOptions.url.includes('rediss://')) {
+    redisOptions['socket'] = { tls: true, rejectUnauthorized: false };
+  }
+
+  try {
+    const client = createClient(redisOptions);
+    client.on('error', err => logger.error(`Redis error (${instanceType})`, err));
+    client.on('reconnecting', () => logger.info(`Redis reconnecting (${instanceType})`));
+    client.on('connect', () => logger.info(`Redis connected (${instanceType})`));
+    client.on('ready', () => logger.info(`Redis ready (${instanceType})`));
+    client.on('end', () => logger.info(`Redis connection closed (${instanceType})`));
+
+    await client.connect();
+    redisClients[instanceType] = client as RedisClientType;
+  } catch (err) {
+    logger.error(`Redis connection error (${instanceType})`, err);
+    redisClients[instanceType] = null;
+  }
+
+  return redisClients[instanceType];
 }
 
 export async function closeRedisClient() {
-  if (redisClient) {
-    await redisClient.disconnect();
+  for (const instance in redisClients) {
+    await redisClients[instance].disconnect();
+    delete redisClients[instance];
   }
-  redisClient = null;
 }
