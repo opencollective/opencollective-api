d7410a0b14a10c944d3647d38e0288f5665c45ac
François Hodierne <francois@hodierne.net>
2024-01-19 14:33:57 +0100

    add PaymentMethodId to Expense

---
+++ migrations/20240119132020-expense-paymentMethod.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Expenses', 'PaymentMethodId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'PaymentMethods',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('Expenses', 'PaymentMethodId');
+  },
+};
