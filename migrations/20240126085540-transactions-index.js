45f505a3fcabdc0584be892ea441a1c5753e4621
François Hodierne <francois@hodierne.net>
2024-01-26 10:18:25 +0100

    update more indices

--- migrations/20240126085540-transactions-index.js
+++ migrations/20240126085540-transactions-index.js
@@ -2,24 +2,63 @@
 
 module.exports = {
   up: async queryInterface => {
+    // Host Unsorted
     await queryInterface.sequelize.query(`
       CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__host_collective_id"
       ON "Transactions"("HostCollectiveId")
       WHERE "deletedAt" IS NULL AND "HostCollectiveId" IS NOT NULL
     `);
     await queryInterface.sequelize.query(`
-      DROP INDEX "transaction__host_collective_id"
+      DROP INDEX IF EXISTS "transaction__host_collective_id"
+    `);
+
+    // Collective Unsorted
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__collective_id"
+      ON "Transactions"("CollectiveId")
+      WHERE "deletedAt" IS NULL AND "CollectiveId" IS NOT NULL
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "Transactions_GroupId"
+    `);
+    // Collective Sorted
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__collective_id_createdAt"
+      ON "Transactions"("CollectiveId", ROUND(EXTRACT(epoch FROM "createdAt" AT TIME ZONE 'UTC') / 10) DESC)
+      WHERE "deletedAt" IS NULL AND "CollectiveId" IS NOT NULL
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__collective_id_sorted"
     `);
   },
 
   down: async queryInterface => {
+    // Host Unsorted
     await queryInterface.sequelize.query(`
       CREATE INDEX CONCURRENTLY IF NOT EXISTS "transaction__host_collective_id"
       ON "Transactions"("HostCollectiveId")
       WHERE "deletedAt" IS NULL
     `);
     await queryInterface.sequelize.query(`
-      DROP INDEX "transactions__host_collective_id"
+      DROP INDEX IF EXISTS "transactions__host_collective_id"
+    `);
+
+    // Collective Unsorted
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "Transactions_GroupId"
+      ON "Transactions"("CollectiveId", "deletedAt")
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__collective_id"
+    `);
+    // Collective Sorted
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__collective_id_sorted"
+      ON "Transactions"("CollectiveId", "id")
+      WHERE "deletedAt" IS NULL
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__collective_id_createdAt"
     `);
   },
 };
