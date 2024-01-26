ee5509d436e291ce79c8281bcd3737b6cbc8eff7
François Hodierne <francois@hodierne.net>
2024-01-26 10:31:05 +0100

    more

--- migrations/20240126085540-transactions-index.js
+++ migrations/20240126085540-transactions-index.js
@@ -30,6 +30,43 @@ module.exports = {
     await queryInterface.sequelize.query(`
       DROP INDEX IF EXISTS "transactions__collective_id_sorted"
     `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__collective_id_created_at_type"
+    `);
+
+    // Order
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__order_id"
+      ON "Transactions"("OrderId")
+      WHERE "deletedAt" IS NULL AND "OrderId" IS NOT NULL
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "DonationId"
+    `);
+
+    // PaymentMethod
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__payment_method_id"
+      ON "Transactions"("PaymentMethodId")
+      WHERE "deletedAt" IS NULL AND "PaymentMethodId" IS NOT NULL
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "PaymentMethodId-type"
+    `);
+
+    // Expense
+    // Already looking good
+
+    // Indexes that are not looking useful
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "CollectiveId-FromCollectiveId-type"
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "CollectiveId-type"
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__created_by_user_id"
+    `);
   },
 
   down: async queryInterface => {
@@ -58,7 +95,43 @@ module.exports = {
       WHERE "deletedAt" IS NULL
     `);
     await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__collective_id_created_at_type"
+      ON "Transactions"("CollectiveId", "createdAt", "type")
+    `);
+    await queryInterface.sequelize.query(`
       DROP INDEX IF EXISTS "transactions__collective_id_createdAt"
+    `);
+
+    // Order
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "DonationId"
+      ON "Transactions"("OrderId")
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__order_id "
+    `);
+
+    // PaymentMethod
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "PaymentMethodId-type "
+      ON "Transactions"("PaymentMethodId", "type", "deletedAt")
+    `);
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS "transactions__payment_method_id"
+    `);
+
+    // Indexes that were not looking useful
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "CollectiveId-FromCollectiveId-type"
+      ON "Transactions"("CollectiveId", "FromCollectiveId", "deletedAt")
+    `);
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "CollectiveId-type"
+      ON "Transactions"("CollectiveId", "type")
+    `);
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS "transactions__created_by_user_id"
+      ON "Transactions"("CreatedByUserId")
     `);
   },
 };
