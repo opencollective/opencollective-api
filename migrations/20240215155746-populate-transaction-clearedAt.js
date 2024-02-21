1117fec8cd5012307032f5f0d2d2b469834d4be9
Leo Kewitz <leo@opencollective.com>
2024-02-21 14:44:17 +0100

    fix: migration support for disputes and reviews

--- migrations/20240215155746-populate-transaction-clearedAt.js
+++ migrations/20240215155746-populate-transaction-clearedAt.js
@@ -4,26 +4,30 @@
 module.exports = {
   async up(queryInterface) {
     const [, metadata] = await queryInterface.sequelize.query(`
-      WITH to_update AS (SELECT
-        "TransactionGroup",
-        MIN(COALESCE(
-                to_timestamp((data#>>'{dispute,created}')::INT),
-                to_timestamp((data#>>'{refund,created}')::INT),
-                to_timestamp((data#>>'{charge,created}')::INT),
-                to_timestamp((data#>>'{transaction,created}')::INT),
-                (data #>> '{paypalSale,create_time}')::TIMESTAMP,
-                (data #>> '{paypalTransaction,time}')::TIMESTAMP,
-                (data #>> '{capture,create_time}')::TIMESTAMP,
-                (data #>> '{time_processed}')::TIMESTAMP,
-                (data #>> '{transfer,created}')::TIMESTAMP,
-                "createdAt"
-        )) AS "clearedAt"
-      FROM "Transactions"
-      WHERE
-        "deletedAt" IS NULL
-        AND "createdAt" >= '2024-01-01'
-        AND kind IN ('CONTRIBUTION', 'EXPENSE', 'ADDED_FUNDS', 'BALANCE_TRANSFER', 'PREPAID_PAYMENT_METHOD')
-        GROUP BY "TransactionGroup")
+      WITH
+        to_update AS (
+          SELECT
+            "TransactionGroup",
+            MIN(COALESCE(
+                    TO_TIMESTAMP((data #>> '{dispute,balance_transactions,0,created}')::INT),
+                    TO_TIMESTAMP((data #>> '{charge,dispute,balance_transactions,0,created}')::INT),
+                    TO_TIMESTAMP((data #>> '{review,created}')::INT),
+                    TO_TIMESTAMP((data #>> '{refund,created}')::INT),
+                    TO_TIMESTAMP((data #>> '{charge,created}')::INT),
+                    TO_TIMESTAMP((data #>> '{transaction,created}')::INT),
+                    (data #>> '{paypalSale,create_time}')::TIMESTAMP,
+                    (data #>> '{paypalTransaction,time}')::TIMESTAMP,
+                    (data #>> '{capture,create_time}')::TIMESTAMP,
+                    (data #>> '{time_processed}')::TIMESTAMP,
+                    (data #>> '{transfer,created}')::TIMESTAMP,
+                    "createdAt"
+                )) AS "clearedAt"
+          FROM "Transactions"
+          WHERE "deletedAt" IS NULL
+            AND "createdAt" >= '2024-01-01'
+            AND kind IN ('CONTRIBUTION', 'EXPENSE', 'ADDED_FUNDS', 'BALANCE_TRANSFER', 'PREPAID_PAYMENT_METHOD')
+          GROUP BY "TransactionGroup"
+          )
 
       UPDATE "Transactions"
       SET "clearedAt" = u."clearedAt"
