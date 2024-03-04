254033892f041246ca21d211eeefd7e2de3d8b87
Benjamin Piouffle <benjamin@opencollective.com>
2024-03-04 11:45:01 +0100

    fix(Maildev): localhost (#9899)

--- server/maildev.js
+++ server/maildev.js
@@ -2,6 +2,6 @@
 
 import MailDev from 'maildev'; // eslint-disable-line node/no-unpublished-import
 
-const maildev = new MailDev({ smtp: 1025, ip: 'localhost' });
+const maildev = new MailDev({ smtp: 1025, ip: '127.0.0.1' });
 
 export default maildev;
