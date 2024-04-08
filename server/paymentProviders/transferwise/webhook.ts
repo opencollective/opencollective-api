8d9a27e188894e73a04e9af8e883f23cbb5de8ec
Leo Kewitz <leo@opencollective.com>
2024-04-08 14:25:44 +0200

    fix: Wise webhook to update transfer status (#9999)

--- server/paymentProviders/transferwise/webhook.ts
+++ server/paymentProviders/transferwise/webhook.ts
@@ -4,12 +4,13 @@ import { Request } from 'express';
 import { pick, toString } from 'lodash';
 
 import activities from '../../constants/activities';
+import { Service } from '../../constants/connected-account';
 import expenseStatus from '../../constants/expense-status';
 import { TransactionKind } from '../../constants/transaction-kind';
 import logger from '../../lib/logger';
 import * as libPayments from '../../lib/payments';
 import { createTransactionsFromPaidExpense } from '../../lib/transactions';
-import { verifyEvent } from '../../lib/transferwise';
+import { getTransfer, verifyEvent } from '../../lib/transferwise';
 import models from '../../models';
 import { QuoteV2PaymentOption, QuoteV3PaymentOption, TransferStateChangeEvent } from '../../types/transferwise';
 
@@ -29,6 +30,22 @@ export async function handleTransferStateChange(event: TransferStateChangeEvent)
     // This is probably some other transfer not executed through our platform.
     logger.debug('Ignoring transferwise event.', event);
     return;
+  }
+
+  const [connectedAccount] = await expense.host.getConnectedAccounts({
+    where: { service: Service.TRANSFERWISE, deletedAt: null },
+    limit: 1,
+  });
+
+  let transfer;
+  if (!connectedAccount) {
+    logger.error(`Wise: No connected account found for host ${expense.host.slug}.`);
+    transfer = expense.data.transfer;
+  } else {
+    transfer = await getTransfer(connectedAccount, event.data.resource.id).catch(e => {
+      logger.error(`Wise: Failed to fetch transfer ${event.data.resource.id} from Wise`, e);
+      return expense.data.transfer;
+    });
   }
 
   const transaction = await models.Transaction.findOne({
@@ -71,11 +88,11 @@ export async function handleTransferStateChange(event: TransferStateChangeEvent)
     await expense.setAndSavePaymentMethodIfMissing();
 
     await createTransactionsFromPaidExpense(expense.host, expense, feesInHostCurrency, expenseToHostRate, {
-      ...pick(expense.data, ['fund', 'transfer']),
+      ...pick(expense.data, ['fund']),
+      transfer,
       clearedAt: event.data?.occurred_at && new Date(event.data.occurred_at),
     });
-
-    await expense.update({ data: { ...expense.data, feesInHostCurrency } });
+    await expense.update({ data: { ...expense.data, feesInHostCurrency, transfer } });
 
     // Mark Expense as Paid, create activity and send notifications
     await expense.markAsPaid();
@@ -105,6 +122,7 @@ export async function handleTransferStateChange(event: TransferStateChangeEvent)
     } else {
       logger.info(`Wise: Wise transfer #${event.data.resource.id} has no transactions, skipping refund.`);
     }
+    await expense.update({ data: { ...expense.data, transfer } });
     await expense.setError(expense.lastEditedById);
     await expense.createActivity(activities.COLLECTIVE_EXPENSE_ERROR, null, { isSystem: true, event });
   }
