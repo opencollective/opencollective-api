8c03f8b1cfa2bedf1c8d7c4c68c060e91fe8b8a6
Gustav Larsson <gustav.larsson@gmail.com>
2024-05-07 14:54:18 +0200

    feat(TransactionQuery): Use transaction reference input as query argument

--- server/graphql/v2/query/TransactionQuery.ts
+++ server/graphql/v2/query/TransactionQuery.ts
@@ -2,6 +2,7 @@ import { GraphQLString } from 'graphql';
 
 import models from '../../../models';
 import { NotFound } from '../../errors';
+import { fetchTransactionWithReference, GraphQLTransactionReferenceInput } from '../input/TransactionReferenceInput';
 import { GraphQLTransaction } from '../interface/Transaction';
 
 const TransactionQuery = {
@@ -11,11 +12,18 @@ const TransactionQuery = {
     id: {
       type: GraphQLString,
       description: 'The public id identifying the transaction (ie: rvelja97-pkzqbgq7-bbzyx6wd-50o8n4rm)',
+      deprecationReason: '2024-05-07: Please use the `transaction` field.',
+    },
+    transaction: {
+      type: GraphQLTransactionReferenceInput,
+      description: 'Identifiers to retrieve the transaction.',
     },
   },
-  async resolve(_, args) {
+  async resolve(_, args, req) {
     let transaction;
-    if (args.id) {
+    if (args.transaction) {
+      transaction = await fetchTransactionWithReference(args.transaction, req);
+    } else if (args.id) {
       transaction = await models.Transaction.findOne({ where: { uuid: args.id } });
     } else {
       return new Error('Please provide an id');
