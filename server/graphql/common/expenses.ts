664feb7b766b2c1f6a064d0b6ab9fc3aceb149ca
Benjamin Piouffle <benjamin@opencollective.com>
2024-09-03 11:40:38 +0200

    Update server/graphql/common/expenses.ts
    Co-authored-by: Leo Kewitz <leo@opencollective.com>

--- server/graphql/common/expenses.ts
+++ server/graphql/common/expenses.ts
@@ -1637,7 +1637,7 @@ export async function createExpense(req: express.Request, expenseData: ExpenseDa
       (await collective.host.getAccountForPaymentProvider(Service.TRANSFERWISE, { throwIfMissing: false }));
     if (connectedAccount) {
       paymentProviders.transferwise.validatePayoutMethod(connectedAccount, payoutMethod);
-      recipient = await paymentProviders.transferwise.createRecipient(connectedAccount[0], payoutMethod);
+      recipient = await paymentProviders.transferwise.createRecipient(connectedAccount, payoutMethod);
     }
   }
 
