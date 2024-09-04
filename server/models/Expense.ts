2bfea648b3672f7ff8d42b6360a5b3639c8358b4
Leo Kewitz <leo@opencollective.com>
2024-09-04 13:32:09 +0200

    fix: Expense.reference setter bug

--- server/models/Expense.ts
+++ server/models/Expense.ts
@@ -1,5 +1,5 @@
 import { TaxType } from '@opencollective/taxes';
-import { get, isEmpty, pick, sumBy } from 'lodash';
+import { get, isEmpty, isNil, pick, sumBy } from 'lodash';
 import { isMoment } from 'moment';
 import {
   BelongsToGetAssociationMixin,
@@ -936,8 +936,8 @@ Expense.init(
 
     reference: {
       type: DataTypes.STRING,
-      set(reference: string) {
-        this.setDataValue('reference', reference.trim());
+      set(reference: string | null) {
+        this.setDataValue('reference', isNil(reference) ? null : reference.trim());
       },
     },
   },
