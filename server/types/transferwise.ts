c0309cb95850fd1cce8dd75339ec868fff782c7a
Leo Kewitz <leo@opencollective.com>
2024-09-25 20:41:16 +0200

    wip

--- server/types/transferwise.ts
+++ server/types/transferwise.ts
@@ -280,7 +280,7 @@ export type TransferStatus =
 export interface WebhookEvent {
   data: Record<string, unknown>;
   subscription_id: string;
-  event_type: string;
+  event_type: 'transfers#refund' | 'transfers#state-change';
   schema_version: '2.0.0';
   sent_at: string;
 }
@@ -298,6 +298,21 @@ export interface TransferStateChangeEvent extends WebhookEvent {
     occurred_at: string;
   };
   event_type: 'transfers#state-change';
+}
+
+export interface TransferRefundEvent extends WebhookEvent {
+  data: {
+    resource: {
+      type: 'transfer';
+      id: number;
+      profile_id: number;
+      account_id: number;
+      refund_amount: number;
+      refund_currency: string;
+    };
+    occurred_at: string;
+  };
+  event_type: 'transfers#refund';
 }
 
 export type Transfer = {
