733366b658b0d4fcbc1bbc403d647fffd0c1d152
Benjamin Piouffle <benjamin@opencollective.com>
2024-10-14 13:35:37 +0200

    fix(PaymentMethod): prevent returning null for limitedToHosts

--- server/graphql/v2/object/PaymentMethod.js
+++ server/graphql/v2/object/PaymentMethod.js
@@ -146,7 +146,7 @@ export const GraphQLPaymentMethod = new GraphQLObjectType({
             hosts = [host];
           }
 
-          return hosts;
+          return hosts?.filter(Boolean);
         },
       },
       expiryDate: {
