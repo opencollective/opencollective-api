44fccbb406dc9738b5c0da6c7428d9478f21bf19
Henrique <hdiniz@users.noreply.github.com>
2024-11-05 07:39:40 -0300

    Fix expensePolicy migration rollback

--- migrations/20241103183508-migrate-expense-policies-to-policies.js
+++ migrations/20241103183508-migrate-expense-policies-to-policies.js
@@ -22,7 +22,7 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     await queryInterface.addColumn('Collectives', 'expensePolicy', {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
     });
     await queryInterface.sequelize.query(`
       UPDATE "Collectives" SET
