1c80321bd953c63c50c77cad092ccb97a37167e5
Leo Kewitz <leo@opencollective.com>
2024-11-14 06:49:19 -0300

    Export Hosted Collectives: add contributionRefundedTotal and refact average resolvers (#10461)
    * refact: add contributionRefundedTotal to HostedAccountSummary
    
    * refact: HostedAccountSummary average period as argument
    
    * chore: update schemas

--- server/graphql/v2/object/HostedAccountSummary.ts
+++ server/graphql/v2/object/HostedAccountSummary.ts
@@ -1,6 +1,8 @@
 import { GraphQLFloat, GraphQLInt, GraphQLObjectType } from 'graphql';
-import { ceil, min, round } from 'lodash';
+import { min, round } from 'lodash';
 import moment from 'moment';
+
+import { GraphQLAveragePeriod } from '../enum/AveragePeriod';
 
 import { GraphQLAmount } from './Amount';
 
@@ -13,27 +15,9 @@ export const HostedAccountSummary = new GraphQLObjectType({
       type: GraphQLInt,
       resolve: ({ summary }) => summary?.expenseCount || 0,
     },
-    expenseMonthlyAverageCount: {
-      type: GraphQLFloat,
-      description:
-        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
-      resolve: ({ summary, months }) => {
-        const count = summary?.expenseCount || 0;
-        return months > 0 ? round(count / months, 2) : 0;
-      },
-    },
     expenseTotal: {
       type: GraphQLAmount,
       resolve: ({ host, summary }) => ({ value: summary?.expenseTotal || 0, currency: host.currency }),
-    },
-    expenseMonthlyAverageTotal: {
-      type: GraphQLAmount,
-      description:
-        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
-      resolve: ({ host, summary, months }) => {
-        const value = months > 0 && summary?.expenseTotal ? Math.round(summary?.expenseTotal / months || 0) : 0;
-        return { value, currency: host.currency };
-      },
     },
     expenseMaxValue: {
       type: GraphQLAmount,
@@ -47,28 +31,9 @@ export const HostedAccountSummary = new GraphQLObjectType({
       type: GraphQLInt,
       resolve: ({ summary }) => summary?.contributionCount || 0,
     },
-    contributionMonthlyAverageCount: {
-      type: GraphQLFloat,
-      description:
-        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
-      resolve: ({ summary, months }) => {
-        const count = summary?.contributionCount || 0;
-        return months > 0 ? round(count / months, 2) : 0;
-      },
-    },
     contributionTotal: {
       type: GraphQLAmount,
       resolve: ({ host, summary }) => ({ value: summary?.contributionTotal || 0, currency: host.currency }),
-    },
-    contributionMonthlyAverageTotal: {
-      type: GraphQLAmount,
-      description:
-        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
-      resolve: ({ host, summary, months }) => {
-        const value =
-          months > 0 && summary?.contributionTotal ? Math.round(summary?.contributionTotal / months || 0) : 0;
-        return { value, currency: host.currency };
-      },
     },
     hostFeeTotal: {
       type: GraphQLAmount,
@@ -82,36 +47,123 @@ export const HostedAccountSummary = new GraphQLObjectType({
       type: GraphQLAmount,
       resolve: ({ host, summary }) => ({ value: summary?.receivedTotal || 0, currency: host.currency }),
     },
-    spentTotalMonthlyAverage: {
+    // Averages
+    expenseAverageTotal: {
       type: GraphQLAmount,
-      resolve: ({ host, summary, months }) => {
-        const value = months > 0 && summary?.spentTotal ? Math.round(summary?.spentTotal / months || 0) : 0;
+      description:
+        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ host, summary, periods }, args) => {
+        const period = periods[args.period];
+        const value = period > 0 && summary?.expenseTotal ? Math.round(summary?.expenseTotal / period || 0) : 0;
         return { value, currency: host.currency };
       },
     },
-    receivedTotalMonthlyAverage: {
+    expenseAverageCount: {
+      type: GraphQLFloat,
+      description:
+        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ summary, periods }, args) => {
+        const period = periods[args.period];
+        const count = summary?.expenseCount || 0;
+        return period > 0 ? round(count / period, 2) : 0;
+      },
+    },
+    contributionAverageTotal: {
       type: GraphQLAmount,
-      resolve: ({ host, summary, months }) => {
-        const value = months > 0 && summary?.receivedTotal ? Math.round(summary?.receivedTotal / months || 0) : 0;
+      description:
+        'Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less',
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ host, summary, periods }, args) => {
+        const period = periods[args.period];
+        const value =
+          period > 0 && summary?.contributionTotal ? Math.round(summary?.contributionTotal / period || 0) : 0;
         return { value, currency: host.currency };
       },
+    },
+    contributionAverageCount: {
+      type: GraphQLFloat,
+      description:
+        'Average calculated over the number of months/years the collective was approved or the number of months since dateFrom, whichever is less',
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ summary, periods }, args) => {
+        const period = periods[args.period];
+        const count = summary?.contributionCount || 0;
+        return period > 0 ? round(count / period, 2) : 0;
+      },
+    },
+    spentTotalAverage: {
+      type: GraphQLAmount,
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ host, summary, periods }, args) => {
+        const period = periods[args.period];
+        const value = period > 0 && summary?.spentTotal ? Math.round(summary?.spentTotal / period || 0) : 0;
+        return { value, currency: host.currency };
+      },
+    },
+    receivedTotalAverage: {
+      type: GraphQLAmount,
+      args: {
+        period: {
+          type: GraphQLAveragePeriod,
+          defaultValue: 'month',
+        },
+      },
+      resolve: ({ host, summary, periods }, args) => {
+        const period = periods[args.period];
+        const value = period > 0 && summary?.receivedTotal ? Math.round(summary?.receivedTotal / period || 0) : 0;
+        return { value, currency: host.currency };
+      },
+    },
+    contributionRefundedTotal: {
+      type: GraphQLAmount,
+      resolve: ({ host, summary }) => ({ value: summary?.contributionRefundedTotal || 0, currency: host.currency }),
     },
   }),
 });
+
+// It is OK to consider 1.4 months when calculating an average but it is misleading to consider 0.4 months.
+const roundAveragePeriod = value => (value < 1 ? 1 : round(value, 1));
 
 export const resolveHostedAccountSummary = async (account, args, req) => {
   const host = await req.loaders.Collective.byId.load(account.HostCollectiveId);
   const summary = await req.loaders.Collective.stats.hostedAccountSummary.buildLoader(args).load(account.id);
 
-  // Average calculated over the number of months the collective was approved or the number of months since dateFrom, whichever is less
-  const monthsSinceApproved = moment.duration(summary?.daysSinceApproved || 0, 'days').asMonths();
-  let months;
+  // Periods are based on the time the collective is hosted (approved) or the number of months since dateFrom, whichever is less
+  const daysSinceApproved = moment.duration(summary?.daysSinceApproved || 0, 'days');
+  const monthsSinceApproved = daysSinceApproved.asMonths();
+  const yearsSinceApproved = daysSinceApproved.asYears();
+  let month = roundAveragePeriod(monthsSinceApproved);
+  let year = roundAveragePeriod(yearsSinceApproved);
   if (args.dateFrom) {
-    const monthsSinceDateFrom = moment().diff(moment(args.dateFrom), 'months', true);
-    months = ceil(min([monthsSinceApproved, monthsSinceDateFrom]));
-  } else {
-    months = ceil(moment.duration(summary?.daysSinceApproved || 0, 'days').asMonths());
+    month = roundAveragePeriod(min([monthsSinceApproved, moment().diff(moment(args.dateFrom), 'months', true)]));
+    year = roundAveragePeriod(min([yearsSinceApproved, moment().diff(moment(args.dateFrom), 'years', true)]));
   }
-
-  return { host, summary, months };
+  return { host, summary, periods: { month, year } };
 };
