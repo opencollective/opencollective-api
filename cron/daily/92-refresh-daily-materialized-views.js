8d0e086a6c7fdaa8b7c7b73c97737afda35aa70e
François Hodierne <francois@hodierne.net>
2024-11-19 12:15:38 +0100

    introduce daily materialized views

--- cron/daily/92-refresh-daily-materialized-views.js
+++ cron/daily/92-refresh-daily-materialized-views.js
@@ -4,9 +4,7 @@ import logger from '../../server/lib/logger';
 import { sequelize } from '../../server/models';
 import { runCronJob } from '../utils';
 
-const VIEWS = ['CollectiveOrderStats', 'ExpenseTagStats'];
-
-const VIEWS_WITHOUT_UNIQUE_INDEX = ['HostMonthlyTransactions', 'CollectiveTagStats'];
+const VIEWS = ['CollectiveTransactionStats', 'TransactionBalances', 'CollectiveBalanceCheckpoint'];
 
 /**
  * Refresh the materialized views.
@@ -30,24 +28,8 @@ async function run() {
       );
     }
   }
-
-  for (const view of VIEWS_WITHOUT_UNIQUE_INDEX) {
-    logger.info(`Refreshing ${view} materialized view...`);
-    const startTime = process.hrtime();
-    try {
-      await sequelize.query(`REFRESH MATERIALIZED VIEW "${view}"`);
-      const [runSeconds, runMilliSeconds] = process.hrtime(startTime);
-      logger.info(`${view} materialized view refreshed in ${runSeconds}.${runMilliSeconds} seconds`);
-    } catch (e) {
-      const [runSeconds, runMilliSeconds] = process.hrtime(startTime);
-      logger.error(
-        `Error while refreshing ${view} materialized view after ${runSeconds}.${runMilliSeconds} seconds: ${e.message}`,
-        e,
-      );
-    }
-  }
 }
 
 if (require.main === module) {
-  runCronJob('refresh-materialized-views', run, 60 * 60);
+  runCronJob('refresh-daily-materialized-views', run, 24 * 60 * 60);
 }
