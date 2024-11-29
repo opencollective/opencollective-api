f308f476c35f4161ec3fb9803c639f155b7d3e23
Henrique <hdiniz@users.noreply.github.com>
2024-11-29 12:08:40 -0300

    Add deleted at to expense items url index

--- migrations/20241127165812-add-expense-attachments-url-indexes.js
+++ migrations/20241127165812-add-expense-attachments-url-indexes.js
@@ -4,7 +4,9 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.addIndex('ExpenseAttachedFiles', ['url']);
-    await queryInterface.addIndex('ExpenseItems', ['url']);
+    await queryInterface.addIndex('ExpenseItems', ['url'], {
+      where: { deletedAt: null },
+    });
   },
 
   async down(queryInterface) {
