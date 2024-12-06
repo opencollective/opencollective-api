20fe0db5a2492d6044a48da8cd0211ebd2436cd6
Benjamin Piouffle <benjamin@opencollective.com>
2024-12-06 14:06:35 +0100

    pdeps: Upgrade minimum postgres to 14

--- scripts/db_migrate_opencollective_dvl.sh
+++ scripts/db_migrate_opencollective_dvl.sh
@@ -8,6 +8,9 @@ DUMPFILE="test/dbdumps/$PG_DATABASE.pgsql"
 echo "Migrating $PG_DATABASE"
 DEBUG=psql PG_DATABASE=$PG_DATABASE npm run db:migrate
 PG_DATABASE=$PG_DATABASE npm run db:sanitize
-pg_dump -O -F t $PG_DATABASE > $DUMPFILE
+pg_dump -O -F t $PG_DATABASE >$DUMPFILE
+# Uncomment the line below to use a specific version of pg_dump (using docker)
+# (sudo docker run --rm --network host postgres:14.5 pg_dump -O -F t -h localhost -p 5432 -U opencollective $PG_DATABASE) >$DUMPFILE
+
 echo "$DUMPFILE migrated. Please commit it and push it."
 echo ""
