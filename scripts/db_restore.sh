f59985d770d14f1571e152324facafc30fa4e3f2
Benjamin Piouffle <benjamin@opencollective.com>
2024-12-09 15:43:06 +0100

    deps: Upgrade to PG 15

--- scripts/db_restore.sh
+++ scripts/db_restore.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: db_restore.sh -d DBNAME -U DBUSER --use-postgis -f DBDUMP_FILE";
-  echo "e.g.";
+  echo "Usage: db_restore.sh -d DBNAME -U DBUSER --use-postgis -f DBDUMP_FILE"
+  echo "e.g."
   echo "> db_restore.sh -d opencollective_dvl -U opencollective -f test/dbdumps/opencollective_dvl.pgsql"
-  exit 0;
+  exit 0
 }
 
-while [[ $# -gt 0 ]]
-do
-key="$1"
+while [[ $# -gt 0 ]]; do
+  key="$1"
 
-case $key in
-    -d|--dbname)
+  case $key in
+  -d | --dbname)
     LOCALDBNAME="$2"
     shift # past argument
     ;;
-    -U|--username)
+  -U | --username)
     LOCALDBUSER="$2"
     shift # past argument
     ;;
-    --use-postgis)
+  --use-postgis)
     USE_POSTGIS=1
     ;;
-    -f|--file)
+  -f | --file)
     DBDUMP_FILE="$2"
     shift # past argument
     ;;
-    *)
-            # unknown option
+  *)
+    # unknown option
     ;;
-esac
-shift # past argument or value
+  esac
+  shift # past argument or value
 done
 
 LOCALDBUSER=${LOCALDBUSER:-"opencollective"}
 LOCALDBNAME=${LOCALDBNAME:-"opencollective_dvl"}
-DBDUMP_FILE=${DBDUMP_FILE:-"test/dbdumps/opencollective_dvl.pgsql"}
+DBDUMP_FILE=${DBDUMP_FILE:-"./test/dbdumps/opencollective_dvl.pgsql"}
 
 echo "LOCALDBUSER=$LOCALDBUSER"
 echo "LOCALDBNAME=$LOCALDBNAME"
 echo "DBDUMP_FILE=$DBDUMP_FILE"
 
-if [ -z "$LOCALDBNAME" ]; then usage; fi;
+if [ -z "$LOCALDBNAME" ]; then usage; fi
 
 # kill all connections to the postgres server
 # echo "Killing all connections to database '$LOCALDBNAME'"
@@ -53,8 +52,11 @@ if [ -z "$LOCALDBNAME" ]; then usage; fi;
 # where pg_stat_activity.datname = '$LOCALDBNAME'
 # EOF
 
-dropdb -U postgres -h localhost --if-exists $LOCALDBNAME;
-createdb -U postgres -h localhost $LOCALDBNAME 2> /dev/null
+echo "Dropping '$LOCALDBNAME'"
+dropdb -U postgres -h localhost --if-exists $LOCALDBNAME
+
+echo "Creating '$LOCALDBNAME'"
+createdb -U postgres -h localhost $LOCALDBNAME 2>/dev/null
 
 # When restoring old backups, you may need to enable Postgis
 if [ "$USE_POSTGIS" = "1" ]; then
@@ -72,13 +74,54 @@ fi
   set -e
 } | tee >/dev/null
 
-# The first time we run it, we will trigger FK constraints errors
-set +e
-pg_restore -U postgres -h localhost --no-acl --no-owner --role=${LOCALDBUSER} -n public -O -c -d "${LOCALDBNAME}" "${DBDUMP_FILE}" 2>/dev/null
-set -e
+# Update table permissions
+echo "Updating table permissions"
+psql -U postgres -h localhost $LOCALDBNAME -c "GRANT ALL ON SCHEMA public TO ${LOCALDBUSER};"
 
-# So we run it twice :-)
-pg_restore -U postgres -h localhost --no-acl --no-owner --role=${LOCALDBUSER} -n public -O -c -d "${LOCALDBNAME}" "${DBDUMP_FILE}"
+PG_RESTORE_OPTIONS=(-U postgres -h localhost --no-acl --no-owner --role="${LOCALDBUSER}" -n public -O -d "${LOCALDBNAME}")
+PG_RESTORE_COMMAND=pg_restore
+if [ -n "$USE_DOCKER" ]; then
+  PG_RESTORE_COMMAND="sudo docker run --rm --network host -v $(dirname "$DBDUMP_FILE"):/backup postgres:14 pg_restore -h localhost -p 5432 -U opencollective"
+  DBDUMP_FILE="/backup/$(basename "$DBDUMP_FILE")"
+fi
+
+# The first time we run it with the -s option (schema only)
+echo "Restoring schema"
+$PG_RESTORE_COMMAND "${PG_RESTORE_OPTIONS[@]}" -s "${DBDUMP_FILE}"
+
+# Disable triggers
+echo "Disabling triggers"
+psql -U postgres -h localhost "${LOCALDBNAME}" -c "
+DO \$\$ 
+BEGIN 
+   EXECUTE (
+      SELECT string_agg(
+         format('ALTER TABLE %I.%I DISABLE TRIGGER ALL', schemaname, tablename), '; '
+      ) 
+      FROM pg_tables 
+      WHERE schemaname = 'public'
+   ); 
+END \$\$;
+"
+
+# Restore data (-a flag)
+echo "Restoring data"
+$PG_RESTORE_COMMAND "${PG_RESTORE_OPTIONS[@]}" -a "${DBDUMP_FILE}"
+
+# Re-enable triggers
+echo "Re-enabling triggers"
+psql -U postgres -h localhost "${LOCALDBNAME}" -c "
+DO \$\$ 
+BEGIN 
+   EXECUTE (
+      SELECT string_agg(
+         format('ALTER TABLE %I.%I ENABLE TRIGGER ALL', schemaname, tablename), '; '
+      ) 
+      FROM pg_tables 
+      WHERE schemaname = 'public'
+   ); 
+END \$\$;
+"
 
 echo "DB restored to postgres://localhost/${LOCALDBNAME}"
 
