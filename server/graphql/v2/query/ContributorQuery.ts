b60cc8c42567b75a96253f1f31738ba6506bf881
Leo Kewitz <leo@opencollective.com>
2025-01-14 10:28:49 +0100

    refact: move queries to lib/contributors

--- server/graphql/v2/query/ContributorQuery.ts
+++ server/graphql/v2/query/ContributorQuery.ts
@@ -1,67 +1,33 @@
-import assert from 'assert';
-
 import { GraphQLNonNull } from 'graphql';
 
-import { sequelize } from '../../../models';
+import { getHostContributorDetail } from '../../../lib/contributors';
 import { BadRequest } from '../../errors';
 import { fetchAccountWithReference, GraphQLAccountReferenceInput } from '../input/AccountReferenceInput';
 import { GraphQLContributor } from '../object/Contributor';
 
 const ContributorQuery = {
   description: 'Get Contributors grouped by their profiles',
-  type: new GraphQLNonNull(GraphQLContributor),
+  type: GraphQLContributor,
   args: {
     account: {
-      type: GraphQLAccountReferenceInput,
+      type: new GraphQLNonNull(GraphQLAccountReferenceInput),
       description: 'Contributor Account reference',
     },
     host: {
-      type: GraphQLAccountReferenceInput,
+      type: new GraphQLNonNull(GraphQLAccountReferenceInput),
       description: 'Context host to fetch the contributor from',
     },
   },
   async resolve(_: void, args, req: Express.Request) {
-    assert(args.account && args.host, 'You must provide either an account or a host to fetch the contributors');
-    const account = await fetchAccountWithReference(args.account, { throwIfMissing: true });
-    const host = await fetchAccountWithReference(args.host, { throwIfMissing: true });
+    const [account, host] = await Promise.all([
+      fetchAccountWithReference(args.account, { throwIfMissing: true }),
+      fetchAccountWithReference(args.host, { throwIfMissing: true }),
+    ]);
     if (!req.remoteUser?.isAdminOfCollective(host)) {
       throw new BadRequest('Only admins can lookup for members using the "host" argument');
     }
 
-    const result = await sequelize.query(
-      `
-        SELECT mc.*,
-          ARRAY_AGG(DISTINCT (m.role)) as roles,
-          ARRAY_AGG(DISTINCT (c.id)) as "ContributedCollectiveIds",
-          'ADMIN' = ANY (ARRAY_AGG(m.role)) as "isAdmin",
-          'ADMIN' = ANY (ARRAY_AGG(m.role))  OR 'MEMBER' = ANY (ARRAY_AGG(m.role)) as "isCore",
-          'BACKER' = ANY (ARRAY_AGG(m.role)) as "isBacker"
-        FROM
-          "Collectives" mc
-          INNER JOIN "Members" m ON mc.id = m."MemberCollectiveId" AND m."deletedAt" IS NULL
-          INNER JOIN "Collectives" c ON c.id = m."CollectiveId" AND c."deletedAt" IS NULL
-          LEFT JOIN "Users" u ON u."CollectiveId" = mc.id AND u."deletedAt" IS NULL
-        WHERE mc."deletedAt" IS NULL
-          AND mc."isIncognito" = FALSE
-          AND mc."id" = :accountid
-          AND c."HostCollectiveId" = :hostid AND c."approvedAt" IS NOT NULL
-            GROUP BY
-          mc.id
-            ORDER BY mc."createdAt" DESC
-        LIMIT 1;
-        `,
-      {
-        replacements: {
-          accountid: account.id,
-          hostid: host.id,
-        },
-        type: sequelize.QueryTypes.SELECT,
-        raw: true,
-        plain: true,
-      },
-    );
-
-    return result;
+    return getHostContributorDetail(account.id, host.id);
   },
 };
 
--- server/graphql/v2/query/collection/ContributorsCollectionQuery.ts
+++ server/graphql/v2/query/collection/ContributorsCollectionQuery.ts
@@ -1,17 +1,17 @@
 import assert from 'assert';
 
-import { GraphQLBoolean, GraphQLInt, GraphQLList, GraphQLNonNull } from 'graphql';
+import { GraphQLInt, GraphQLList, GraphQLNonNull } from 'graphql';
 import { isNil } from 'lodash';
 
-import { ifStr } from '../../../../lib/utils';
-import { sequelize } from '../../../../models';
+import { countHostContributors, getHostContributors } from '../../../../lib/contributors';
 import { BadRequest } from '../../../errors';
 import { GraphQLContributorCollection } from '../../collection/ContributorCollection';
 import { AccountTypeToModelMapping, GraphQLAccountType } from '../../enum/AccountType';
 import { GraphQLMemberRole } from '../../enum/MemberRole';
 import { fetchAccountWithReference, GraphQLAccountReferenceInput } from '../../input/AccountReferenceInput';
-import { GraphQLChronologicalOrderInput } from '../../input/ChronologicalOrderInput';
 import EmailAddress from '../../scalar/EmailAddress';
+
+const DEFAULT_LIMIT = 100;
 
 const ContributorsCollectionQuery = {
   description: 'Get Contributors grouped by their profiles',
@@ -25,37 +25,30 @@ const ContributorsCollectionQuery = {
       type: GraphQLAccountReferenceInput,
       description: 'Host hosting the account',
     },
-    limit: { type: new GraphQLNonNull(GraphQLInt), defaultValue: 100 },
-    offset: { type: new GraphQLNonNull(GraphQLInt), defaultValue: 0 },
     role: { type: new GraphQLList(GraphQLMemberRole) },
     type: { type: new GraphQLList(GraphQLAccountType) },
     email: {
       type: EmailAddress,
       description: 'Admin only. To filter on the email address of a member, useful to check if a member exists.',
     },
-    orderBy: {
-      type: new GraphQLNonNull(GraphQLChronologicalOrderInput),
-      defaultValue: { field: 'createdAt', direction: 'ASC' },
-      description: 'Order of the results',
-    },
-    includeInherited: {
-      type: GraphQLBoolean,
-      defaultValue: true,
-    },
+    limit: { type: new GraphQLNonNull(GraphQLInt), defaultValue: DEFAULT_LIMIT },
+    offset: { type: new GraphQLNonNull(GraphQLInt), defaultValue: 0 },
   },
   async resolve(_: void, args, req: Express.Request) {
-    // Check Pagination arguments
     if (isNil(args.limit) || args.limit < 0) {
-      args.limit = 100;
+      args.limit = DEFAULT_LIMIT;
     }
     if (isNil(args.offset) || args.offset < 0) {
       args.offset = 0;
     }
-    if (args.limit > 100 && !req.remoteUser?.isRoot()) {
-      throw new Error('Cannot fetch more than 1,000 members at the same time, please adjust the limit');
+    if (args.limit > DEFAULT_LIMIT && !req.remoteUser?.isRoot()) {
+      throw new Error(`Cannot fetch more than ${DEFAULT_LIMIT},members at the same time, please adjust the limit`);
     }
 
-    assert(args.account || args.host, 'You must provide either an account or a host to fetch the contributors');
+    assert(
+      Boolean(args.account) !== Boolean(args.host),
+      'You must provide either an account or a host to fetch the contributors',
+    );
 
     const replacements: {
       limit: number;
@@ -81,7 +74,6 @@ const ContributorsCollectionQuery = {
     let account, host;
     if (args.account) {
       account = await fetchAccountWithReference(args.account, { throwIfMissing: true });
-
       replacements['collectiveid'] = account.id;
     }
 
@@ -101,50 +93,8 @@ const ContributorsCollectionQuery = {
       }
     }
 
-    const baseQuery = `
-      FROM
-        "Collectives" mc
-        INNER JOIN "Members" m ON mc.id = m."MemberCollectiveId" AND m."deletedAt" IS NULL
-        INNER JOIN "Collectives" c ON c.id = m."CollectiveId" AND c."deletedAt" IS NULL
-        LEFT JOIN "Users" u ON u."CollectiveId" = mc.id AND u."deletedAt" IS NULL
-      WHERE mc."deletedAt" IS NULL
-        AND mc."isIncognito" = FALSE
-        ${ifStr(replacements.email, 'AND u.email = :email')}
-        ${ifStr(replacements.type, 'AND mc.type in (:type)')}
-        ${ifStr(replacements.role, 'AND m.role in (:role)')}
-        ${ifStr(replacements.collectiveid, 'AND c."id" = :collectiveid')}
-        ${ifStr(replacements.hostid, 'AND c."HostCollectiveId" = :hostid AND c."approvedAt" IS NOT NULL')}
-    `;
-
-    const nodes = () =>
-      sequelize.query(
-        `
-          SELECT mc.*,
-            ARRAY_AGG(DISTINCT (m.role)) as roles,
-            ARRAY_AGG(DISTINCT (c.id)) as "ContributedCollectiveIds",
-            'ADMIN' = ANY (ARRAY_AGG(m.role)) as "isAdmin",
-            'ADMIN' = ANY (ARRAY_AGG(m.role))  OR 'MEMBER' = ANY (ARRAY_AGG(m.role)) as "isCore",
-            'BACKER' = ANY (ARRAY_AGG(m.role)) as "isBacker"
-          ${baseQuery}
-          GROUP BY
-          mc.id
-            ORDER BY mc."createdAt" DESC
-            LIMIT :limit
-            OFFSET :offset;
-        `,
-        { replacements, type: sequelize.QueryTypes.SELECT, raw: true },
-      );
-
-    const totalCount = async () => {
-      const result = await sequelize.query(
-        `
-          SELECT COUNT(DISTINCT (mc.id))
-          ${baseQuery}
-        `,
-        { replacements, type: sequelize.QueryTypes.SELECT, raw: true },
-      );
-      return result[0].count;
-    };
+    const nodes = () => getHostContributors(replacements);
+    const totalCount = () => countHostContributors(replacements);
 
     return { nodes, totalCount, limit: args.limit, offset: args.offset };
   },
