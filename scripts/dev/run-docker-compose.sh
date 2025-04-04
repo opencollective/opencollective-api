83fa14a3f6d63ffc690384471536e3871592b722
Benjamin Piouffle <benjamin@opencollective.com>
2025-04-04 11:24:26 +0200

    add support for docker compose as a plugin

--- scripts/dev/run-docker-compose.sh
+++ scripts/dev/run-docker-compose.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # A wrapper script to run Docker Compose commands
 
-# Reverse condition: just exit if docker not installed
-if ! command -v docker-compose &>/dev/null; then
+# First, check if standalone docker-compose is installed
+if command -v docker-compose &>/dev/null; then
+  # Try running docker-compose directly
+  if docker-compose version &>/dev/null; then
+    docker-compose "$@"
+  else
+    echo "Docker Compose requires root privileges. Running with sudo: docker-compose $@"
+    sudo docker-compose "$@"
+  fi
+# If standalone not found, check if docker compose plugin is available
+elif command -v docker &>/dev/null && docker compose version &>/dev/null; then
+  # Docker compose plugin is available
+  ./scripts/dev/run-docker.sh compose "$@"
+else
+  # Neither method is available
   echo "Docker Compose is not installed. See https://docs.docker.com/compose/install/."
   exit 1
-fi
-
-if docker-compose version &>/dev/null; then
-  docker-compose "$@"
-else
-  echo "Docker Compose requires root privileges. Running with sudo: docker $@"
-  sudo docker-compose "$@"
 fi
