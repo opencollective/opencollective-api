6139b4025cb98aba153b29f3e3883105554d7b75
Benjamin Piouffle <benjamin@opencollective.com>
2025-04-14 09:46:05 +0200

    docs: Improve docker/podman docs & commands

--- scripts/dev/run-docker-compose.sh
+++ scripts/dev/run-docker-compose.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# A wrapper script to run Docker Compose commands
+# A wrapper script to run Docker Compose or Podman Compose commands
 
-# First, check if standalone docker-compose is installed
-if command -v docker-compose &>/dev/null; then
-  # Try running docker-compose directly
+if command -v podman-compose &>/dev/null; then
+    podman-compose "$@"
+elif command -v docker-compose &>/dev/null; then
   if docker-compose version &>/dev/null; then
     docker-compose "$@"
   else
@@ -15,7 +15,8 @@ elif command -v docker &>/dev/null && docker compose version &>/dev/null; then
   # Docker compose plugin is available
   ./scripts/dev/run-docker.sh compose "$@"
 else
-  # Neither method is available
-  echo "Docker Compose is not installed. See https://docs.docker.com/compose/install/."
+  # No compose tool is available
+  echo "Neither Podman Compose nor Docker Compose is installed."
+  echo "See https://github.com/containers/podman-compose#installation or https://docs.docker.com/compose/install/"
   exit 1
 fi
--- scripts/dev/run-docker.sh
+++ scripts/dev/run-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# A wrapper script to run Docker commands
+# A wrapper script to run Docker/Podman commands
 
-# Reverse condition: just exit if docker not installed
-if ! command -v docker &>/dev/null; then
-  echo "Docker is not installed. See https://docs.docker.com/engine/install/."
+# Check for podman first
+if command -v podman &>/dev/null; then
+    podman "$@"
+elif ! command -v docker &>/dev/null; then
+  echo "Neither Podman nor Docker is installed. See https://podman.io/getting-started/installation or https://docs.docker.com/engine/install/."
   exit 1
-fi
-
-if docker info &>/dev/null; then
+elif docker info &>/dev/null; then
   docker "$@"
 else
   echo "Docker requires root privileges. Running with sudo: docker $@"
