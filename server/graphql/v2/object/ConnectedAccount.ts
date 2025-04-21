d54ae9f0f7941f1f3745c9ab73d897f6cb472ec3
Leo Kewitz <leo@opencollective.com>
2025-04-21 14:10:23 +0200

    refact: expose more ConnectedAccount informations

--- server/graphql/v2/object/ConnectedAccount.ts
+++ server/graphql/v2/object/ConnectedAccount.ts
@@ -1,10 +1,13 @@
-import { GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import { GraphQLDateTime, GraphQLJSON } from 'graphql-scalars';
 
+import { Collective, ConnectedAccount } from '../../../models';
+import { Unauthorized } from '../../errors';
 import { GraphQLConnectedAccountService } from '../enum/ConnectedAccountService';
 import { getIdEncodeResolver, IDENTIFIER_TYPES } from '../identifiers';
+import { GraphQLAccount } from '../interface/Account';
 
-export const GraphQLConnectedAccount = new GraphQLObjectType({
+export const GraphQLConnectedAccount = new GraphQLObjectType<ConnectedAccount, Express.Request>({
   name: 'ConnectedAccount',
   description: 'This represents a Connected Account',
   fields: () => ({
@@ -31,5 +34,47 @@ export const GraphQLConnectedAccount = new GraphQLObjectType({
     },
     settings: { type: GraphQLJSON },
     service: { type: new GraphQLNonNull(GraphQLConnectedAccountService) },
+    accountsMirrored: {
+      type: new GraphQLList(GraphQLAccount),
+      description: 'The accounts that are mirroring this connected account',
+      async resolve(connectedAccount, _, req) {
+        if (!req.remoteUser?.isAdmin(connectedAccount.CollectiveId)) {
+          throw new Unauthorized('You need to be logged in as an admin of the account');
+        }
+        const mirroredConnectedAccounts = await ConnectedAccount.findAll({
+          where: {
+            data: {
+              MirrorConnectedAccountId: connectedAccount.id,
+            },
+          },
+          include: [
+            {
+              model: Collective,
+              as: 'collective',
+            },
+          ],
+        });
+        return mirroredConnectedAccounts.map(mirroredConnectedAccount => {
+          return mirroredConnectedAccount.collective;
+        });
+      },
+    },
+    hash: { type: GraphQLString },
+    createdByAccount: {
+      type: GraphQLAccount,
+      description: 'The account who connected this account',
+      async resolve(connectedAccount, _, req) {
+        if (!req.remoteUser?.isAdmin(connectedAccount.CollectiveId)) {
+          throw new Unauthorized('You need to be logged in as an admin of the account');
+        }
+        const user = await req.loaders.User.byId.load(connectedAccount.CreatedByUserId);
+        if (user && user.CollectiveId) {
+          const collective = await req.loaders.Collective.byId.load(user.CollectiveId);
+          if (collective && !collective.isIncognito) {
+            return collective;
+          }
+        }
+      },
+    },
   }),
 });
