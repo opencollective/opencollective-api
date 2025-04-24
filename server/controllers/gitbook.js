0dce14eaf1c15aa43b01a2c0d7250061337e5b0b
François Hodierne <francois@hodierne.net>
2025-04-24 13:50:45 +0200

    make spaceId configurable in gitbook

--- server/controllers/gitbook.js
+++ server/controllers/gitbook.js
@@ -4,12 +4,13 @@ import fetch from 'node-fetch';
 
 const GITBOOK_API_URL = get(config, 'gitbook.apiUrl');
 const GITBOOK_API_KEY = get(config, 'gitbook.apiKey');
+const GITBOOK_SPACE_ID = get(config, 'gitbook.spaceId');
 
 export async function search(req, res) {
   const { query } = req.query;
 
   try {
-    const response = await fetch(`${GITBOOK_API_URL}/v1/spaces/-LWSZizTt4ZC1UNDV89f/search?query=${query}`, {
+    const response = await fetch(`${GITBOOK_API_URL}/v1/spaces/${GITBOOK_SPACE_ID}/search?query=${query}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${GITBOOK_API_KEY}`,
