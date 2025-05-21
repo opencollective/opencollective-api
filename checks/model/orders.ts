d2cebec7108e54e10b5aba69b136b80587346309
François Hodierne <francois@hodierne.net>
2025-05-21 11:42:38 +0200

    checks(orders): add check Paid Orders with CollectiveId/FromCollectiveId mismatch in Transactions

--- checks/model/orders.ts
+++ checks/model/orders.ts
@@ -96,10 +96,63 @@ async function checkPaidOrdersWithDeletedTransactions({ fix = false } = {}) {
   }
 }
 
+async function checkOrdersCollectiveIdMismatch({ fix = false } = {}) {
+  const message = 'Paid Orders with CollectiveId/FromCollectiveId mimsatch in Transactions';
+
+  const results = await sequelize.query(
+    `
+    SELECT *
+    FROM "Orders"
+    INNER JOIN "Transactions" ON "OrderId" = "Orders"."id" AND "Transactions"."deletedAt" IS NULL
+    AND "Transactions"."kind" IN ('CONTRIBUTION', 'ADDED_FUNDS') AND "Transactions"."type" = 'CREDIT'
+    AND "Transactions"."RefundTransactionId" IS NULL
+    WHERE "Orders"."deletedAt" IS NULL
+    AND "Orders"."status" = 'PAID'
+    AND NOT EXISTS (
+      SELECT *
+      FROM "Transactions" t
+      WHERE "OrderId" = "Orders"."id"
+      AND "Orders"."CollectiveId" = t."CollectiveId"
+      AND "Orders"."FromCollectiveId" = t."FromCollectiveId"
+      AND t."deletedAt" IS NULL
+    )
+    AND "Orders"."createdAt" > '2017-01-01'
+    `,
+    { type: sequelize.QueryTypes.SELECT, raw: true },
+  );
+
+  if (results.length > 0) {
+    if (!fix) {
+      throw new Error(message);
+    } else {
+      await sequelize.query(`
+      UPDATE "Orders"
+      SET "FromCollectiveId" = "Transactions" ."FromCollectiveId", "CollectiveId" = "Transactions" ."CollectiveId"
+      FROM "Transactions"
+      WHERE "Orders"."deletedAt" IS NULL
+      AND "Orders"."status" = 'PAID'
+      AND NOT EXISTS (
+        SELECT *
+        FROM "Transactions" t
+        WHERE "OrderId" = "Orders"."id"
+        AND "Orders"."CollectiveId" = t."CollectiveId"
+        AND "Orders"."FromCollectiveId" = t."FromCollectiveId"
+        AND t."deletedAt" IS NULL
+      )
+      AND "Orders"."createdAt" > '2017-01-01'
+      AND "OrderId" = "Orders"."id" AND "Transactions"."deletedAt" IS NULL
+      AND "Transactions"."kind" IN ('CONTRIBUTION', 'ADDED_FUNDS') AND "Transactions"."type" = 'CREDIT'
+      AND "Transactions"."RefundTransactionId" IS NULL
+      `);
+    }
+  }
+}
+
 export const checks = [
   checkDuplicateNonRecurringContribution,
   checkPaidOrdersWithNullProcessedAt,
   checkPaidOrdersWithDeletedTransactions,
+  checkOrdersCollectiveIdMismatch,
 ];
 
 if (!module.parent) {
