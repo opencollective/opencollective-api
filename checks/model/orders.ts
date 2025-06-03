3a7c224a21fc807ac212325485a73a6fd119359d
François Hodierne <francois@hodierne.net>
2025-06-03 13:38:39 +0200

    Add check for Paid Orders with CollectiveId mismatch in Transactions (#10881)
    * checks(orders): add check Paid Orders with CollectiveId/FromCollectiveId mismatch in Transactions
    
    * add logging for orders fixes
    
    * Update checks/model/orders.ts
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>
    
    ---------
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- checks/model/orders.ts
+++ checks/model/orders.ts
@@ -1,5 +1,6 @@
 import '../../server/env';
 
+import logger from '../../server/lib/logger';
 import { sequelize } from '../../server/models';
 
 import { runAllChecksThenExit } from './_utils';
@@ -47,6 +48,7 @@ async function checkPaidOrdersWithNullProcessedAt({ fix = false } = {}) {
     if (!fix) {
       throw new Error(message);
     } else {
+      logger.warn(`Fixing: ${message}`);
       await sequelize.query(`
         UPDATE "Orders"
         SET "processedAt" = "updatedAt"
@@ -80,6 +82,7 @@ async function checkPaidOrdersWithDeletedTransactions({ fix = false } = {}) {
     if (!fix) {
       throw new Error(message);
     } else {
+      logger.warn(`Fixing: ${message}`);
       await sequelize.query(`
         UPDATE "Orders"
         SET "deletedAt" = NOW()
@@ -96,10 +99,64 @@ async function checkPaidOrdersWithDeletedTransactions({ fix = false } = {}) {
   }
 }
 
+async function checkOrdersCollectiveIdMismatch({ fix = false } = {}) {
+  const message = 'Paid Orders with CollectiveId/FromCollectiveId mimsatch in Transactions';
+
+  const results = await sequelize.query(
+    `
+    SELECT *
+    FROM "Orders"
+    INNER JOIN "Transactions" ON "OrderId" = "Orders"."id" AND "Transactions"."deletedAt" IS NULL
+    AND "Transactions"."kind" IN ('CONTRIBUTION', 'ADDED_FUNDS') AND "Transactions"."type" = 'CREDIT'
+    AND "Transactions"."RefundTransactionId" IS NULL
+    WHERE "Orders"."deletedAt" IS NULL
+    AND "Orders"."status" = 'PAID'
+    AND NOT EXISTS (
+      SELECT *
+      FROM "Transactions" t
+      WHERE "OrderId" = "Orders"."id"
+      AND "Orders"."CollectiveId" = t."CollectiveId"
+      AND "Orders"."FromCollectiveId" = t."FromCollectiveId"
+      AND t."deletedAt" IS NULL
+    )
+    AND "Orders"."createdAt" > '2017-01-01'
+    `,
+    { type: sequelize.QueryTypes.SELECT, raw: true },
+  );
+
+  if (results.length > 0) {
+    if (!fix) {
+      throw new Error(message);
+    } else {
+      logger.warn(`Fixing: ${message}`);
+      await sequelize.query(`
+      UPDATE "Orders"
+      SET "FromCollectiveId" = "Transactions"."FromCollectiveId", "CollectiveId" = "Transactions"."CollectiveId"
+      FROM "Transactions"
+      WHERE "Orders"."deletedAt" IS NULL
+      AND "Orders"."status" = 'PAID'
+      AND NOT EXISTS (
+        SELECT *
+        FROM "Transactions" t
+        WHERE "OrderId" = "Orders"."id"
+        AND "Orders"."CollectiveId" = t."CollectiveId"
+        AND "Orders"."FromCollectiveId" = t."FromCollectiveId"
+        AND t."deletedAt" IS NULL
+      )
+      AND "Orders"."createdAt" > '2017-01-01'
+      AND "OrderId" = "Orders"."id" AND "Transactions"."deletedAt" IS NULL
+      AND "Transactions"."kind" IN ('CONTRIBUTION', 'ADDED_FUNDS') AND "Transactions"."type" = 'CREDIT'
+      AND "Transactions"."RefundTransactionId" IS NULL
+      `);
+    }
+  }
+}
+
 export const checks = [
   checkDuplicateNonRecurringContribution,
   checkPaidOrdersWithNullProcessedAt,
   checkPaidOrdersWithDeletedTransactions,
+  checkOrdersCollectiveIdMismatch,
 ];
 
 if (!module.parent) {
