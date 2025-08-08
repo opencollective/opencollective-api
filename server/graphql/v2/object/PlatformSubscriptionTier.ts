aedc586594bf311c408d0357e1086cab9574b909
Leo Kewitz <leo@oficonsortium.org>
2025-08-08 13:35:35 +0200

    refact: GraphQLPlatformSubscriptionTier NonNull pricing and amount

--- server/graphql/v2/object/PlatformSubscriptionTier.ts
+++ server/graphql/v2/object/PlatformSubscriptionTier.ts
@@ -3,44 +3,57 @@ import { GraphQLBoolean, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLS
 import { CommercialFeatures, CommercialFeaturesType } from '../../../constants/feature';
 import { PlatformSubscription } from '../../../models';
 
+import { GraphQLAmount } from './Amount';
+
 export const GraphQLPlatformSubscriptionTier = new GraphQLObjectType({
   name: 'PlatformSubscriptionTier',
   description: 'Type for Platform Subscription Tier',
   fields: () => ({
     title: {
-      type: GraphQLString,
+      type: new GraphQLNonNull(GraphQLString),
       description: 'The title of the subscription tier',
     },
     type: {
-      type: GraphQLString,
+      type: new GraphQLNonNull(GraphQLString),
       description: 'The type of the subscription tier (e.g., "basic", "premium")',
     },
     pricing: {
-      type: new GraphQLObjectType({
-        name: 'PlatformSubscriptionTierPricing',
-        fields: {
-          pricePerMonth: {
-            type: GraphQLInt,
-            description: 'The price of the subscription tier per month',
+      type: new GraphQLNonNull(
+        new GraphQLObjectType({
+          name: 'PlatformSubscriptionTierPricing',
+          fields: {
+            pricePerMonth: {
+              type: new GraphQLNonNull(GraphQLAmount),
+              description: 'The price of the subscription tier per month',
+              resolve: pricing => {
+                return { value: pricing.pricePerMonth, currency: 'USD' };
+              },
+            },
+            pricePerAdditionalCollective: {
+              type: new GraphQLNonNull(GraphQLAmount),
+              description: 'Price for each additional collective beyond the included limit',
+              resolve: pricing => {
+                return { value: pricing.pricePerAdditionalCollective, currency: 'USD' };
+              },
+            },
+            pricePerAdditionalExpense: {
+              type: new GraphQLNonNull(GraphQLAmount),
+              description: 'Price for each additional expense beyond the included limit',
+              resolve: pricing => {
+                return { value: pricing.pricePerAdditionalExpense, currency: 'USD' };
+              },
+            },
+            includedCollectives: {
+              type: new GraphQLNonNull(GraphQLInt),
+              description: 'Number of collectives included in this subscription tier',
+            },
+            includedExpensesPerMonth: {
+              type: new GraphQLNonNull(GraphQLInt),
+              description: 'Number of expenses included in this subscription tier per month',
+            },
           },
-          includedCollectives: {
-            type: GraphQLInt,
-            description: 'Number of collectives included in this subscription tier',
-          },
-          pricePerAdditionalCollective: {
-            type: GraphQLInt,
-            description: 'Price for each additional collective beyond the included limit',
-          },
-          includedExpensesPerMonth: {
-            type: GraphQLInt,
-            description: 'Number of expenses included in this subscription tier per month',
-          },
-          pricePerAdditionalExpense: {
-            type: GraphQLInt,
-            description: 'Price for each additional expense beyond the included limit',
-          },
-        },
-      }),
+        }),
+      ),
     },
     features: {
       type: new GraphQLNonNull(GraphQLPlatformSubscriptionFeatures),
