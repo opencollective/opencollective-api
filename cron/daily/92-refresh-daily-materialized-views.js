0b33242452ea75c681262788f465c62cabd44b55
Leo Kewitz <leo@oficonsortium.org>
2025-12-08 16:52:24 +0100

    chore: refresh heavy materialized view CommunityActivitySummary daily instead of hourly

--- cron/daily/92-refresh-daily-materialized-views.js
+++ cron/daily/92-refresh-daily-materialized-views.js
@@ -5,7 +5,12 @@ import { HandlerType, reportErrorToSentry } from '../../server/lib/sentry';
 import { sequelize } from '../../server/models';
 import { runCronJob } from '../utils';
 
-const VIEWS = ['CollectiveTransactionStats', 'TransactionBalances', 'CollectiveBalanceCheckpoint'];
+const VIEWS = [
+  'CollectiveTransactionStats',
+  'TransactionBalances',
+  'CollectiveBalanceCheckpoint',
+  'CommunityActivitySummary',
+];
 
 /**
  * Refresh the materialized views.
