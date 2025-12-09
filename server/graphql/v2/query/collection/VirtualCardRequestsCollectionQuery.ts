5fa0758b1ad55324cf094787b8df3480bf515dd0
Benjamin Piouffle <benjamin@opencollective.com>
2025-12-09 10:16:21 +0100

    enhancement: Misc improvements around authorization

--- server/graphql/v2/query/collection/VirtualCardRequestsCollectionQuery.ts
+++ server/graphql/v2/query/collection/VirtualCardRequestsCollectionQuery.ts
@@ -3,6 +3,8 @@ import { isEmpty } from 'lodash';
 import { Order } from 'sequelize';
 
 import models from '../../../../models';
+import { checkRemoteUserCanUseVirtualCards } from '../../../common/scope-check';
+import { Forbidden } from '../../../errors';
 import { GraphQLVirtualCardRequestCollection } from '../../collection/VirtualCardRequestCollection';
 import { GraphQLVirtualCardRequestStatus } from '../../enum/VirtualCardRequestStatus';
 import { fetchAccountWithReference, GraphQLAccountReferenceInput } from '../../input/AccountReferenceInput';
@@ -20,18 +22,32 @@ const VirtualCardRequestsCollectionQuery = {
     collective: { type: new GraphQLList(GraphQLAccountReferenceInput) },
   },
   async resolve(_: void, args, req: Express.Request): Promise<CollectionReturnType> {
-    const host = await fetchAccountWithReference(args.host, { throwIfMissing: true });
+    checkRemoteUserCanUseVirtualCards(req);
 
+    const host = await fetchAccountWithReference(args.host, { throwIfMissing: true });
+    const isHostAdmin = req.remoteUser.isAdminOfCollective(host);
     const where = {
       HostCollectiveId: host.id,
     };
 
     if (!isEmpty(args.collective)) {
-      where['CollectiveId'] = await Promise.all(
+      const collectives = await Promise.all(
         args.collective.map(collectiveReference =>
           fetchAccountWithReference(collectiveReference, { throwIfMissing: true, loaders: req.loaders }),
         ),
-      ).then(collectives => collectives.map(collective => collective.id));
+      );
+
+      for (const collective of collectives) {
+        if (collective.HostCollectiveId !== host.id) {
+          throw new Forbidden(`One of the requested collectives is not hosted by ${host.name}.`);
+        } else if (!isHostAdmin && !req.remoteUser.isAdminOfCollective(collective)) {
+          throw new Forbidden('You are not authorized to view virtual card requests for this collective.');
+        }
+      }
+
+      where['CollectiveId'] = collectives.map(collective => collective.id);
+    } else if (!isHostAdmin) {
+      throw new Forbidden('You are not authorized to view virtual card requests for this host.');
     }
 
     if (!isEmpty(args.status)) {
