d8780d24551ad2eb793d89780cb4f13ca3dbd5b9
Gustav Larsson <gustav.larsson@gmail.com>
2025-12-30 15:32:01 +0100

    Add "createdByUsers" resolver to orders collection query

--- server/graphql/v2/collection/OrderCollection.js
+++ server/graphql/v2/collection/OrderCollection.js
@@ -1,6 +1,7 @@
 import { GraphQLList, GraphQLObjectType } from 'graphql';
 
 import { CollectionFields, GraphQLCollection } from '../interface/Collection';
+import { GraphQLAccount } from '../interface/Account';
 import { GraphQLOrder } from '../object/Order';
 
 export const GraphQLOrderCollection = new GraphQLObjectType({
@@ -12,6 +13,10 @@ export const GraphQLOrderCollection = new GraphQLObjectType({
       ...CollectionFields,
       nodes: {
         type: new GraphQLList(GraphQLOrder),
+      },
+      createdByUsers: {
+        type: new GraphQLList(GraphQLAccount),
+        description: 'The accounts that created the orders in this collection, regardless of pagination',
       },
     };
   },
