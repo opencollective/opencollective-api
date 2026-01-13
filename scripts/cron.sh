23e2254085b1b0ce4a2f36657046862ac28ac4f1
François Hodierne <francois@hodierne.net>
2026-01-13 20:58:01 +0100

    cleanup npm run script

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,5 +4,5 @@ TZ=utc
 
 for FILE in `ls $FILE_PATH`; do 
   echo "Running cron job $FILE_PATH$FILE";
-  NODE_OPTIONS="-r @sentry/node/preload" npm run script -- "$FILE_PATH$FILE";
+  NODE_OPTIONS="-r @sentry/node/preload" npm run script "$FILE_PATH$FILE";
 done;
--- scripts/watch_email_template.sh
+++ scripts/watch_email_template.sh
@@ -24,7 +24,7 @@ fi
 
 function compileTemplate {
   echo "Compiling $1";
-  npm run script -- scripts/compile-email.js $1 > /tmp/$1.html
+  npm run script scripts/compile-email.js $1 > /tmp/$1.html
   echo "Done: /tmp/$1.html";
   return 0
 }
