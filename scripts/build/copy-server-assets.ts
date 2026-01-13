0b9fb5539987f6608d62236f31e7580e09713cb7
François Hodierne <francois@hodierne.net>
2026-01-13 12:13:15 +0100

    review eslint config

--- scripts/build/copy-server-assets.ts
+++ scripts/build/copy-server-assets.ts
@@ -1,15 +1,15 @@
-const path = require('path');
+import path from 'path';
 
-const fs = require('fs-extra');
+import fs from 'fs-extra';
 
 const rootDir = path.join(__dirname, '..', '..');
 const sourceDir = path.join(rootDir, 'server');
 const distDir = path.join(rootDir, 'dist');
-const relativeFromRepo = filePath => path.relative(rootDir, filePath);
+const relativeFromRepo = (filePath: string) => path.relative(rootDir, filePath);
 
 const excludedExtensions = new Set(['.ts', '.tsx', '.d.ts', '.js', '.jsx', '.md']);
 
-const shouldCopy = src => {
+const shouldCopy = (src: string): boolean => {
   const stat = fs.statSync(src);
   if (stat.isDirectory()) {
     return true;
