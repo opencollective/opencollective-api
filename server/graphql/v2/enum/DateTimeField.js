5e2750031b53fc93f10d8780d47e3143e5a6256b
Gustav Larsson <gustav.larsson@gmail.com>
2026-01-20 14:19:19 +0100

    feat(Expenses): Add `paidAt` and ability to sort on `paidAt` to ExpensesCollectionQuery (#11333)
    * feat(Expenses): Add `paidAt` and ability to sort on `paidAt` to ExpensesCollectionQuery
    
    * Add expense paidAt index
    
    * Update server/graphql/v2/query/collection/ExpensesCollectionQuery.ts
    
    Co-authored-by: Benjamin Piouffle <benjamin@oficonsortium.org>
    
    * Update migrations/20260116133511-add-expense-payment-date-index.ts
    
    Co-authored-by: Benjamin Piouffle <benjamin@oficonsortium.org>
    
    * Add checking RefundTransactionId = null in the Expense.paidAt resolver
    
    * Linting migration
    
    * Update gql
    
    * Lint
    
    * Fix migration
    
    ---------
    
    Co-authored-by: Benjamin Piouffle <benjamin@oficonsortium.org>

--- server/graphql/v2/enum/DateTimeField.js
+++ server/graphql/v2/enum/DateTimeField.js
@@ -16,5 +16,9 @@ export const GraphQLDateTimeField = new GraphQLEnumType({
       value: 'lastChargedAt',
       description: 'Orders only: The date when an order was last charged, defaults to createdAt if never charged',
     },
+    PAID_AT: {
+      value: 'paidAt',
+      description: 'Expenses only: The date when an expense was paid (based on the related transaction clearedAt)',
+    },
   },
 });
