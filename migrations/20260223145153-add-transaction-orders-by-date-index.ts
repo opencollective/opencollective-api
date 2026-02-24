f647ae2e23be17a0f7d6557515b77d956d8c03e2
Henrique <hdiniz@users.noreply.github.com>
2026-02-24 15:05:30 -0300

    Update migrations/20260223145153-add-transaction-orders-by-date-index.ts
    Co-authored-by: Leo Kewitz <leo@opencollective.com>

--- migrations/20260223145153-add-transaction-orders-by-date-index.ts
+++ migrations/20260223145153-add-transaction-orders-by-date-index.ts
@@ -8,7 +8,6 @@ module.exports = {
       ON "Transactions" (COALESCE("clearedAt", "createdAt")) INCLUDE ("OrderId", "isDebt", "isRefund", kind, "CollectiveId", "HostCollectiveId", "FromCollectiveId") WHERE (kind = ANY (ARRAY['CONTRIBUTION'::"enum_Transactions_kind", 'ADDED_FUNDS'::"enum_Transactions_kind"])) AND "deletedAt" IS NULL AND type::text = 'CREDIT'::text
     `);
 
-    // "Transactions_Orders_by_date" btree (COALESCE("clearedAt", "createdAt")) INCLUDE ("OrderId", "isDebt", "isRefund", kind, "CollectiveId", "HostCollectiveId", "FromCollectiveId") WHERE (kind = ANY (ARRAY['CONTRIBUTION'::"enum_Transactions_kind", 'ADDED_FUNDS'::"enum_Transactions_kind"])) AND "deletedAt" IS NULL AND type::text = 'CREDIT'::text
   },
 
   async down(queryInterface) {
