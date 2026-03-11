c0758afdfe70d17d0e12fe8251e49f1afd6c886e
Leo Kewitz <leo@oficonsortium.org>
2026-03-11 11:46:06 +0100

    fix: update upload-sourcemaps to fetch correct release commit hash

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -6,10 +6,19 @@ if [ -z "$SENTRY_PROJECT" ]; then
   exit 0
 fi
 
-if [ -z "$HEROKU_SLUG_COMMIT" ]; then
-  RELEASE=$(git rev-parse HEAD)
-else
+if ! [ -z "$SOURCE_VERSION" ]; then
+  RELEASE=$SOURCE_VERSION
+elif ! [ -z "$HEROKU_SLUG_COMMIT" ]; then
   RELEASE=$HEROKU_SLUG_COMMIT
+# Fallback for when running locally. Remember that this breaks Heroku since git is available but the build folder does not contain the .git folder.
+elif [ -x "$(command -v git)" ]; then
+  RELEASE=$(git rev-parse HEAD)
+fi
+
+
+if [ -z "$HEROKU_SLUG_COMMIT" ]; then
+  echo "Could not determine release version. Please set the SENTRY_RELEASE environment variable or ensure that git is available. Ignoring..."
+  exit 0
 fi
 
 echo "Injecting source maps for release $RELEASE..."
