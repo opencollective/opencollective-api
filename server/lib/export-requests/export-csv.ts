377facb11a721bea6e9d233666f9e438c2b1b4f7
Leo Kewitz <leo@opencollective.com>
2026-03-12 10:18:02 +0100

    refact: add TransasctionsCollectionQuery (#11522)
    includeEditedReversedTransactions argument
    
    This argument defaults to true to guarantee backward compatibility. When
    set to false, will omit edited transactions.

--- server/lib/export-requests/export-csv.ts
+++ server/lib/export-requests/export-csv.ts
@@ -98,6 +98,12 @@ const makeUrl = ({
   if (!isNil(queryFilter.variables.hasDebt)) {
     url.searchParams.set('hasDebt', queryFilter.variables.hasDebt ? '1' : '0');
   }
+  if (!isNil(queryFilter.variables.includeEditedReversedTransactions)) {
+    url.searchParams.set(
+      'includeEditedReversedTransactions',
+      queryFilter.variables.includeEditedReversedTransactions ? '1' : '0',
+    );
+  }
   if (queryFilter.variables.order) {
     url.searchParams.set('orderId', String((queryFilter.variables.order as OrderReferenceInputGraphQLType).legacyId));
   }
