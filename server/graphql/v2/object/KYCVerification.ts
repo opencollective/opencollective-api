f086a30b88cdd9d8b2ef59e775595be82eeec380
Henrique <hdiniz@users.noreply.github.com>
2026-03-19 10:54:32 -0300

    Remove persona kyc related code (#11556)
    * Remove persona related code
    
    * Update graphql

--- server/graphql/v2/object/KYCVerification.ts
+++ server/graphql/v2/object/KYCVerification.ts
@@ -1,5 +1,5 @@
-import { GraphQLBoolean, GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLUnionType } from 'graphql';
-import { GraphQLDateTime, GraphQLJSON } from 'graphql-scalars';
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLUnionType } from 'graphql';
+import { GraphQLDateTime } from 'graphql-scalars';
 
 import { KYCProviderName } from '../../../lib/kyc/providers';
 import { EntityShortIdPrefix, isEntityMigratedToPublicId } from '../../../lib/permalink/entity-map';
@@ -135,50 +135,13 @@ const GraphQLManualKYCProviderData = new GraphQLObjectType({
   }),
 });
 
-const GraphQLPersonaKYCProviderData = new GraphQLObjectType({
-  name: 'PersonaKYCProviderData',
-  description: 'Persona KYC data',
-  fields: () => ({
-    imported: {
-      type: new GraphQLNonNull(GraphQLBoolean),
-      description: 'Whether this persona inquiry was imported',
-      resolve(kycVerification: KYCVerification<KYCProviderName.PERSONA>) {
-        return !!kycVerification.providerData.imported;
-      },
-    },
-    id: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'ID of the Persona inquiry',
-      resolve(kycVerification: KYCVerification<KYCProviderName.PERSONA>) {
-        return kycVerification.providerData.inquiry.id;
-      },
-    },
-    status: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'Status of the Persona inquiry',
-      resolve(kycVerification: KYCVerification<KYCProviderName.PERSONA>) {
-        return kycVerification.providerData.inquiry.attributes.status;
-      },
-    },
-    fields: {
-      type: new GraphQLNonNull(GraphQLJSON),
-      description: 'Contains KYC data verified by this inquiry',
-      resolve(kycVerification: KYCVerification<KYCProviderName.PERSONA>) {
-        return kycVerification.providerData.inquiry.attributes.fields ?? {};
-      },
-    },
-  }),
-});
-
 const GraphQLKYCProviderData = new GraphQLUnionType({
   name: 'KYCProviderData',
-  types: [GraphQLManualKYCProviderData, GraphQLPersonaKYCProviderData],
+  types: [GraphQLManualKYCProviderData],
   resolveType(kycVerification: KYCVerification) {
     switch (kycVerification.provider) {
       case KYCProviderName.MANUAL:
         return 'ManualKYCProviderData';
-      case KYCProviderName.PERSONA:
-        return 'PersonaKYCProviderData';
     }
   },
 });
--- server/lib/kyc/index.ts
+++ server/lib/kyc/index.ts
@@ -1,12 +1,9 @@
 import { manualKycProvider } from './providers/manual';
-import { personaKycProvider } from './providers/persona';
 import { KYCProviderName } from './providers';
 
 export function getKYCProvider(name: KYCProviderName) {
   switch (name) {
     case KYCProviderName.MANUAL:
       return manualKycProvider;
-    case KYCProviderName.PERSONA:
-      return personaKycProvider;
   }
 }
