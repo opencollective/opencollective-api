4596c2d153ffa7a63fed0f711d419f60d0455df6
Benjamin Piouffle <benjamin@oficonsortium.org>
2026-05-06 12:15:19 +0200

    WIP

--- server/graphql/v2/enum/LastCommentByType.ts
+++ server/graphql/v2/enum/LastCommentByType.ts
@@ -5,6 +5,10 @@ export const GraphQLLastCommentBy = new GraphQLEnumType({
   description: 'Defines role of the last comment author',
   values: {
     USER: { value: 'USER', description: 'Expense Submitter' },
+    NON_FROM_ACCOUNT_ADMIN: {
+      value: 'NON_FROM_ACCOUNT_ADMIN',
+      description: 'Last comment author is not an admin of the expense payee (FromCollectiveId)',
+    },
     HOST_ADMIN: { value: 'HOST_ADMIN', description: 'Fiscal Host Admin' },
     NON_HOST_ADMIN: { value: 'NON_HOST_ADMIN', description: 'Not a Fiscal Host Admin' },
     COLLECTIVE_ADMIN: { value: 'COLLECTIVE_ADMIN', description: 'Collective Admin' },
